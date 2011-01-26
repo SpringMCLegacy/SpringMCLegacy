8484cb2e178f5f397703099df823e29b690b63b1
mechwarspring <zverofaust@users.noreply.github.com>
2011-01-26 20:48:13 +0000

    -Slightly increased Demolisher model size (closer to Challenger); -Changed Scorpion weapon config to 1 Light Gauss, 2 MG

---
+++ weapons/LightGauss.lua
@@ -0,0 +1,34 @@
+weaponDef = {
+	name                    = "Light Gauss Rifle",
+	weaponType              = "Cannon",
+	explosionGenerator    	= "custom:AP_SMALL",
+	cegTag					= "RailTrail",
+	soundHit              	= [[GEN_Explode1]],
+	soundStart            	= [[Gauss_Fire]],
+	burnblow				= false, 	--Bullets explode at range limit.
+	collideFriendly			= true,
+	noSelfDamage            = true,
+	turret                  = true,
+	ballistic				= 1,
+	range                   = 2500,
+	accuracy                = 100,
+	areaOfEffect            = 3,
+	weaponVelocity          = 2700,
+	reloadtime              = 2,
+	renderType				= 1,
+	size					= 1.5,
+	sizeDecay				= 0,
+	separation				= 2, 		--Distance between each plasma particle.
+	stages					= 200, 		--Number of particles used in one plasma shot.
+--	AlphaDecay				= 0.05, 		--How much a plasma particle is more transparent than the previous particle. 
+	rgbcolor				= "0.75 0.75 1.0",
+	intensity				= 0.5,
+	damage = {
+		default = 160, --80 DPS
+	},
+	
+	
+	
+}
+
+return lowerkeys({ LightGauss = weaponDef })
