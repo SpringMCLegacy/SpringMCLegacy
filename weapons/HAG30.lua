32bfb8e98dd8c38603de1e6be9232b2533f83ef3
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-01-28 20:50:33 +0000

    Changed Oro to "Jihad-era" late model with Hyper Assault Gauss Rifle and 2x armor.

---
+++ weapons/HAG30.lua
@@ -0,0 +1,34 @@
+weaponDef = {
+	name                    = "LBX/20 AutoCannon",
+	weaponType              = "Cannon",
+	explosionGenerator    	= "custom:HE_MEDIUM",
+	soundHit              	= [[GEN_Explode1]],
+	soundStart            	= [[Gauss_Fire]],
+	burnblow				= false, 	--Bullets explode at range limit.
+	collideFriendly			= true,
+	noSelfDamage            = true,
+	turret                  = true,
+	ballistic				= 1,
+	range                   = 2400,
+	accuracy                = 200,
+	areaOfEffect            = 1,
+	weaponVelocity          = 2000,
+	reloadtime              = 5,
+	burst					= 2,
+	burstrate				= 0.00001,
+	sprayAngle				= 100,
+	projectiles				= 15,
+	renderType				= 1,
+	size					= 0.5,
+	sizeDecay				= 0,
+	separation				= 2, 		--Distance between each plasma particle.
+	stages					= 50, 		--Number of particles used in one plasma shot.
+--	AlphaDecay				= 0.05, 		--How much a plasma particle is more transparent than the previous particle. 
+	rgbcolor				= "0.75 0.75 1.0",
+	intensity				= 0.5,
+	damage = {
+		default = 50, --300 DPS
+	},
+}
+
+return lowerkeys({ HAG30 = weaponDef })
