c4086b6be99a8caae5a1b2f4897f4d54e6a8af19
mechwarspring <zverofaust@users.noreply.github.com>
2011-01-31 17:46:20 +0000

    Added that volumescale widget New PPC muzzleflash thingies IS Locust mech added (doesnt work)

---
+++ scripts/IS_Locust.lua
@@ -0,0 +1,119 @@
+-- Test Tank Script
+-- useful global stuff
+local ud = UnitDefs[Spring.GetUnitDefID(unitID)] -- unitID is available automatically to all LUS
+local weapons = ud.weapons
+--piece defines
+local pelvis, torso, launcher1, launcher2, barrel = piece ("pelvis", "torso", "launcher1", "launcher2", "barrel")
+local lupperleg, llowerleg, rupperleg, rlowerleg, rfronttoes, rbacktoes, lfronttoes, lbacktoes = piece ("lupperleg", "llowerleg", "rupperleg", "rlowerleg", "rfronttoes", "rbacktoes", "lfronttoes", "lbacktoes")
+local flare = piece ("flare")
+local smokePieces = {body, turret}
+
+local missileWeaponIDs = {[2] = true, [3] = true}
+ 
+local launchPoints = {}
+local numPoints = {}
+local currPoints = {}
+ 
+for weaponID in pairs(missileWeaponIDs) do
+        launchPoints[weaponID] = {}
+        currPoints[weaponID] = 1
+        numPoints[weaponID] = WeaponDefs[weapons[weaponID].weaponDef].salvoSize
+        for i = 1, numPoints[weaponID] do
+                launchPoints[weaponID][i] = piece("launchpoint_" .. weaponID .. "_" .. i)
+        end
+end
+
+local rad = math.rad
+local currLaunchPoint = 1
+
+-- constants
+local SIG_AIM = 2
+
+local RESTORE_DELAY = Spring.UnitScript.GetLongestReloadTime(unitID) * 2
+
+-- includes
+include "smokeunit.lua"
+
+--SFX defines
+SMALL_MUZZLEFLASH = SFX.CEG+0
+MG_MUZZLEFLASH = SFX.CEG+1
+
+function script.Create()
+	StartThread(SmokeUnit())
+end
+
+function script.StartMoving()
+	--omg WALK!!!!!
+end
+
+function script.StopMoving()
+	--omg STOP WALKING!!!!!
+end
+
+local function RestoreAfterDelay(unitID)
+	Sleep(RESTORE_DELAY)
+	Turn(torso, y_axis, 0, math.rad(75))
+	Turn(barrel, x_axis, 0, math.rad(100))
+	Turn(launcher1, x_axis, 0, math.rad(100))
+	Turn(launcher2, x_axis, 0, math.rad(100))
+end
+
+function script.AimWeapon(weaponID, heading, pitch)
+	Signal(SIG_AIM ^ weaponID) -- 2 'to the power of' weapon ID
+    SetSignalMask(SIG_AIM ^ weaponID)
+		if weaponID == 1 then
+			Turn(barrel, x_axis, -pitch, rad(200))
+		--elseif weaponID == 2 then
+		--	Turn(launcher1, x_axis, -pitch, rad(150))
+		--elseif weaponID == 3 then
+		--	Turn(launcher2, x_axis, -pitch, rad(150))
+		end
+	Turn(torso, y_axis, heading, rad(100))
+	WaitForTurn(torso, y_axis)
+	StartThread(RestoreAfterDelay)
+	return true
+end
+
+function script.FireWeapon(weaponID)
+		if weaponID == 1 then
+			EmitSfx(flare, MG_MUZZLEFLASH)
+		end
+end
+
+function script.Shot(weaponID)
+	if missileWeaponIDs[weaponID] then
+		EmitSfx(launchPoints[weaponID][currPoints[weaponID]], SMALL_MUZZLEFLASH)
+        currPoints[weaponID] = currPoints[weaponID] + 1
+        if currPoints[weaponID] > numPoints[weaponID] then 
+                currPoints[weaponID] = 1
+        end
+	end		
+end
+
+function script.AimFromWeapon(weaponID) 
+	return torso
+end
+
+function script.QueryWeapon(weaponID) 
+	if missileWeaponIDs[weaponID] then
+		return launchPoints[weaponID][currPoints[weaponID]]
+	else
+		if weaponID == 1 then
+			return flare
+		end
+	end
+end
+
+function script.Killed(recentDamage, maxHealth)
+	--local severity = recentDamage / maxHealth * 100
+	--if severity <= 25 then
+	--	Explode(body, math.bit_or({SFX.BITMAPONLY, SFX.BITMAP1}))
+	--	return 1
+	--elseif severity <= 50 then
+	--	Explode(body, math.bit_or({SFX.FALL, SFX.BITMAP1}))
+	--	return 2
+	--else
+	--	Explode(body, math.bit_or({SFX.FALL, SFX.SMOKE, SFX.FIRE, SFX.EXPLODE_ON_HIT, SFX.BITMAP1}))
+	--	return 3
+	--end
+end
---
+++ units/is/IS_Locust.lua
@@ -0,0 +1,71 @@
+local IS_Locust = {
+	name              	= "Locust",
+	description         = "Light Scout Mech",
+	objectName        	= "IS_Locust.s3o",
+	script				= "IS_Locust.lua",
+	category 			= "mech ground",
+	sightDistance       = 1000,
+	radarDistance      	= 1500,
+		activateWhenBuilt   = true,
+	maxDamage           = 3000,
+	mass                = 2000,
+	footprintX			= 2,
+	footprintZ 			= 2,
+	collisionVolumeType = "box",
+	collisionVolumeScales = "15 35 15",
+	collisionVolumeOffsets = "0 2 0",
+	collisionVolumeTest = 1,
+--	leaveTracks			= 1,
+--	trackOffset			= 10,--no idea what this does
+--	trackStrength		= 2.5,--how visible the tracks are
+--	trackStretch		= 1,-- how much the tracks stretch, the higher the number the more "compact" they become
+--	trackType			= "Thick",--graphics file to use for the track decal, from \bitmaps\tracks\ folder
+--	trackWidth			= 20,--width to render the decal
+	buildCostEnergy     = 0,
+	buildCostMetal      = 0,
+	buildTime           = 0,
+	canMove				= true,
+		movementClass   = "TANK",
+		maxVelocity		= 6.5, --130kph/10/2
+		maxReverseVelocity= 3.0,
+		acceleration    = 2.0,
+		brakeRate       = 0.5,
+		turnRate 		= 500,
+	
+	canAttack 			= true,
+		--Makes unit use weapon from /weapons folder
+		weapons 		= {	
+			[1] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 90,
+			},
+			[2] = {
+				name	= "SRM6",
+				--weaponSlaveTo2 = 1,
+				mainDir = "0 0 1",
+				maxAngleDif = 90,
+			},
+			[3] = {
+				name	= "SRM6",
+				--weaponSlaveTo3 = 1,
+				mainDir = "0 0 1",
+				maxAngleDif = 90,
+			},
+		},
+		
+	--Gets CEG effects from /gamedata/explosions folder
+
+	sfxtypes = {
+		explosiongenerators = {
+		"custom:SMALL_MUZZLEFLASH",
+		"custom:MG_MUZZLEFLASH",
+		},
+	},
+    customparams = {
+		hasturnbutton	= "1",
+		helptext		= "Armament: 1 x Medium Beam Laser, 2 x SRM-6 - Armor: 3 tons",
+    },
+}
+
+return lowerkeys({ ["IS_Locust"] = IS_Locust })
