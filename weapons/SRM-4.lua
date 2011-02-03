2ae850ca5599f1b76636e9db215a71d769e4c771
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-02-03 07:34:16 +0000

    -Added Clan Firemoth, mbut it doesnt work. -Edited Locust helptext. Also, dicks. Dicks everywhere.

---
+++ weapons/SRM-4.lua
@@ -0,0 +1,40 @@
+weaponDef = {
+	name                    = "Short Range Missile 4",
+	weaponType              = "MissileLauncher",
+	renderType				= 1,
+	explosionGenerator    	= "custom:HE_MEDIUM",
+--	cegTag					= "BazookaTrail",
+	smokeTrail				= true,
+	smokeDelay				= "0.05",
+	soundHit              	= [[GEN_Explode3]],
+	soundStart            	= [[SRM_Fire]],
+--	soundTrigger			= 0,
+	burnblow				= true, 	--Bullets explode at range limit.
+	collideFriendly			= true,
+	noSelfDamage            = true,
+	turret                  = true,
+	range                   = 900,
+	accuracy                = 100,
+	guidance				= true,
+	selfprop				= true,
+	lineOfSight				= true,
+	tracks					= true,
+	turnRate				= 4000,
+	weaponTimer				= 5,
+	areaOfEffect            = 20,
+	startVelocity			= 700,
+	weaponVelocity          = 900,
+	reloadtime              = 5,
+	burst					= 4,
+	burstrate				= 0.1,
+	sprayAngle 				= 100,
+	model					= "Missile.s3o",
+	damage = {
+		default = 100,--20 DPS
+	},
+	
+	
+	
+}
+
+return lowerkeys({ SRM4 = weaponDef })
