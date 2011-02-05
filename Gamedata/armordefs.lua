b1b973d2af3817b5314688a22444d6e934232297
mechwarspring <zverofaust@users.noreply.github.com>
2011-02-05 23:09:09 +0000

    Fixed

--- Gamedata/armordefs.lua
+++ Gamedata/armordefs.lua
@@ -1,6 +1,18 @@
 local armorDefs = {
-	dropships  =  {
+   dropships = {
 		"CL_Factory",
 		"IS_Factory",	
-	},
+   },
 }
+-- convert to named maps  (trepan is a noob)
+for categoryName, categoryTable in pairs(armorDefs) do
+  local t = {}
+  for _, unitName in pairs(categoryTable) do
+    t[unitName] = 1
+  end
+  armorDefs[categoryName] = t
+end
+
+local system = VFS.Include('gamedata/system.lua')  
+
+return system.lowerkeys(armorDefs)
