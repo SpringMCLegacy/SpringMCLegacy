b8bc14f3bb48cda6a37d3a781ae2e4a27e6e25bc
mechwarspring <zverofaust@users.noreply.github.com>
2011-02-09 19:47:28 +0000

    Auras, for doing stuff like repair radius and improving weapons of units within a certain radius and etc. Implemented on Dropships.

---
+++ LuaRules/Configs/aura_defs.lua
@@ -0,0 +1,56 @@
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--
+--local auras = {
+--      reb_commander = {
+--              range = 300,            --range of the aura (applies to all effects)
+--              transport = true,       --only applies to units being transported by this unit (you should also set range to 0 if this is true)
+--              buildpower = {
+--                      rate = 2,       --buildspeed multiplier
+--                      mask = {"infantry","factories"},
+--                                      --A mask will only apply this effect to these categories of units
+--                                      --To apply to all (applicable) units, use the keyword "all"
+--                                      --All masks work this way. The values are the same as those in the "Category" tag in the unit's fbi
+--                                      --(you may need to add the new keywords. Remember there's a max of 32 categories)
+--                                      --Do not use any caps
+--              },
+--              hp = {
+--                      rate = 2,       --armor multiplier
+--                      mask = {"all"},
+--              },
+--              heal = {
+--                      rate = 0.1,     --heals the unit by maxHP * rate per second. The per second is non-negotiable
+--                      mask = {"infantry"},
+--              },
+--              weapons = {
+--                      reloadtime = 0.5,       --reloadtime multiplier
+--                      range = 2,              --Range multiplier. This also increases the projectile speed so lasers don't fade out.
+--                                              --Missiles and artillery may have problems with excessive range increases
+--                      mask = {"infantry","droid"},
+--              },
+--      },
+--
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+ 
+ 
+local auras = {
+        cl_factory = {
+                range = 300,
+                transport = false,
+                heal = {
+						rate = 0.1,     --heals the unit by maxHP * rate per second. The per second is non-negotiable
+						mask = {"all"},
+				},
+        },
+		is_factory = {
+                range = 300,
+                transport = false,
+                heal = {
+						rate = 0.1,     --heals the unit by maxHP * rate per second. The per second is non-negotiable
+						mask = {"all"},
+				},
+        },
+}
+ 
+return auras
