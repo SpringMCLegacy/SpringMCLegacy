e657d469355fc836ae3eb7799e04c969091ef19d
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-02-10 18:02:52 +0000

    -Myst Linx stuff that apparently wasnt added??? -Beacon model.

---
+++ maps/flagConfig/Seths_Ravine_V2_profile.lua
@@ -0,0 +1,24 @@
+local resources = {
+	{
+		x = 3270,
+		z = 2430,
+		feature = nil
+	},
+	{
+		x = 4975,
+		z = 3750,
+		feature = nil
+	},
+	{
+		x = 2530,
+		z = 5300,
+		feature = nil
+	},
+	{
+		x = 5650,
+		z = 950,
+		feature = nil
+	},
+}
+
+return resources
