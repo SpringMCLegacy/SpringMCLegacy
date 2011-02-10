aaac953bfa94e8466932de3d0ae961e9f66ad145
mechwarspring <zverofaust@users.noreply.github.com>
2011-02-10 23:49:30 +0000

    -Added Clan Cougar light mech. -Added ATM-6 mufuckin weaponz.

---
+++ weapons/ATM6.lua
@@ -0,0 +1,42 @@
+weaponDef = {
+	name                    = "Advanced Tactical Missile-6",
+	weaponType              = "MissileLauncher",
+	renderType				= 1,
+	explosionGenerator    	= "custom:HE_MEDIUM",
+--	cegTag					= "BazookaTrail",
+	smokeTrail				= true,
+	smokeDelay				= "0.05",
+	soundHit              	= [[GEN_Explode3]],
+	soundStart            	= [[ATM_Fire]],
+--	soundTrigger			= 0,
+	burnblow				= true, 	--Bullets explode at range limit.
+	collideFriendly			= true,
+	noSelfDamage            = true,
+	turret                  = true,
+	range                   = 1800,
+	accuracy                = 100,
+	wobble					= 700,
+	dance					= 80,
+	guidance				= true,
+	selfprop				= true,
+	lineOfSight				= true,
+	tracks					= true,
+	turnRate				= 4000,
+	weaponTimer				= 10,
+	areaOfEffect            = 20,
+	startVelocity			= 700,
+	weaponVelocity          = 900,
+	reloadtime              = 10,
+	burst					= 6,
+	burstrate				= 0.1,
+	sprayAngle 				= 100,
+	model					= "Missile.s3o",
+	damage = {
+		default = 167,--15 DPS
+	},
+	customparams = {
+		heatgenerated		= "60",--6/sec
+    },
+}
+
+return lowerkeys({ ATM6 = weaponDef })
