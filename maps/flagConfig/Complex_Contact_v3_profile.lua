51eeae208cd4fce5b3339886f5638e30479efcc4
mechwarspring <zverofaust@users.noreply.github.com>
2011-02-10 18:30:00 +0000

    More beacon profiles.

---
+++ maps/flagConfig/Complex_Contact_v3_profile.lua
@@ -0,0 +1,29 @@
+local resources = {
+	{
+		x = 2560,
+		z = 3070,
+		feature = nil
+	},
+	{
+		x = 1245,
+		z = 4330,
+		feature = nil
+	},
+	{
+		x = 3880,
+		z = 4330,
+		feature = nil
+	},
+	{
+		x = 3880,
+		z = 1315,
+		feature = nil
+	},
+	{
+		x = 1245,
+		z = 1315,
+		feature = nil
+	},
+}
+
+return resources
