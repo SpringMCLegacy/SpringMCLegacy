e26adce52e37d7fd66fa44bed6f1dc21a2d45ce6
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-02-10 18:40:15 +0000

    Another map profile.

---
+++ maps/flagConfig/Eye_Of_Horus_v2_profile.lua
@@ -0,0 +1,19 @@
+local resources = {
+	{
+		x = 3080,
+		z = 3585,
+		feature = nil
+	},
+	{
+		x = 1390,
+		z = 3430,
+		feature = nil
+	},
+	{
+		x = 4875,
+		z = 3420,
+		feature = nil
+	},
+}
+
+return resources
