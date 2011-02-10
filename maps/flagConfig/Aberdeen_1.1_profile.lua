09f6cc892f278a7062f136c83e7853963d183f22
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-02-10 18:30:00 +0000

    More beacon profiles.

---
+++ maps/flagConfig/Aberdeen_1.1_profile.lua
@@ -0,0 +1,19 @@
+local resources = {
+	{
+		x = 4210,
+		z = 5770,
+		feature = nil
+	},
+	{
+		x = 2450,
+		z = 3000,
+		feature = nil
+	},
+	{
+		x = 6075,
+		z = 2675,
+		feature = nil
+	},
+}
+
+return resources
---
+++ maps/flagConfig/XantheTerra_v3_profile.lua
@@ -0,0 +1,19 @@
+local resources = {
+	{
+		x = 3660,
+		z = 3515,
+		feature = nil
+	},
+	{
+		x = 1375,
+		z = 3400,
+		feature = nil
+	},
+	{
+		x = 5750,
+		z = 3300,
+		feature = nil
+	},
+}
+
+return resources
