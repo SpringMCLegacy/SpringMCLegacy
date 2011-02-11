b626da37252b5089f0661c298f640b5d1232b501
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-02-11 20:56:23 +0000

    -Fixed mapconfig for Complex_Contact_v3 (misplaced beacons) -Added config for Apophis_v2, pretty cool asteroid map.

---
+++ maps/flagConfig/Apophis_v2_profile.lua
@@ -0,0 +1,19 @@
+local resources = {
+	{
+		x = 2100,
+		z = 3700,
+		feature = nil
+	},
+	{
+		x = 4050,
+		z = 4000,
+		feature = nil
+	},
+	{
+		x = 5900,
+		z = 4200,
+		feature = nil
+	},
+}
+
+return resources
