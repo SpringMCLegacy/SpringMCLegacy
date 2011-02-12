160ab91f658b3337cfd490a9e592ceed7ae736d2
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-02-12 06:18:34 +0000

    -Added Clan Ice Ferret (Fenris) Medium Skirmish Mech, gives them a bit more (very) fast hitting power.

---
+++ units/cl/CL_Iceferret.lua
@@ -0,0 +1,90 @@
+local CL_Iceferret = {
+	name              	= "Ice Ferret (Fenris)",
+	description         = "Medium Strike Mech",
+	objectName        	= "CL_Iceferret.s3o",
+	script				= "CL_Iceferret.lua",
+	category 			= "mech ground notbeacon",
+	sightDistance       = 1500,
+	radarDistance      	= 2000,
+		activateWhenBuilt   = true,
+		onoffable           = true,
+	maxDamage           = 7500,
+	mass                = 4500,
+	footprintX			= 2,
+	footprintZ 			= 2,
+	collisionVolumeType = "box",
+	collisionVolumeScales = "30 50 25",
+	collisionVolumeOffsets = "0 0 0",
+	collisionVolumeTest = 1,
+--	leaveTracks			= 1,
+--	trackOffset			= 10,--no idea what this does
+--	trackStrength		= 2.5,--how visible the tracks are
+--	trackStretch		= 1,-- how much the tracks stretch, the higher the number the more "compact" they become
+--	trackType			= "Thick",--graphics file to use for the track decal, from \bitmaps\tracks\ folder
+--	trackWidth			= 20,--width to render the decal
+	buildCostEnergy     = 0,
+	buildCostMetal      = 3500,
+	buildTime           = 0,
+	upright				= true,
+	canMove				= true,
+		movementClass   = "SMALLMECH",
+		maxVelocity		= 4.3, --130kph/30
+		maxReverseVelocity= 2.15,
+		acceleration    = 1.8,
+		brakeRate       = 0.1,
+		turnRate 		= 850,
+		smoothAnim		= 1,
+	
+	canAttack 			= true,
+		--Makes unit use weapon from /weapons folder
+		weapons 		= {	
+			[1] = {
+				name	= "CMPL",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+			},
+			[2] = {
+				name	= "CMPL",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+			},
+			[3] = {
+				name	= "CMPL",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[4] = {
+				name	= "CMPL",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[5] = {
+				name	= "SSRM6",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+		},
+		
+	--Gets CEG effects from /gamedata/explosions folder
+
+	sfxtypes = {
+		explosiongenerators = {
+		"custom:SMALL_MUZZLEFLASH",
+		"custom:MG_MUZZLEFLASH",
+		},
+	},
+    customparams = {
+		hasturnbutton	= "1",
+		helptext		= "Armament: 4 x Medium Pulse Laser, 1 x SSRM-6 - Armor: 7.5 tons",
+    },
+}
+
+return lowerkeys({ ["CL_Iceferret"] = CL_Iceferret })
