5fc3f27bfaf88ac58ea65d357dc136cd1d78c5b9
mechwarspring <zverofaust@users.noreply.github.com>
2011-02-12 06:33:51 +0000

    -Reclassing Bushwacker as a Strike (range) Mech.

---
+++ weapons/ERMBL.lua
@@ -0,0 +1,43 @@
+weaponDef = {
+	name                    = "Extended Range Medium Beam Laser",
+	weaponType              = "BeamLaser",
+	beamLaser				= true,
+	beamLaser				= 1,
+--	beamWeapon				= true,
+--	largeBeamLaser			= true,
+	renderType				= 0,
+	lineOfSight				= true,
+	explosionGenerator    	= "custom:AP_SMALL",
+	soundHit              	= [[GEN_Beam_Explode1]],
+	soundStart           	= [[MBL_Fire]],
+	burnblow				= true, 	--Bullets explode at range limit.
+	collideFriendly			= true,
+	noSelfDamage            = true,
+	turret                  = true,
+	range                   = 1200,
+	accuracy                = 100,
+	tollerance				= 100,
+	areaOfEffect            = 5,
+	weaponVelocity          = 2000,
+	weaponTimer				= 0.8,
+	reloadtime              = 3,
+	size 					= 0.05,
+	laserFlareSize			= 0.1,
+	thickness				= 1.0,
+	coreThickness			= 0.3,
+	beamDecay          		= 1,
+	beamTime           		= 0.75,
+	beamTTL           		= 0.75,
+	minIntensity			= 1,
+	rgbcolor				= "0.4 1.0 0.4",
+	intensity				= 0.75,
+	damage = {
+		default = 150, --50 DPS
+		beacons = 0,
+	},
+	customparams = {
+		heatgenerated		= "9",--3/sec
+    },
+}
+
+return lowerkeys({ ERMBL = weaponDef })
