ee731e02df6093aac478af514832480220df74df
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-02-20 20:59:03 +0000

    Beacon support for Valles_Marineris_v2

---
+++ maps/flagConfig/Valles_Marineris_v2_profile.lua
@@ -0,0 +1,29 @@
+local resources = {
+	{
+		x = 4082,
+		z = 2742,
+		feature = nil
+	},
+	{
+		x = 1160,
+		z = 2600,
+		feature = nil
+	},
+	{
+		x = 6982,
+		z = 2600,
+		feature = nil
+	},
+	{
+		x = 4093,
+		z = 804,
+		feature = nil
+	},
+	{
+		x = 4093,
+		z = 5151,
+		feature = nil
+	},
+}
+
+return resources
