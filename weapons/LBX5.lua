b041ae5b565f364a7154a61704d888c356492314
mechwarspring <zverofaust@users.noreply.github.com>
2011-02-22 15:37:18 +0000

    LBX-5 weapon.

---
+++ weapons/LBX5.lua
@@ -0,0 +1,44 @@
+weaponDef = {
+	name                    = "LBX/5 AutoCannon",
+	weaponType              = "Cannon",
+	explosionGenerator    	= "custom:Bullet",
+	soundHit              	= [[GEN_Explode1]],
+	soundStart            	= [[LBX10_Fire]],
+	soundTrigger			= 1,
+	burnblow				= false, 	--Bullets explode at range limit.
+	collideFriendly			= true,
+	noSelfDamage            = true,
+	turret                  = true,
+	ballistic				= 1,
+	range                   = 1800,
+	accuracy                = 100,
+	areaOfEffect            = 1,
+	weaponVelocity          = 1750,
+	reloadtime              = 1,
+	burst					= 5,
+	burstrate				= 0.00001,
+	sprayAngle				= 200,
+	projectiles				= 1,
+	renderType				= 1,
+	size					= 0.5,
+	sizeDecay				= 0,
+	separation				= 2, 		--Distance between each plasma particle.
+	stages					= 50, 		--Number of particles used in one plasma shot.
+--	AlphaDecay				= 0.05, 		--How much a plasma particle is more transparent than the previous particle. 
+	rgbcolor				= "1 0.8 0",
+	intensity				= 0.5,
+	damage = {
+		default = 10, --100 DPS
+		beacons = 0,
+		light = 10,
+		medium = 8.5,
+		heavy = 7,
+		assault = 5,
+		vehicle = 15,
+	},
+	customparams = {
+		heatgenerated		= "3",--2/sec
+    },
+}
+
+return lowerkeys({ LBX5 = weaponDef })
