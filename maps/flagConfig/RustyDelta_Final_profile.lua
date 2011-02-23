8556d8dd11989f3970382f2c1dc594e211a9d222
mechwarspring <zverofaust@users.noreply.github.com>
2011-02-23 11:34:07 +0000

    Wolfhound size tweak Beacon profile for RustyDelta_Final

---
+++ maps/flagConfig/RustyDelta_Final_profile.lua
@@ -0,0 +1,34 @@
+local resources = {
+	{
+		x = 5156,
+		z = 2053,
+		feature = nil
+	},
+	{
+		x = 5096,
+		z = 301,
+		feature = nil
+	},
+	{
+		x = 3821,
+		z = 3842,
+		feature = nil
+	},
+	{
+		x = 6399,
+		z = 3842,
+		feature = nil
+	},
+	{
+		x = 2368,
+		z = 899,
+		feature = nil
+	},
+	{
+		x = 7873,
+		z = 899,
+		feature = nil
+	},
+}
+
+return resources
