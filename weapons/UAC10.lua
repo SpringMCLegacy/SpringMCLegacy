06ae1ba1faa32b3b5ca21a8cebb1e8ada005beb2
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-03-05 21:57:53 +0000

    Changed Ultra AC weapons to have "proper" rates of fire, 2 x original ACs, so 2 x the DPS.

---
+++ weapons/UAC10.lua
@@ -0,0 +1,39 @@
+weaponDef = {
+	name                    = "Ultra AutoCannon/10",
+	weaponType              = "Cannon",
+	explosionGenerator    	= "custom:HE_MEDIUM",
+	soundHit              	= [[GEN_Explode3]],
+	soundStart            	= [[AC10_Fire]],
+	burnblow				= false, 	--Bullets explode at range limit.
+	collideFriendly			= true,
+	noSelfDamage            = true,
+	turret                  = true,
+	ballistic				= 1,
+	range                   = 1500,
+	accuracy                = 25,
+	areaOfEffect            = 25,
+	weaponVelocity          = 1500,
+	reloadtime              = 0.75,
+	renderType				= 1,
+	size					= 1.5,
+	sizeDecay				= 0,
+	separation				= 2, 		--Distance between each plasma particle.
+	stages					= 50, 		--Number of particles used in one plasma shot.
+--	AlphaDecay				= 0.05, 		--How much a plasma particle is more transparent than the previous particle. 
+	rgbcolor				= "1 0.8 0",
+	intensity				= 0.1,
+	damage = {
+		default = 150, --200 DPS, 2 x ROF
+		beacons = 0,
+		light = 150,
+		medium = 127.5,
+		heavy = 105,
+		assault = 75,
+		vehicle = 225,
+	},
+	customparams = {
+		heatgenerated		= "2",--3/sec
+    },
+}
+
+return lowerkeys({ UAC10 = weaponDef })
