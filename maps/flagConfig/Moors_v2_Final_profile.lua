38263332c23081c287d66671a21a3f1ec6295dc0
mechwarspring <zverofaust@users.noreply.github.com>
2011-03-26 19:09:32 +0000

    Moors profile

---
+++ maps/flagConfig/Moors_v2_Final_profile.lua
@@ -0,0 +1,34 @@
+local resources = {
+	{
+		x = 5184,
+		z = 6338,
+		feature = nil
+	},
+	{
+		x = 7162,
+		z = 6147,
+		feature = nil
+	},
+	{
+		x = 6112,
+		z = 3225,
+		feature = nil
+	},
+	{
+		x = 5881,
+		z = 9472,
+		feature = nil
+	},
+	{
+		x = 2377,
+		z = 5971,
+		feature = nil
+	},
+	{
+		x = 9697,
+		z = 5701,
+		feature = nil
+	},
+}
+
+return resources
