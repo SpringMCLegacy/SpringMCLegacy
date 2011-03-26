7fca83ec4a5eb90c4bf80c53e7f406c6e7f62999
mechwarspring <zverofaust@users.noreply.github.com>
2011-03-26 15:56:09 +0000

    Inner Sphere Devestator Assault-class Sniper Mech

---
+++ units/is/IS_Devastator.lua
@@ -0,0 +1,102 @@
+local IS_Devastator = {
+	name              	= "Devastator",
+	description         = "Assault-class Brawler Mech",
+	objectName        	= "IS_Devastator.s3o",
+	script				= "Mech.lua",
+	corpse				= "IS_Devastator_X",
+	explodeAs          	= "mechexplode",
+	category 			= "mech ground notbeacon",
+	sightDistance       = 800,
+	radarDistance      	= 1500,
+		activateWhenBuilt   = true,
+		onoffable           = true,
+	maxDamage           = 29600,
+	mass                = 10000,
+	footprintX			= 3,
+	footprintZ 			= 3,
+	collisionVolumeType = "box",
+	collisionVolumeScales = "40 60 40",
+	collisionVolumeOffsets = "0 0 0",
+	collisionVolumeTest = 1,
+--	leaveTracks			= 1,
+--	trackOffset			= 10,--no idea what this does
+--	trackStrength		= 2.5,--how visible the tracks are
+--	trackStretch		= 1,-- how much the tracks stretch, the higher the number the more "compact" they become
+--	trackType			= "Thick",--graphics file to use for the track decal, from \bitmaps\tracks\ folder
+--	trackWidth			= 20,--width to render the decal
+	buildCostEnergy     = 100, -- in tons
+	buildCostMetal      = 36100,
+	buildTime           = 0,
+	upright				= true,
+	canMove				= true,
+		movementClass   = "LARGEMECH",
+		maxVelocity		= 2.7, --54kph/30
+		maxReverseVelocity= 1.63,
+		acceleration    = .80,
+		brakeRate       = 0.1,
+		turnRate 		= 600,
+		smoothAnim		= 1,
+	
+	canAttack 			= true,
+		--Makes unit use weapon from /weapons folder
+		weapons 		= {	
+			[1] = {
+				name	= "Gauss",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+			},
+			[2] = {
+				name	= "Gauss",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[3] = {
+				name	= "PPC",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[4] = {
+				name	= "PPC",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[5] = {
+				name	= "ERMBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[6] = {
+				name	= "ERMBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[7] = {
+				name	= "ERMBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+		},
+		
+	--Gets CEG effects from /gamedata/explosions folder
+    customparams = {
+		hasturnbutton	= "1",
+		helptext		= "Armament: 2 x Gauss Rifle, 2 x PPC, 3 x Medium Beam Laser - Armor: 19 tons Standard",
+		heatlimit		= "34",
+		torsoturnspeed	= "100",
+    },
+}
+
+return lowerkeys({ ["IS_Devastator"] = IS_Devastator })
