c5eec913262421e6e11428660670aefee9f77c99
FLOZi <FLOZi@users.noreply.github.com>
2011-04-11 18:24:05 +0000

    * Fix error Gamedata/unitdefs_post.lua relating to speed modifier modoption

--- Gamedata/unitdefs_post.lua
+++ Gamedata/unitdefs_post.lua
@@ -13,5 +13,5 @@ for name, ud in pairs(UnitDefs) do
 			ud.buildtime = RAMP_DISTANCE / speed
 		end
 	end
-	ud.maxvelocity = ud.maxvelocity * (modOptions.speed or 1)
+	ud.maxvelocity = (ud.maxvelocity or 0) * (modOptions.speed or 1)
 end
