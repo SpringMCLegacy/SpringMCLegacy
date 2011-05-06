c22997dd22094d2e5cd1d94ad5469d81cad04797
mechwarspring <zverofaust@users.noreply.github.com>
2011-05-06 20:57:50 +0000

    Partisan Lua

---
+++ units/is/Vehicles/IS_Partisan.lua
@@ -0,0 +1,73 @@
+local IS_Partisan = {
+	name              	= "Partisan",
+	description         = "Heavy Anti-Aircraft Tank",
+	objectName        	= "IS_Partisan.s3o",
+	script				= "Vehicle.lua",
+	corpse				= "IS_Partisan_X",
+	explodeAs          	= "mechexplode",
+	category 			= "tank ground notbeacon",
+	sightDistance       = 800,
+	radarDistance      	= 1500,
+		activateWhenBuilt   = true,
+		onoffable           = true,
+	maxDamage           = 11200,
+	mass                = 8000,
+	footprintX			= 3,
+	footprintZ 			= 3,
+	collisionVolumeType = "box",
+	collisionVolumeScales = "30 20 40",
+	collisionVolumeOffsets = "0 5 0",
+	collisionVolumeTest = 1,
+	leaveTracks			= 1,
+	trackOffset			= 10,--no idea what this does
+	trackStrength		= 9,--how visible the tracks are
+	trackStretch		= 1,-- how much the tracks stretch, the higher the number the more "compact" they become
+	trackType			= "Thick",--graphics file to use for the track decal, from \bitmaps\tracks\ folder
+	trackWidth			= 32,--width to render the decal
+	buildCostEnergy     = 70,
+	buildCostMetal      = 5000,
+	buildTime           = 0,
+	canMove				= true,
+		movementClass   = "TANK",
+		maxVelocity		= 1.8, --54kph/30
+		maxReverseVelocity= 1.3,
+		acceleration    = 0.6,
+		brakeRate       = 0.1,
+		turnRate 		= 425,
+	
+	canAttack 			= true,
+		--Makes unit use weapon from /weapons folder
+		weapons 		= {	
+			[1] = {
+				name	= "AC5",
+				OnlyTargetCategory = "notbeacon",
+			},
+			[2] = {
+				name	= "AC5",
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[3] = {
+				name	= "AC5",
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+			[4] = {
+				name	= "AC5",
+				OnlyTargetCategory = "notbeacon",
+				WeaponSlaveTo = 1,
+			},
+		},
+	--Gets CEG effects from /gamedata/explosions folder
+	customparams = {
+		hasturnbutton	= "1",
+		helptext		= "Armament: 4 x AC/5 (Anti-Aircraft) - Armor: 7 tons",
+		heatlimit		= "20",
+		unittype		= "vehicle",
+		turretturnspeed = "120",
+		elevationspeed  = "300",
+		wheelspeed      = "200",
+	},
+}
+
+return lowerkeys({ ["IS_Partisan"] = IS_Partisan })
