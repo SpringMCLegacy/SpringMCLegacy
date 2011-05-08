d52585e4d22ddbc5dcd5e4cf32cfcec7ca3db528
mechwarspring <zverofaust@users.noreply.github.com>
2011-05-08 18:59:22 +0000

    Some changes to which types of outposts/beacons a couple of maps spawn.

--- maps/flagConfig/RustyDelta_Final_profile.lua
+++ maps/flagConfig/RustyDelta_Final_profile.lua
@@ -1,20 +1,5 @@
 local resources = {
 	{
-		x = 5096,
-		z = 301,
-		feature = nil
-	},
-	{
-		x = 2368,
-		z = 899,
-		feature = nil
-	},
-	{
-		x = 7873,
-		z = 899,
-		feature = nil
-	},
-	{
 		x = 3821,
 		z = 3842,
 		feature = nil
@@ -30,7 +15,22 @@ local outposts = {
 	{
 		x = 5156,
 		z = 2053,
-		types = {"outpost_listeningpost", "outpost_vehicledepot", "outpost_c3center", "outpost_garrison"},
+		types = {"outpost_vehicledepot", "outpost_c3center", "outpost_garrison"},
+	},
+		{
+		x = 5096,
+		z = 301,
+		types = {"outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 2368,
+		z = 899,
+		types = {"outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 7873,
+		z = 899,
+		types = {"outpost_listeningpost", "outpost_garrison"},
 	},
 }
 
