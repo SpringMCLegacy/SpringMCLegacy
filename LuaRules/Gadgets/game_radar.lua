7a4ee09ddf90fe1ccaf18a0b0d9e7a7e72c2291e
FLOZi <FLOZi@ef3f1c50-5774-440b-a771-62f173def817>
2011-05-14 12:32:52 +0000

    * Fix radar issues wrt capping beacons

--- LuaRules/Gadgets/game_radar.lua
+++ LuaRules/Gadgets/game_radar.lua
@@ -32,6 +32,16 @@ local NARC_DURATION = 32 * 60 -- 30 seconds
 local modOptions = Spring.GetModOptions()
 local inRadarUnits = {}
 local outRadarUnits = {}
+
+local allyTeams = Spring.GetAllyTeamList()
+local numAllyTeams = #allyTeams
+
+for i = 1, numAllyTeams do
+	local allyTeam = allyTeams[i]
+	inRadarUnits[allyTeam] = {}
+	outRadarUnits[allyTeam] = {}
+end
+
 local narcUnits = {}
 
 local function NARC(unitID, allyTeam)
@@ -53,13 +63,15 @@ local function DeNARC(unitID, allyTeam)
 end
 
 function gadget:UnitEnteredRadar(unitID, unitTeam, allyTeam, unitDefID)
-	inRadarUnits[unitID] = allyTeam
-	outRadarUnits[unitID] = nil
+	--Spring.Echo(UnitDefs[unitDefID].name .. " entered radar " .. allyTeam)
+	inRadarUnits[allyTeam][unitID] = true
+	outRadarUnits[allyTeam][unitID] = nil
 end
 
 function gadget:UnitLeftRadar(unitID, unitTeam, allyTeam, unitDefID)
-	outRadarUnits[unitID] = allyTeam
-	inRadarUnits[unitID] = nil
+	--Spring.Echo(UnitDefs[unitDefID].name .. " left radar" .. allyTeam)
+	outRadarUnits[allyTeam][unitID] = true
+	inRadarUnits[allyTeam][unitID] = nil
 end
 
 function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponID, attackerID, attackerDefID, attackerTeam)
@@ -74,17 +86,20 @@ function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, w
 end
 
 function gadget:GameFrame(n)
-	for unitID, allyTeam in pairs(inRadarUnits) do
-		if not narcUnits[unitID] then
-			SetUnitLosState(unitID, allyTeam, {los=true, prevLos=true, radar=true, contRadar=true} ) 
-			SetUnitLosMask(unitID, allyTeam, {los=true, prevLos=false, radar=false, contRadar=false} )	
-			inRadarUnits[unitID] = nil
+	for i = 1, numAllyTeams do
+		local allyTeam = allyTeams[i]
+		for unitID in pairs(inRadarUnits[allyTeam]) do
+			if not narcUnits[unitID] then
+				SetUnitLosState(unitID, allyTeam, {los=true, prevLos=true, radar=true, contRadar=true} ) 
+				SetUnitLosMask(unitID, allyTeam, {los=true, prevLos=false, radar=false, contRadar=false} )	
+				inRadarUnits[allyTeam][unitID] = nil
+			end
 		end
-	end
-	for unitID, allyTeam in pairs(outRadarUnits) do
-		if not narcUnits[unitID] then
-			SetUnitLosMask(unitID, allyTeam, {los=false, prevLos=false, radar=false, contRadar=false} )
-			outRadarUnits[unitID] = nil
+		for unitID in pairs(outRadarUnits[allyTeam]) do
+			if not narcUnits[unitID] then
+				SetUnitLosMask(unitID, allyTeam, {los=false, prevLos=false, radar=false, contRadar=false} )
+				outRadarUnits[allyTeam][unitID] = nil
+			end
 		end
 	end
 end
