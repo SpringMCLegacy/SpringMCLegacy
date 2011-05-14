ff324919bf90ae7629495e4b0938c60eb517bdf4
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-05-14 23:53:29 +0000

    Ugh gave Donar the wrong weapon

--- units/cl/Vehicles/CL_Donar.lua
+++ units/cl/Vehicles/CL_Donar.lua
@@ -47,14 +47,14 @@ local CL_Donar = {
 				maxAngleDif = 90,
 			},
 			[2] = {
-				name	= "SRM4",
+				name	= "SSRM4",
 				OnlyTargetCategory = "notbeacon",
 				WeaponSlaveTo = 1,
 				mainDir = "0 0 1",
 				maxAngleDif = 90,
 			},
 			[3] = {
-				name	= "SRM4",
+				name	= "SSRM4",
 				OnlyTargetCategory = "notbeacon",
 				WeaponSlaveTo = 1,
 				mainDir = "0 0 1",
