d236b8924f46d0d0877c7c50d45104648a49f441
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2011-05-15 18:02:51 +0000

    Added config for Mercury_v1.

---
+++ maps/flagConfig/Mercury_v1_profile.lua
@@ -0,0 +1,37 @@
+local resources = {	
+}
+
+local outposts = {
+	{
+		x = 7210,--top right
+		z = 1138,
+		types = {"outpost_vehicledepot", "outpost_controltower"},
+	},
+	{
+		x = 968,--bottom left
+		z = 7008,
+		types = {"outpost_vehicledepot", "outpost_controltower"},
+	},
+	{
+		x = 3955,--mid top
+		z = 2395,
+		types = {"beacon", "outpost_vehicledepot", "outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 6053,--mid-top
+		z = 4433,
+		types = {"beacon", "outpost_vehicledepot", "outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 4282,--mid bottom
+		z = 5923,
+		types = {"beacon", "outpost_vehicledepot", "outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 2394,-- mid left
+		z = 3902,
+		types = {"beacon", "outpost_vehicledepot", "outpost_listeningpost", "outpost_garrison"},
+	},
+}
+
+return resources, outposts
