206965c35bbd3ca443c2c73a9c6fbda5e1687918
FLOZi <FLOZi@ef3f1c50-5774-440b-a771-62f173def817>
2011-12-24 16:04:33 +0000

    * Make Aeros exiting the Dropship equally awesome

--- Gamedata/unitdefs_post.lua
+++ Gamedata/unitdefs_post.lua
@@ -4,13 +4,16 @@ if (Spring.GetModOptions) then
 end
 
 local RAMP_DISTANCE = 156 -- 206
+local HANGAR_DISTANCE = 256
 
 for name, ud in pairs(UnitDefs) do
 	ud.shownanoframe = false
-	if ud.customparams.unittype == "mech" then
-		local speed = (ud.maxvelocity or 0) * 30
-		if speed > 0 then
+	local speed = (ud.maxvelocity or 0) * 30
+	if speed > 0 then
+		if ud.customparams.unittype == "mech" then
 			ud.buildtime = RAMP_DISTANCE / speed
+		elseif ud.canfly then
+			ud.buildtime = HANGAR_DISTANCE / (speed * 0.5)
 		end
 	end
 	ud.maxvelocity = (ud.maxvelocity or 0) * (modOptions.speed or 1)
