3a41887d7785d5e2ec8b4c9403938eea247e1aa2
FLOZi <FLOZi@users.noreply.github.com>
2011-12-24 21:19:27 +0000

    * Update modinfo.lua to officially name us "BattleTech: Legacy"

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,13 @@
 local modinfo = {
-		name					=	"MW:S",
-		shortName				= "MWS",
-		game					= "MW:S",
-		shortGame				= "MWS",
+		name					= "BattleTech: Legends",
+		shortName				= "BTL",
+		game					= "BattleTech: Legends",
+		shortGame				= "BTL",
 		mutator					= "Official",
-		description				=	"War is hell.",
-		url						=	"http://www.google.com/",
-		modtype					=	"1",
+		description				= "War is hell.",
+		url						= "http://springrts.com/phpbb/viewforum.php?f=70",
+		modtype					= "1",
+		version					= "0.1",
 }
 
 return modinfo
