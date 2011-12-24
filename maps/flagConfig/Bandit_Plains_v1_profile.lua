e469a41aea6fd909e261e0ad545c6c985e8245ce
mechwarspring <zverofaust@users.noreply.github.com>
2011-12-24 21:34:05 +0000

    Map config for Bandit_Plains_v1.

---
+++ maps/flagConfig/Bandit_Plains_v1_profile.lua
@@ -0,0 +1,22 @@
+local resources = {	
+}
+
+local outposts = {
+	{
+		x = 1370,
+		z = 4000,
+		types = {"outpost_vehicledepot", "outpost_controltower", "outpost_garrison"},
+	},
+	{
+		x = 4000,
+		z = 4250,
+		types = {"beacon", "outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 6800,
+		z = 4200,
+		types = {"outpost_vehicledepot", "outpost_controltower", "outpost_garrison"},
+	},
+}
+
+return resources, outposts
