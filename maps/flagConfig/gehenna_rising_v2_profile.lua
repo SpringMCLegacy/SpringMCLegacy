c9dafadd0d5191ee0d4850ebd3e4340b4a06b7b9
mechwarspring <zverofaust@users.noreply.github.com>
2011-12-25 00:07:33 +0000

    -Map profiles for Gehanna Rising (which doesnt work) and Gantelope.

---
+++ maps/flagConfig/gehenna_rising_v2_profile.lua
@@ -0,0 +1,32 @@
+local resources = {	
+}
+
+local outposts = {
+	{
+		x = 4125,--Center
+		z = 4150,
+		types = {"outpost_vehicledepot", "outpost_controltower"},
+	},
+	{
+		x = 4080,--Top
+		z = 1530,
+		types = {"beacon", "outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 2060,--Left
+		z = 4100,
+		types = {"beacon", "outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 4100,--Bottom
+		z = 6570,
+		types = {"beacon", "outpost_listeningpost", "outpost_garrison"},
+	},
+	{
+		x = 6130,--Right
+		z = 4100,
+		types = {"beacon", "outpost_listeningpost", "outpost_garrison"},
+	},
+}
+
+return resources, outposts
