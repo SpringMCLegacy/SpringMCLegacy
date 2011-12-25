592aacb62b352d33309fcd12e7aabdbdcccd8976
FLOZi <FLOZi@users.noreply.github.com>
2011-12-25 01:13:24 +0000

    * Legends -> Legacy * Fix debug echos in Gadgets/game_flagManager.lua

--- modinfo.lua
+++ modinfo.lua
@@ -1,13 +1,13 @@
 local modinfo = {
-		name					= "BattleTech: Legends",
+		name					= "BattleTech: Legacy",
 		shortName				= "BTL",
-		game					= "BattleTech: Legends",
+		game					= "BattleTech: Legacy",
 		shortGame				= "BTL",
 		mutator					= "Official",
 		description				= "War is hell.",
 		url						= "http://springrts.com/phpbb/viewforum.php?f=70",
 		modtype					= "1",
-		version					= "0.1",
+		version					= "0.1.svn", -- change to rev before release
 }
 
 return modinfo
