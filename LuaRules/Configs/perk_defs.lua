1203a345bffcfa637fb4061cbc966fc930620609
FLOZi <FLOZi@ef3f1c50-5774-440b-a771-62f173def817>
2013-04-03 12:36:47 +0000

    * Cache results of valid() checks per unitDefID

--- LuaRules/Configs/perk_defs.lua
+++ LuaRules/Configs/perk_defs.lua
@@ -90,8 +90,10 @@ return {
 			--Spring.Echo("Sensor range selected") 
 			local currRadar = Spring.GetUnitSensorRadius(unitID, "radar")
 			local currLos = Spring.GetUnitSensorRadius(unitID, "los")
+			local currAirLos = Spring.GetUnitSensorRadius(unitID, "airlos")
 			Spring.SetUnitSensorRadius(unitID, "radar", currRadar * 1.5)
 			Spring.SetUnitSensorRadius(unitID, "los", currLos * 1.5)
+			Spring.SetUnitSensorRadius(unitID, "airlos", currAirLos * 1.5)
 		end,
 	},
 	narcduration = {
