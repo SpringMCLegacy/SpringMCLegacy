8bbbf28e9f2a3cff21ae8b6cc792bdbe992b5987
mechwarspring <zverofaust@users.noreply.github.com>
2013-04-05 12:24:54 +0000

    -Added Mech Night Hawk NTK-2Q; -Removed Orion bits;

--- units/is/Mechs/IS_Nighthawk_NTK2Q.lua
+++ units/is/Mechs/IS_Nighthawk_NTK2Q.lua
@@ -1,23 +1,21 @@
-local IS_Orion = {
-	name              	= "Orion",
-	description         = "Heavy Brawler Mech",
-	objectName        	= "IS_Orion.s3o",
-	iconType			= "heavymech",
+local IS_Nighthawk_NTK2Q = {
+	name              	= "Night Hawk NTK-2Q",
+	description         = "Light Sniper Mech",
+	objectName        	= "IS_Nighthawk_NTK2Q.s3o",
+	iconType			= "lightmech",
 	script				= "Mech.lua",
-	corpse				= "IS_Orion_X",
+	corpse				= "IS_Nighthawk_X",
 	explodeAs          	= "mechexplode",
 	category 			= "mech ground notbeacon",
 	noChaseCategory		= "beacon air",
-	sightDistance       = 800,
-	radarDistance      	= 1500,
 		activateWhenBuilt   = true,
 		onoffable           = true,
-	maxDamage           = 22400,
-	mass                = 7500,
-	footprintX			= 3,
-	footprintZ 			= 3,
+	maxDamage           = 11200,
+	mass                = 3500,
+	footprintX			= 2,
+	footprintZ 			= 2,
 	collisionVolumeType = "box",
-	collisionVolumeScales = "45 50 40",
+	collisionVolumeScales = "30 35 15",
 	collisionVolumeOffsets = "0 0 0",
 	collisionVolumeTest = 1,
 --	leaveTracks			= 1,
@@ -26,53 +24,35 @@ local IS_Orion = {
 --	trackStretch		= 1,-- how much the tracks stretch, the higher the number the more "compact" they become
 --	trackType			= "Thick",--graphics file to use for the track decal, from \bitmaps\tracks\ folder
 --	trackWidth			= 20,--width to render the decal
-	buildCostEnergy     = 75,
-	buildCostMetal      = 28600,
+	buildCostEnergy     = 35,
+	buildCostMetal      = 15000,
 	buildTime           = 0,
 	upright				= true,
 	canMove				= true,
-		movementClass   = "LARGEMECH",
-		maxVelocity		= 3.25, --65kph/3
-		maxReverseVelocity= 1.63,
-		acceleration    = 1,
-		brakeRate       = 0.2,
-		turnRate 		= 650,
+		movementClass   = "SMALLMECH",
+		maxVelocity		= 4.5, --90kph/20
 		smoothAnim		= 1,
 	
 	canAttack 			= true,
 		--Makes unit use weapon from /weapons folder
 		weapons 		= {	
 			[1] = {
-				name	= "LPL",
+				name	= "ERLBL",
 				mainDir = "0 0 1",
-				maxAngleDif = 240,
+				maxAngleDif = 220,
 				OnlyTargetCategory = "notbeacon",
 			},
 			[2] = {
-				name	= "MPL",
+				name	= "LBL",
 				mainDir = "0 0 1",
-				maxAngleDif = 240,
+				maxAngleDif = 220,
 				OnlyTargetCategory = "notbeacon",
 				SlaveTo = 1,
 			},
 			[3] = {
-				name	= "AC20",
+				name	= "MPL",
 				mainDir = "0 0 1",
-				maxAngleDif = 240,
-				OnlyTargetCategory = "notbeacon",
-				SlaveTo = 1,
-			},
-			[4] = {
-				name	= "SRM6",
-				mainDir = "0 0 1",
-				maxAngleDif = 240,
-				OnlyTargetCategory = "notbeacon",
-				SlaveTo = 1,
-			},
-			[5] = {
-				name	= "SRM6",
-				mainDir = "0 0 1",
-				maxAngleDif = 240,
+				maxAngleDif = 220,
 				OnlyTargetCategory = "notbeacon",
 				SlaveTo = 1,
 			},
@@ -82,18 +62,17 @@ local IS_Orion = {
 
 	--[[sfxtypes = {
 		explosiongenerators = {
-		"custom:PPC_MUZZLEFLASH",
 		"custom:MISSILE_MUZZLEFLASH",
 		"custom:LASER_MUZZLEFLASH",
 		},
 	},]]
     customparams = {
 		hasturnbutton	= "1",
-		helptext		= "Armament: 1 x AC/20, 1 x Large Pulse Laser, 1 x Medium Pulse Laser, 2 x SRM-6 - Armor: 12.5 tons Standard",
-		heatlimit		= "29",
-		torsoturnspeed	= "120",
+		helptext		= "Armament: 1 x ER Large Beam Laser, 1 x Large Beam Laser, 1 x Medium Pulse Laser - Armor: 7.5 tons Stabdard",
+		heatlimit 		= "22",
+		torsoturnspeed	= "180",
 		unittype		= "mech",
     },
 }
 
-return lowerkeys({ ["IS_Orion"] = IS_Orion })
+return lowerkeys({ ["IS_Nighthawk_NTK2Q"] = IS_Nighthawk_NTK2Q })
