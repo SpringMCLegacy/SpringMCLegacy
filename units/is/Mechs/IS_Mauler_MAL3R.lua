ad3d79f629d283d964bd83f9eb98a04230c83bc1
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2013-04-06 13:24:33 +0000

    -Added Mauler MAL-3R variant -Removed Wolfhound files

--- units/is/Mechs/IS_Mauler_MAL3R.lua
+++ units/is/Mechs/IS_Mauler_MAL3R.lua
@@ -1,23 +1,19 @@
-local IS_Warhammer = {
-	name              	= "Warhammer",
-	description         = "Heavy Strike Mech",
-	objectName        	= "IS_Warhammer.s3o",
-	iconType			= "heavymech",
+local IS_Mauler_MAL3R = {
+	name              	= "Mauler MAL-3R",
+	description         = "Assault-class Brawler Mech",
+	objectName        	= "IS_Mauler_MAL3R.s3o",
 	script				= "Mech.lua",
-	corpse				= "IS_Warhammer_X",
+	corpse				= "IS_Mauler_X",
 	explodeAs          	= "mechexplode",
 	category 			= "mech ground notbeacon",
-	noChaseCategory		= "beacon air",
-	sightDistance       = 800,
-	radarDistance      	= 1500,
 		activateWhenBuilt   = true,
 		onoffable           = true,
-	maxDamage           = 21600,
-	mass                = 7000,
+	maxDamage           = 27900,
+	mass                = 9000,
 	footprintX			= 3,
 	footprintZ 			= 3,
 	collisionVolumeType = "box",
-	collisionVolumeScales = "45 50 40",
+	collisionVolumeScales = "40 60 40",
 	collisionVolumeOffsets = "0 0 0",
 	collisionVolumeTest = 1,
 --	leaveTracks			= 1,
@@ -26,81 +22,49 @@ local IS_Warhammer = {
 --	trackStretch		= 1,-- how much the tracks stretch, the higher the number the more "compact" they become
 --	trackType			= "Thick",--graphics file to use for the track decal, from \bitmaps\tracks\ folder
 --	trackWidth			= 20,--width to render the decal
-	buildCostEnergy     = 70,
-	buildCostMetal      = 26300,
+	buildCostEnergy     = 90,
+	buildCostMetal      = 23500,
 	buildTime           = 0,
 	upright				= true,
 	canMove				= true,
 		movementClass   = "LARGEMECH",
-		maxVelocity		= 3.25, --65kph/3
-		maxReverseVelocity= 1.63,
-		acceleration    = 1,
-		brakeRate       = 0.2,
-		turnRate 		= 650,
+		maxVelocity		= 2.5, --50kph/20
 		smoothAnim		= 1,
 	
 	canAttack 			= true,
 		--Makes unit use weapon from /weapons folder
 		weapons 		= {	
 			[1] = {
-				name	= "PPC",
+				name	= "LBX10",
 				mainDir = "0 0 1",
-				maxAngleDif = 220,
+				maxAngleDif = 270,
 				OnlyTargetCategory = "notbeacon",
 			},
 			[2] = {
-				name	= "PPC",
+				name	= "LBX10",
 				mainDir = "0 0 1",
-				maxAngleDif = 220,
+				maxAngleDif = 270,
 				OnlyTargetCategory = "notbeacon",
 				SlaveTo = 1,
 			},
 			[3] = {
-				name	= "MBL",
+				name	= "SBL",
 				mainDir = "0 0 1",
-				maxAngleDif = 220,
+				maxAngleDif = 270,
 				OnlyTargetCategory = "notbeacon",
 				SlaveTo = 1,
 			},
 			[4] = {
-				name	= "MBL",
+				name	= "LRM15",
 				mainDir = "0 0 1",
-				maxAngleDif = 220,
+				maxAngleDif = 270,
 				OnlyTargetCategory = "notbeacon",
 				SlaveTo = 1,
 			},
 			[5] = {
-				name	= "ERSBL",
+				name	= "LRM15",
 				mainDir = "0 0 1",
-				maxAngleDif = 220,
-				OnlyTargetCategory = "notbeacon",
-				SlaveTo = 1,
-			},
-			[6] = {
-				name	= "ERSBL",
-				mainDir = "0 0 1",
-				maxAngleDif = 220,
-				OnlyTargetCategory = "notbeacon",
-				SlaveTo = 1,
-			},
-			[7] = {
-				name	= "MG",
-				mainDir = "0 0 1",
-				maxAngleDif = 220,
-				OnlyTargetCategory = "notbeacon",
-				SlaveTo = 1,
-			},
-			[8] = {
-				name	= "MG",
-				mainDir = "0 0 1",
-				maxAngleDif = 220,
-				OnlyTargetCategory = "notbeacon",
-				SlaveTo = 1,
-			},
-			[9] = {
-				name	= "SRM6",
-				mainDir = "0 0 1",
-				maxAngleDif = 220,
+				maxAngleDif = 270,
 				OnlyTargetCategory = "notbeacon",
 				SlaveTo = 1,
 			},
@@ -110,18 +74,18 @@ local IS_Warhammer = {
 
 	--[[sfxtypes = {
 		explosiongenerators = {
-		"custom:PPC_MUZZLEFLASH",
 		"custom:MISSILE_MUZZLEFLASH",
+		"custom:AC2_MUZZLEFLASH",
 		"custom:LASER_MUZZLEFLASH",
 		},
 	},]]
     customparams = {
 		hasturnbutton	= "1",
-		helptext		= "Armament: 2 x PPC, 2 x Medium Beam Laser, 2 x Small Beam Laser, 2 x MG, 1 x LRM-10 - Armor: 10 tons Standard",
-		heatlimit		= "34",
+		helptext		= "Armament: 2 x LBX/10, 1 x Small Beam Laser, 2 x LRM-15 - Armor: 16 tons Ferro-Fibrous",
+		heatlimit		= "20",
 		torsoturnspeed	= "120",
-		unittype		= "mech",
+		maxammo 		= {lrm = 720, ac10 = 60},
     },
 }
 
-return lowerkeys({ ["IS_Warhammer"] = IS_Warhammer })
+return lowerkeys({ ["IS_Mauler_MAL3R"] = IS_Mauler_MAL3R })
