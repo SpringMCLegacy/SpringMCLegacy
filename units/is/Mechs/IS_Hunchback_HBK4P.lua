6771463dbd1790f301f959402c3ea0837cf60b62
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2013-04-06 23:29:04 +0000

    -Added Hunchback variants HBK-4G and -4P.

---
+++ units/is/Mechs/IS_Hunchback_HBK4P.lua
@@ -0,0 +1,107 @@
+local IS_Hunchback_HBK4P = {
+	name              	= "Hunchback HBK-4P",
+	description         = "Medium Strike Mech",
+	objectName        	= "IS_Hunchback_HBK4P.s3o",
+	iconType			= "mediummech",
+	script				= "Mech.lua",
+	corpse				= "IS_Hunchback_X",
+	explodeAs          	= "mechexplode",
+	category 			= "mech ground notbeacon",
+	noChaseCategory		= "beacon air",
+		activateWhenBuilt   = true,
+		onoffable           = true,
+	maxDamage           = 16000,
+	mass                = 5000,
+	footprintX			= 2,
+	footprintZ 			= 2,
+	collisionVolumeType = "box",
+	collisionVolumeScales = "25 50 25",
+	collisionVolumeOffsets = "0 0 0",
+	collisionVolumeTest = 1,
+--	leaveTracks			= 1,
+--	trackOffset			= 10,--no idea what this does
+--	trackStrength		= 2.5,--how visible the tracks are
+--	trackStretch		= 1,-- how much the tracks stretch, the higher the number the more "compact" they become
+--	trackType			= "Thick",--graphics file to use for the track decal, from \bitmaps\tracks\ folder
+--	trackWidth			= 20,--width to render the decal
+	buildCostEnergy     = 55,
+	buildCostMetal      = 21100,
+	buildTime           = 0,
+	upright				= true,
+	canMove				= true,
+		movementClass   = "SMALLMECH",
+		maxVelocity		= 3, --60kph/20
+		smoothAnim		= 1,
+	
+	canAttack 			= true,
+		--Makes unit use weapon from /weapons folder
+		weapons 		= {	
+			[1] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 270,
+				OnlyTargetCategory = "notbeacon",
+			},
+			[2] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 220,
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+			[3] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 220,
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+			[4] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 220,
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+			[5] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 220,
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+			[6] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 220,
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+			[7] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 220,
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+			[8] = {
+				name	= "MBL",
+				mainDir = "0 0 1",
+				maxAngleDif = 220,
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+		},
+		
+    customparams = {
+		hasturnbutton	= "1",
+		helptext		= "Armament: 8 x Medium Beam Laser - Armor: 10 tons Standard",
+		heatlimit		= "23",
+		torsoturnspeed	= "160",
+		leftarmid		= "8",
+		rightarmid 		= "7",
+		unittype		= "mech",
+    },
+}
+
+return lowerkeys({ ["IS_Hunchback_HBK4P"] = IS_Hunchback_HBK4P })
