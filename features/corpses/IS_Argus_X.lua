0925edfd5958246bf16d7476df18ba446fedbc84
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2013-04-08 15:30:29 +0000

    -Added Argus AGS-4D.. with RAC-5!!!

---
+++ features/corpses/IS_Argus_X.lua
@@ -0,0 +1,18 @@
+local featureDefs = {
+  IS_Argus_X = {
+    blocking           = false,
+    category           = "corpses",
+    damage             = 5400,
+    description        = "Wreckage",
+    --featureDead        = "rubble",
+    footprintX         = 3,
+    footprintZ         = 3,
+    height             = "25",
+    hitdensity         = "50",
+    metal              = 2000,
+    object             = "IS_Argus_X.s3o",
+    reclaimable        = true,
+    world              = "All Worlds",
+  },
+}
+return featureDefs
