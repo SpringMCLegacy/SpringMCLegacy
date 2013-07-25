eec00150217ada525d1fbcca78c23eda9f7f1453
flozi <flozi@ef3f1c50-5774-440b-a771-62f173def817>
2013-07-25 21:38:45 +0000

    * Read custom limbscale and torsoscale from customparams in Gadgets/unit_pieceHitVols.lua * Set defaults as 1.1, 0.75 respectively

--- LuaRules/Gadgets/unit_pieceHitVols.lua
+++ LuaRules/Gadgets/unit_pieceHitVols.lua
@@ -22,8 +22,8 @@ local GetPieceColVol = Spring.GetUnitPieceCollisionVolumeData
 local SetPieceColVol = Spring.SetUnitPieceCollisionVolumeData
 
 -- Constants
-local TORSO_SCALE = 0.5
-local LIMB_SCALE = 1.5
+local TORSO_SCALE = 0.75
+local LIMB_SCALE = 1.1
 
 function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
 	local ud = UnitDefs[unitDefID]
@@ -37,8 +37,8 @@ function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
 				SetPieceColVol (unitID, i - 1, false, 0,0,0, 0,0,0, -1, 0)
 			else 
 				local scaleX, scaleY, scaleZ, offsetX, offsetY, offsetZ, volumeType, _, primaryAxis = GetPieceColVol (unitID, i-1)
-				local scaleMult = LIMB_SCALE
-				if pieceName == "torso" or pieceName == "pelvis" then scaleMult = TORSO_SCALE end
+				local scaleMult = (cp.limbscale or LIMB_SCALE)
+				if pieceName == "torso" or pieceName == "pelvis" then scaleMult = (cp.torsoscale or TORSO_SCALE) end
 				SetPieceColVol(unitID, i - 1, true, 
 								scaleMult*scaleX, scaleMult*scaleY, scaleMult*scaleZ, 
 								offsetX, offsetY, offsetZ, volumeType, primaryAxis)
