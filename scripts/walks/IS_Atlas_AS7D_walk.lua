1538d570dda828feba7f8eba6c4ce7076440a2e1
mechwarspring <zverofaust@users.noreply.github.com>
2013-07-28 11:04:43 +0000

    -A bunch of walk animation improvements.

--- scripts/walks/IS_Atlas_AS7D_walk.lua
+++ scripts/walks/IS_Atlas_AS7D_walk.lua
@@ -1,23 +1,25 @@
 --piece defines
 -- NB. local here means main script can't read them, may want to change that for e.g. Killed (or put Killed in here for per-unit death anims! But then other pieces need to be none-local)
-local lupperleg, llowerleg, rupperleg, rlowerleg, rfoot, lfoot = piece ("lupperleg", "llowerleg", "rupperleg", "rlowerleg", "rfoot", "lfoot")
+local pelvis, lupperleg, llowerleg, rupperleg, rlowerleg, rfoot, lfoot = piece ("pelvis", "lupperleg", "llowerleg", "rupperleg", "rlowerleg", "rfoot", "lfoot")
 
 --Turning/Movement Locals
-local LEG_SPEED = rad(350)
+local LEG_SPEED = rad(300)
 
 -- Walk script
 function MotionControl()
 	while true do
 		if walking then
-			--Spring.Echo("Step ONE")
+			--Spring.Echo("Step 0.5")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(2), LEG_SPEED)
 			--Left Leg--
-			Turn(lupperleg, x_axis, rad(25), LEG_SPEED)
-			Turn(llowerleg, x_axis, rad(25), LEG_SPEED)
-			Turn(lfoot, x_axis, rad(-15), LEG_SPEED)
+			Turn(lupperleg, x_axis, rad(7.5), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(2.5), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(-7.5), LEG_SPEED / 4)
 			--Right Leg--
-			Turn(rupperleg, x_axis, rad(-25), LEG_SPEED)
-			Turn(rlowerleg, x_axis, rad(15), LEG_SPEED)
-			Turn(rfoot, x_axis, rad(45), LEG_SPEED)
+			Turn(rupperleg, x_axis, rad(-22.5), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(37.5), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(0), LEG_SPEED)
 			--Wait For Turns...--
 			WaitForTurn(lupperleg, x_axis)
 			WaitForTurn(llowerleg, x_axis)
@@ -25,16 +27,17 @@ function MotionControl()
 			WaitForTurn(rupperleg, x_axis)
 			WaitForTurn(rlowerleg, x_axis)
 			WaitForTurn(rfoot, x_axis)
-			Sleep(10)
-			--Spring.Echo("Step TWO")
+			--Spring.Echo("Step ONE")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(2), LEG_SPEED)
 			--Left Leg--
-			Turn(lupperleg, x_axis, rad(25), LEG_SPEED)
-			Turn(llowerleg, x_axis, rad(45), LEG_SPEED)
-			Turn(lfoot, x_axis, rad(-60), LEG_SPEED)
+			Turn(lupperleg, x_axis, rad(15), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(5), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(-15), LEG_SPEED / 4)
 			--Right Leg--
 			Turn(rupperleg, x_axis, rad(-45), LEG_SPEED)
-			Turn(rlowerleg, x_axis, rad(0), LEG_SPEED)
-			Turn(rfoot, x_axis, rad(40), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(75), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(0), LEG_SPEED)
 			--Wait For Turns...--
 			WaitForTurn(lupperleg, x_axis)
 			WaitForTurn(llowerleg, x_axis)
@@ -42,11 +45,69 @@ function MotionControl()
 			WaitForTurn(rupperleg, x_axis)
 			WaitForTurn(rlowerleg, x_axis)
 			WaitForTurn(rfoot, x_axis)
-			Sleep(10)
-			--Spring.Echo("Step THREE")
+			--Sleep(10)
+			--Spring.Echo("Step 1.5")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(2), LEG_SPEED)
 			--Left Leg--
-			Turn(lupperleg, x_axis, rad(60), LEG_SPEED)
-			Turn(llowerleg, x_axis, rad(15), LEG_SPEED)
+			Turn(lupperleg, x_axis, rad(17.5), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(7.5), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(-17.5), LEG_SPEED / 4)
+			--Right Leg--
+			Turn(rupperleg, x_axis, rad(-47.5), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(50), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(0), LEG_SPEED)
+			--Wait For Turns...--
+			WaitForTurn(lupperleg, x_axis)
+			WaitForTurn(llowerleg, x_axis)
+			WaitForTurn(lfoot, x_axis)
+			WaitForTurn(rupperleg, x_axis)
+			WaitForTurn(rlowerleg, x_axis)
+			WaitForTurn(rfoot, x_axis)
+			--Spring.Echo("Step TWO")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(3), LEG_SPEED / 4)
+			--Left Leg--
+			Turn(lupperleg, x_axis, rad(20), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(10), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(-20), LEG_SPEED)
+			--Right Leg--
+			Turn(rupperleg, x_axis, rad(-50), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(25), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(0), LEG_SPEED)
+			--Wait For Turns...--
+			WaitForTurn(lupperleg, x_axis)
+			WaitForTurn(llowerleg, x_axis)
+			WaitForTurn(lfoot, x_axis)
+			WaitForTurn(rupperleg, x_axis)
+			WaitForTurn(rlowerleg, x_axis)
+			WaitForTurn(rfoot, x_axis)
+			--Sleep(10)
+			--Spring.Echo("Step 2.5")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(3), LEG_SPEED / 4)
+			--Left Leg--
+			Turn(lupperleg, x_axis, rad(10), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(42.5), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(-10), LEG_SPEED)
+			--Right Leg--
+			Turn(rupperleg, x_axis, rad(-40), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(20), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(7.5), LEG_SPEED)
+			--Wait For Turns...--
+			WaitForTurn(lupperleg, x_axis)
+			WaitForTurn(llowerleg, x_axis)
+			WaitForTurn(lfoot, x_axis)
+			WaitForTurn(rupperleg, x_axis)
+			WaitForTurn(rlowerleg, x_axis)
+			WaitForTurn(rfoot, x_axis)
+			--Sleep(10)
+			--Spring.Echo("Step THREE")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(-2), LEG_SPEED / 4)
+			--Left Leg--
+			Turn(lupperleg, x_axis, rad(0), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(75), LEG_SPEED)
 			Turn(lfoot, x_axis, rad(0), LEG_SPEED)
 			--Right Leg--
 			Turn(rupperleg, x_axis, rad(-30), LEG_SPEED)
@@ -59,11 +120,32 @@ function MotionControl()
 			WaitForTurn(rupperleg, x_axis)
 			WaitForTurn(rlowerleg, x_axis)
 			WaitForTurn(rfoot, x_axis)
-			Sleep(10)
-			--Spring.Echo("Step FOUR")
+			--Sleep(10)
+			--Spring.Echo("Step 3.5")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(-2), LEG_SPEED / 4)
 			--Left Leg--
-			Turn(lupperleg, x_axis, rad(-10), LEG_SPEED)
-			Turn(llowerleg, x_axis, rad(-15), LEG_SPEED)
+			Turn(lupperleg, x_axis, rad(-22.5), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(75), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(15), LEG_SPEED)
+			--Right Leg--
+			Turn(rupperleg, x_axis, rad(-15), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(10), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(7.5), LEG_SPEED)
+			--Wait For Turns...--
+			WaitForTurn(lupperleg, x_axis)
+			WaitForTurn(llowerleg, x_axis)
+			WaitForTurn(lfoot, x_axis)
+			WaitForTurn(rupperleg, x_axis)
+			WaitForTurn(rlowerleg, x_axis)
+			WaitForTurn(rfoot, x_axis)
+			--Sleep(10)
+			--Spring.Echo("Step FOUR")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(-3), LEG_SPEED / 4)
+			--Left Leg--
+			Turn(lupperleg, x_axis, rad(-45), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(75), LEG_SPEED)
 			Turn(lfoot, x_axis, rad(30), LEG_SPEED)
 			--Right Leg--
 			Turn(rupperleg, x_axis, rad(0), LEG_SPEED)
@@ -76,16 +158,37 @@ function MotionControl()
 			WaitForTurn(rupperleg, x_axis)
 			WaitForTurn(rlowerleg, x_axis)
 			WaitForTurn(rfoot, x_axis)
-			Sleep(10)
+			--Sleep(10)
+			--Spring.Echo("Step 4.5")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(-3), LEG_SPEED / 4)
+			--Left Leg--
+			Turn(lupperleg, x_axis, rad(-47.5), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(50), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(15), LEG_SPEED)
+			--Right Leg--
+			Turn(rupperleg, x_axis, rad(10), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(7.5), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(-10), LEG_SPEED)
+			--Wait For Turns...--
+			WaitForTurn(lupperleg, x_axis)
+			WaitForTurn(llowerleg, x_axis)
+			WaitForTurn(lfoot, x_axis)
+			WaitForTurn(rupperleg, x_axis)
+			WaitForTurn(rlowerleg, x_axis)
+			WaitForTurn(rfoot, x_axis)
+			--Sleep(10)
 			--Spring.Echo("Step FIVE")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(-3), LEG_SPEED / 4)
 			--Left Leg--
-			Turn(lupperleg, x_axis, rad(-25), LEG_SPEED)
-			Turn(llowerleg, x_axis, rad(-15), LEG_SPEED)
-			Turn(lfoot, x_axis, rad(45), LEG_SPEED)
+			Turn(lupperleg, x_axis, rad(-50), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(25), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(0), LEG_SPEED)
 			--Right Leg--
-			Turn(rupperleg, x_axis, rad(25), LEG_SPEED)
-			Turn(rlowerleg, x_axis, rad(25), LEG_SPEED)
-			Turn(rfoot, x_axis, rad(-45), LEG_SPEED)
+			Turn(rupperleg, x_axis, rad(20), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(10), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(-20), LEG_SPEED)
 			--Wait For Turns...--
 			WaitForTurn(lupperleg, x_axis)
 			WaitForTurn(llowerleg, x_axis)
@@ -93,32 +196,36 @@ function MotionControl()
 			WaitForTurn(rupperleg, x_axis)
 			WaitForTurn(rlowerleg, x_axis)
 			WaitForTurn(rfoot, x_axis)
-			Sleep(10)
+			--Sleep(10)
+			--Spring.Echo("Step 5.5")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(-3), LEG_SPEED / 4)
+			--Left Leg--
+			Turn(lupperleg, x_axis, rad(-40), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(20), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(7.5), LEG_SPEED)
+			--Right Leg--
+			Turn(rupperleg, x_axis, rad(10), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(42.5), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(-10), LEG_SPEED)
+			--Wait For Turns...--
+			WaitForTurn(lupperleg, x_axis)
+			WaitForTurn(llowerleg, x_axis)
+			WaitForTurn(lfoot, x_axis)
+			WaitForTurn(rupperleg, x_axis)
+			WaitForTurn(rlowerleg, x_axis)
+			WaitForTurn(rfoot, x_axis)
+			--Sleep(10)
 			--Spring.Echo("Step SIX")
-			--Left Leg--
-			Turn(lupperleg, x_axis, rad(-45), LEG_SPEED)
-			Turn(llowerleg, x_axis, rad(0), LEG_SPEED)
-			Turn(lfoot, x_axis, rad(60), LEG_SPEED)
-			--Right Leg--
-			Turn(rupperleg, x_axis, rad(25), LEG_SPEED)
-			Turn(rlowerleg, x_axis, rad(45), LEG_SPEED)
-			Turn(rfoot, x_axis, rad(-60), LEG_SPEED)
-			--Wait For Turns...--
-			WaitForTurn(lupperleg, x_axis)
-			WaitForTurn(llowerleg, x_axis)
-			WaitForTurn(lfoot, x_axis)
-			WaitForTurn(rupperleg, x_axis)
-			WaitForTurn(rlowerleg, x_axis)
-			WaitForTurn(rfoot, x_axis)
-			Sleep(10)
-			--Spring.Echo("Step SEVEN")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(2), LEG_SPEED / 4)
 			--Left Leg--
 			Turn(lupperleg, x_axis, rad(-30), LEG_SPEED)
 			Turn(llowerleg, x_axis, rad(15), LEG_SPEED)
 			Turn(lfoot, x_axis, rad(15), LEG_SPEED)
 			--Right Leg--
-			Turn(rupperleg, x_axis, rad(45), LEG_SPEED)
-			Turn(rlowerleg, x_axis, rad(15), LEG_SPEED)
+			Turn(rupperleg, x_axis, rad(0), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(75), LEG_SPEED)
 			Turn(rfoot, x_axis, rad(0), LEG_SPEED)
 			--Wait For Turns...--
 			WaitForTurn(lupperleg, x_axis)
@@ -127,16 +234,18 @@ function MotionControl()
 			WaitForTurn(rupperleg, x_axis)
 			WaitForTurn(rlowerleg, x_axis)
 			WaitForTurn(rfoot, x_axis)
-			Sleep(10)
-			--Spring.Echo("Step EIGHT")
+			--Sleep(10)
+			--Spring.Echo("Step 6.5")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(2), LEG_SPEED / 4)
 			--Left Leg--
-			Turn(lupperleg, x_axis, rad(0), LEG_SPEED)
-			Turn(llowerleg, x_axis, rad(5), LEG_SPEED)
-			Turn(lfoot, x_axis, rad(0), LEG_SPEED)
+			Turn(lupperleg, x_axis, rad(-15), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(7.5), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(7.5), LEG_SPEED)
 			--Right Leg--
-			Turn(rupperleg, x_axis, rad(-10), LEG_SPEED)
-			Turn(rlowerleg, x_axis, rad(-15), LEG_SPEED)
-			Turn(rfoot, x_axis, rad(30), LEG_SPEED)
+			Turn(rupperleg, x_axis, rad(-22.5), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(75), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(0), LEG_SPEED)
 			--Wait For Turns...--
 			WaitForTurn(lupperleg, x_axis)
 			WaitForTurn(llowerleg, x_axis)
@@ -144,7 +253,26 @@ function MotionControl()
 			WaitForTurn(rupperleg, x_axis)
 			WaitForTurn(rlowerleg, x_axis)
 			WaitForTurn(rfoot, x_axis)
-			Sleep(10)
+			--Sleep(10)
+			--Spring.Echo("Step SEVEN")
+			--Pelvis--
+			Turn(pelvis, z_axis, rad(3), LEG_SPEED / 4)
+			--Left Leg--
+			Turn(lupperleg, x_axis, rad(0), LEG_SPEED)
+			Turn(llowerleg, x_axis, rad(0), LEG_SPEED)
+			Turn(lfoot, x_axis, rad(0), LEG_SPEED)
+			--Right Leg--
+			Turn(rupperleg, x_axis, rad(-45), LEG_SPEED)
+			Turn(rlowerleg, x_axis, rad(75), LEG_SPEED)
+			Turn(rfoot, x_axis, rad(0), LEG_SPEED)
+			--Wait For Turns...--
+			WaitForTurn(lupperleg, x_axis)
+			WaitForTurn(llowerleg, x_axis)
+			WaitForTurn(lfoot, x_axis)
+			WaitForTurn(rupperleg, x_axis)
+			WaitForTurn(rlowerleg, x_axis)
+			WaitForTurn(rfoot, x_axis)
+			--Sleep(10)
 		else
 			Turn(lupperleg, x_axis, rad(0), LEG_SPEED)
 			Turn(llowerleg, x_axis, rad(0), LEG_SPEED)
@@ -152,7 +280,7 @@ function MotionControl()
 			Turn(rupperleg, x_axis, rad(0), LEG_SPEED)
 			Turn(rlowerleg, x_axis, rad(0), LEG_SPEED)
 			Turn(rfoot, x_axis, rad(0), LEG_SPEED)
-			Sleep(100)
+			Sleep(10)
 		end
 	end
 end
