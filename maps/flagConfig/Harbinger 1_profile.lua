fcc63434a6818e11690569ce541e93d92498810e
mechwarspring <zverofaust@users.noreply.github.com>
2013-07-29 00:35:49 +0000

    -More map profiles -Chimera texture!

---
+++ maps/flagConfig/Harbinger 1_profile.lua
@@ -0,0 +1,32 @@
+local resources = {
+	{
+		x = 7250,
+		z = 600,
+		feature = nil
+	},
+	{
+		x = 2700,
+		z = 9400,
+		feature = nil
+	},
+	{
+		x = 3275,
+		z = 5080,
+		feature = nil
+	},
+	{
+		x = 6800,
+		z = 5160,
+		feature = nil
+	},
+}
+
+local outposts = {
+	{
+		x = 5160,--middle
+		z = 5120,
+		types = {"outpost_vehicledepot", "outpost_controltower"},
+	},
+}
+
+return resources, outposts
