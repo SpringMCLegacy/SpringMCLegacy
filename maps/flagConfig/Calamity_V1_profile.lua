41cb665352da8aa3023ca03ac07092243be3db04
mechwarspring <zverofaust@users.noreply.github.com>
2013-07-29 12:26:51 +0000

    Calamity map config.

---
+++ maps/flagConfig/Calamity_V1_profile.lua
@@ -0,0 +1,22 @@
+local resources = {
+	{
+		x = 1670,
+		z = 4230,
+		feature = nil
+	},
+	{
+		x = 6500,
+		z = 4000,
+		feature = nil
+	},
+}
+	
+local outposts = {
+	{
+		x = 4020,
+		z = 4060,
+		types = {"outpost_listeningpost", "outpost_garrison", "outpost_c3center"},
+	},
+}
+
+return resources, outposts
