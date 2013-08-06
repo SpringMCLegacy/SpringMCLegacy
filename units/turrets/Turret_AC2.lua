44a16ca05d1ccf64415122b1d145fe4b01dc7058
mechwarspring <zverofaust@users.noreply.github.com>
2013-08-06 11:57:32 +0000

    -Replacing old and dusty turrets with the new hotness turrets. -Added units/turrets for new turrets. -TODO: Simplified turret system. All turrets will have leg_# (1-4), base, turret, rot, and flare_#weaponid (for every weapon). Be it a missile launcher, laser, LAMS or autocannon turret. Currently the models follow the ~old ways~ of base, turret, mantlet for ACs, launcher for missiles etc.

---
+++ units/turrets/Turret_AC2.lua
@@ -0,0 +1,60 @@
+local Turret_AC2 = {
+	name              	= "Weapon Emplacement",
+	description         = "Quad AC/2",
+	objectName        	= "Turret_AC2.s3o",
+	script				= "Turret.lua",
+	category 			= "structure notbeacon",
+		activateWhenBuilt   = true,
+	maxDamage           = 12000,
+	mass                = 5000,
+	footprintX			= 3,
+	footprintZ 			= 3,
+	collisionVolumeType = "box",
+	collisionVolumeScales = "25 25 25",
+	collisionVolumeOffsets = "0 0 0",
+	collisionVolumeTest = 1,
+	buildCostEnergy     = 0,
+	buildCostMetal      = 0,
+	buildTime           = 0,
+	canMove				= false,
+	maxVelocity			= 0,
+	idleAutoHeal		= 0,
+	maxSlope			= 100,
+	builder				= false,
+	TEDClass			= "FORT",
+	canAttack 			= true,
+		--Makes unit use weapon from /weapons folder
+		weapons 		= {	
+			[1] = {
+				name	= "AC2",
+				OnlyTargetCategory = "notbeacon",
+			},
+			[2] = {
+				name	= "AC2",
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+			[3] = {
+				name	= "AC2",
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+			[4] = {
+				name	= "AC2",
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+		},
+	customparams = {
+		ammosupplier	= "0",
+		supplyradius	= "0",
+		flagdefendrate	= "100",
+		helptext		= "An auto-spawning defensive turret for Garrisons.",
+		barrelrecoildist = "{[1] = 5}",
+    },
+	sounds = {
+    underattack        = "Dropship_Alarm",
+	},
+}
+
+return lowerkeys({ ["Turret_AC2"] = Turret_AC2 })
---
+++ units/turrets/Turret_PPC.lua
@@ -0,0 +1,50 @@
+local Turret_PPC = {
+	name              	= "Weapon Emplacement",
+	description         = "Dual PPC",
+	objectName        	= "Turret_PPC.s3o",
+	script				= "Turret.lua",
+	category 			= "structure notbeacon",
+		activateWhenBuilt   = true,
+	maxDamage           = 12000,
+	mass                = 5000,
+	footprintX			= 3,
+	footprintZ 			= 3,
+	collisionVolumeType = "box",
+	collisionVolumeScales = "25 25 25",
+	collisionVolumeOffsets = "0 0 0",
+	collisionVolumeTest = 1,
+	buildCostEnergy     = 0,
+	buildCostMetal      = 0,
+	buildTime           = 0,
+	canMove				= false,
+	maxVelocity			= 0,
+	idleAutoHeal		= 0,
+	maxSlope			= 100,
+	builder				= false,
+	TEDClass			= "FORT",
+	canAttack 			= true,
+		--Makes unit use weapon from /weapons folder
+		weapons 		= {	
+			[1] = {
+				name	= "PPC",
+				OnlyTargetCategory = "notbeacon",
+			},
+			[2] = {
+				name	= "PPC",
+				OnlyTargetCategory = "notbeacon",
+				SlaveTo = 1,
+			},
+		},
+	customparams = {
+		ammosupplier	= "0",
+		supplyradius	= "0",
+		flagdefendrate	= "100",
+		helptext		= "An auto-spawning defensive turret for Garrisons.",
+		barrelrecoildist = "{[1] = 5}",
+    },
+	sounds = {
+    underattack        = "Dropship_Alarm",
+	},
+}
+
+return lowerkeys({ ["Turret_PPC"] = Turret_PPC })
