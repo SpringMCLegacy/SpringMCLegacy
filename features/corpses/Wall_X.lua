d74c0afe34cc36026428d65b8c32b47e36f5a4ac
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2013-08-20 16:35:33 +0000

    WALLS

---
+++ features/corpses/Wall_X.lua
@@ -0,0 +1,20 @@
+local featureDefs = {
+  Wall_X = {
+    blocking           = false,
+    category           = "corpses",
+    damage             = 5400,
+    description        = "Wrecked Wall",
+    --featureDead        = "rubble",
+    collisionVolumeType = "box",
+	collisionVolumeScales = "70 70 25",
+	collisionVolumeOffsets = "0 10 0",
+	collisionVolumeTest = 1,
+    height             = "50",
+    hitdensity         = "10",
+    metal              = 0,
+    object             = "Wall_X.s3o",
+    reclaimable        = false,
+    world              = "All Worlds",
+  },
+}
+return featureDefs
---
+++ units/Wall.lua
@@ -0,0 +1,38 @@
+local Wall = {
+	name              	= "Wall",
+	description         = "Fortified Barrier",
+	objectName        	= "Wall.s3o",
+	script				= "Wall.lua",
+	corpse				= "Wall_X",
+	--iconType			= "",
+	category 			= "structure ground notbeacon",
+	activateWhenBuilt   = true,
+	maxDamage           = 10000,
+	mass                = 1000,
+	footprintX			= 3,
+	footprintZ 			= 3,
+	collisionVolumeType = "box",
+	collisionVolumeScales = "70 70 25",
+	collisionVolumeOffsets = "0 10 0",
+	collisionVolumeTest = 1,
+	buildCostEnergy     = 0,
+	buildCostMetal      = 0,
+	buildTime           = 0,
+	canMove				= false,
+	maxVelocity			= 0,
+	idleAutoHeal		= 0,
+	maxSlope			= 100,
+	cantbetransported	= false,
+
+	customparams = {
+		ammosupplier	= "0",
+		supplyradius	= "0",
+		helptext		= "A Wall",
+		--flagdefendrate = 0,
+    },
+	--sounds = {
+    --underattack        = "Dropship_Alarm",
+	--},
+}
+
+return lowerkeys({ ["Wall"] = Wall })
