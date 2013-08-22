7ae575f1b2dbf3e12c838a7385d1a8d5391de236
flozi <flozi@ef3f1c50-5774-440b-a771-62f173def817>
2013-08-22 01:46:07 +0000

    * Make dropzone invulnerable * Cleanup junk from units/upgrades/Upgrade_Dropzone.lua

--- units/upgrades/Upgrade_Dropzone.lua
+++ units/upgrades/Upgrade_Dropzone.lua
@@ -5,47 +5,26 @@ local Upgrade_Dropzone = {
 	iconType			= "Ddropzone",
 	script				= "Upgrade_Dropzone.lua",
 	category 			= "beacon",
-	sightDistance       = 0,
-	radarDistance      	= 0,
-	activateWhenBuilt   = true,
-	maxDamage           = 50000,
-	mass                = 1000,
+	maxDamage           = 50000, -- should never take damage
 	footprintX			= 2,
 	footprintZ 			= 2,
+	-- Colvol
 	collisionVolumeType = "box",
 	collisionVolumeScales = "4 25 4",
-	collisionVolumeOffsets = "0 0 0",
-	collisionVolumeTest = 1,
-	buildCostEnergy     = 0,
-	buildCostMetal      = 0,
-	buildTime           = 0,
-	--canMove				= false,
-	energyStorage		= 0,
-	metalMake			= 0,
-	metalStorage		= 0,
-	idleAutoHeal		= 0,
-	maxSlope			= 50,
-		movementClass   = "LARGEMECH",
-		
 	-- Constructor stuff
 	builder				= true,
 	builddistance 		= 460,
 	workerTime			= 10, -- ?	
 	terraformSpeed		= 10000,
 	showNanoSpray		= false,
+	-- Set in weapondefs_post.lua
 	--[[sfxtypes = {
-		explosiongenerators = {"custom:reentry_fx", "custom:JumpJetTrail"},
+		explosiongenerators = {"custom:beacon"},
 	},]]
 	
 	customparams = {
-		dontcount		= 1,
 		helptext		= "A Beacon indicating a strategically important location.",
-		minbuildrange	= 230,
     },
-	
-	sounds = {
-    underattack        = "Dropship_Alarm",
-	},
 }
 
 return lowerkeys({ ["Upgrade_Dropzone"] = Upgrade_Dropzone })
