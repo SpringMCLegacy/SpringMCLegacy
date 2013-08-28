5a9175fd5c849af2e1a86983fe00801531ba0d42
flozi <flozi@ef3f1c50-5774-440b-a771-62f173def817>
2013-08-28 21:36:02 +0000

    * Update Gamedata/armordefs.lua for 95.0 * Reduce upgrade attack sound cooldown to 20s

--- Gamedata/armordefs.lua
+++ Gamedata/armordefs.lua
@@ -84,13 +84,13 @@ local armorDefs = {
 	},
 }
 -- convert to named maps  (trepan is a noob)
-for categoryName, categoryTable in pairs(armorDefs) do
+--[[for categoryName, categoryTable in pairs(armorDefs) do
   local t = {}
   for _, unitName in pairs(categoryTable) do
     t[unitName] = 1
   end
   armorDefs[categoryName] = t
-end
+end]]
 
 local system = VFS.Include('gamedata/system.lua')  
 
