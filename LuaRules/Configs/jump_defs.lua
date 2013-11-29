42d2a70210227e47ad714596d8a1d4cc8dfcc513
flozi <FLOZi@users.noreply.github.com>
2013-11-29 20:55:37 +0000

    * Fix jumpjets by reverting LuaRules/Configs/jump_defs.lua to r618

--- LuaRules/Configs/jump_defs.lua
+++ LuaRules/Configs/jump_defs.lua
@@ -71,7 +71,7 @@ jumpClassGroups = {
 	cl_nova = { cobscript = false},
 	cl_summoner = { cobscript = false},
 	cl_shadowcat = { cobscript = false},
- noruas = {
+ --[[noruas = {
     reload = 0,  },
 	
  core_slicer = {
@@ -87,9 +87,71 @@ jumpClassGroups = {
     delay = 30, height = 100, range = 300, reload = 13, cobscript = false, rotateMidAir = false},
 	-- corpyro = {
 		-- range = 400, height = 200, speed = 6,  reload = 10, },
-}
---]]
+}--]]
+------------------------------------------------------------------------------------------------------------------------------------------------
+-- this is just an error checking block, not our configuration area
+-- I know n^2, bite me... see error checking comment
+------------------------------------------------------------------------------------------------------------------------------------------------
+Spring.Echo("Jump Jet Defs error checking begining..")
+for groupId,groupcluster in pairs(jumpClassGroups) do
 
+	for i=1,#groupcluster do
+	
+		name = jumpClassGroups[groupId][i]
+		
+		if (UnitDefNames[name]) then -- I am half awake, hey at least someone did some kind of error checking...
+			if ( not jumpCategory[groupId] ) then 
+				Spring.Echo("   Jump Jet Defs error: (bad jumpjet category: " .. groupId .. " missing required parameter range)")
+				IsBadDef = true
+			else
+				if ( not jumpCategory[groupId].range ) then
+					Spring.Echo("   Jump Jet Defs error: (Unit: " .. name .. " missing required parameter range)")
+					IsBadDef = true
+				end
+				
+				if ( not jumpCategory[groupId].height ) then
+					Spring.Echo("   Jump Jet Defs error: (Unit: " .. name .. " missing required parameter height)")
+					IsBadDef = true
+				end
+				
+				if ( not jumpCategory[groupId].speed ) then
+					Spring.Echo("   Jump Jet Defs error: (Unit: " .. name .. " missing required parameter speed)")	
+					IsBadDef = true			
+				end
+				
+				if ( not jumpCategory[groupId].delay ) then
+					Spring.Echo("   Jump Jet Defs error: (Unit: " .. name .. " missing required parameter delay)")	
+					IsBadDef = true			
+				end
+				
+				--[[if ( not jumpCategory[groupId].cobscript ) then
+					Spring.Echo("   Jump Jet Defs error: (Unit: " .. name .. " missing required parameter cobscript)")	
+					IsBadDef = true			
+				end]]
+				
+				if ( not jumpCategory[groupId].rotateMidAir ) then
+					Spring.Echo("   Jump Jet Defs error: (Unit: " .. name .. " missing required parameter rotateMidAir)")	
+					IsBadDef = true			
+				end
+			end
+		else -- unit exists, lets make sure he has proper values
+			IsBadDef = true
+			Spring.Echo("   Jump Jet Defs error: (Unit name not found: " .. name .. " )")
+			Spring.Echo(i)
+		end	
+		
+		if ( IsBadDef == false ) then
+			local default = jumpCategory[groupId]
+			jumpers[name] = {range=default.range, height=default.height, speed=default.speed, reload=(default.reload or nil), delay=default.delay, cobscript=default.cobscript, rotateMidAir=default.rotateMidAir}
+		else
+			Spring.Echo("   Jump Jet Defs error: (Unit not added: " .. name .. " )")
+			IsBadDef = false 
+		end
+		
+	end
+	
+end
+Spring.Echo(".. Jump Jet Defs error checking complete")	
 ------------------------------------------------------------------------------------------------------------------------------------------------
 -- This section allows for overrides, when inidvidual units need to be slightly different but don't justify their own class
 -- 
