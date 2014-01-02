1a0c75750542ca412018748bdbf929aa90c8fecd
flozi <FLOZi@users.noreply.github.com>
2014-01-02 18:19:06 +0000

    * Fix "tap-dancing fleas"

--- scripts/Anims/IS_Flea.lua
+++ scripts/Anims/IS_Flea.lua
@@ -6,8 +6,9 @@ local pelvis, torso, lupperleg, llowerleg, rupperleg, rlowerleg, lfoot, rfoot = 
 local LEG_SPEED = rad(600)
 local LEG_TURN_SPEED = rad (400)
 
-function anim_Turn(signal)
-	SetSignalMask(signal)
+function anim_Turn(clockwise)
+	Signal(SIG_ANIMATE)
+	SetSignalMask(SIG_ANIMATE)
 	while true do
 		--Left Leg Up...
 		Turn(pelvis, z_axis, rad(-5), LEG_TURN_SPEED)
@@ -44,97 +45,11 @@ function anim_Turn(signal)
 	end
 end
 		
-function anim_PreJump()
-	while true do
-		--Crouch...
-		Turn(rupperleg, x_axis, rad(30), LEG_SPEED)
-		Turn(rlowerleg, x_axis, rad(-30), LEG_SPEED)
-		Turn(lupperleg, x_axis, rad(30), LEG_SPEED)
-		Turn(llowerleg, x_axis, rad(-30), LEG_SPEED)
-		Move(pelvis, y_axis, 7.6, LEG_SPEED)
-		Move(pelvis, z_axis, 4.3, LEG_SPEED)
-		--Hold a bit...
-		Sleep(100)
-		--Wait for turns...
-		WaitForTurn(rupperleg, x_axis)
-		WaitForTurn(rlowerleg, x_axis)
-		WaitForTurn(lupperleg, x_axis)
-		WaitForTurn(llowerleg, x_axis)
-	end
-end
-
-function anim_Jump()
-	while true do
-		--Jump!
-		Turn(rupperleg, x_axis, rad(5), LEG_SPEED * 4)
-		Turn(rlowerleg, x_axis, rad(20), LEG_SPEED * 4)
-		Turn(lupperleg, x_axis, rad(5), LEG_SPEED * 4)
-		Turn(llowerleg, x_axis, rad(20), LEG_SPEED * 4)
-		Move(pelvis, y_axis, 0, LEG_SPEED * 4)
-		Move(pelvis, z_axis, 0, LEG_SPEED * 4)
-		--Wait for turns...
-		WaitForTurn(rupperleg, x_axis)
-		WaitForTurn(rlowerleg, x_axis)
-		WaitForTurn(lupperleg, x_axis)
-		WaitForTurn(llowerleg, x_axis)
-	end
-end
-
-function anim_JumpInProgress()
-	while true do
-		--Brace for impact...
-		Turn(rupperleg, x_axis, rad(-15), LEG_SPEED / 4)
-		Turn(rlowerleg, x_axis, rad(20), LEG_SPEED / 4)
-		Turn(rfoot, x_axis, rad(-10), LEG_SPEED / 4)
-		Turn(lupperleg, x_axis, rad(-15), LEG_SPEED / 4)
-		Turn(llowerleg, x_axis, rad(20), LEG_SPEED / 4)
-		Turn(lfoot, x_axis, rad(-10), LEG_SPEED / 4)
-		--WaitForTurns...
-		WaitForTurn(rupperleg, x_axis)
-		WaitForTurn(rlowerleg, x_axis)
-		WaitForTurn(rfoot, x_axis)
-		WaitForTurn(lupperleg, x_axis)
-		WaitForTurn(lfoot, x_axis)
-		WaitForTurn(llowerleg, x_axis)
-	end
-end
-
-function anim_Land()
-	while true do
-		--Touchdown!
-		Turn(rupperleg, x_axis, rad(20), LEG_SPEED * 4)
-		Turn(rlowerleg, x_axis, rad(-25), LEG_SPEED * 4)
-		Turn(rfoot, x_axis, rad(5), LEG_SPEED * 4)
-		Turn(lupperleg, x_axis, rad(20), LEG_SPEED * 4)
-		Turn(llowerleg, x_axis, rad(-25), LEG_SPEED * 4)
-		Turn(lfoot, x_axis, rad(5), LEG_SPEED * 4)
-		--Wait for turns...
-		WaitForTurn(rupperleg, x_axis)
-		WaitForTurn(rlowerleg, x_axis)
-		WaitForTurn(rfoot, x_axis)
-		WaitForTurn(lupperleg, x_axis)
-		WaitForTurn(llowerleg, x_axis)
-		WaitForTurn(lfoot, x_axis)
-		--Recover
-		Turn(rupperleg, x_axis, rad(0), LEG_SPEED / 4)
-		Turn(rlowerleg, x_axis, rad(0), LEG_SPEED / 4)
-		Turn(rfoot, x_axis, rad(0), LEG_SPEED / 4)
-		Turn(lupperleg, x_axis, rad(0), LEG_SPEED / 4)
-		Turn(llowerleg, x_axis, rad(0), LEG_SPEED / 4)
-		Turn(lfoot, x_axis, rad(0), LEG_SPEED / 4)
-		--Wait for turns?
-		WaitForTurn(rupperleg, x_axis)
-		WaitForTurn(rlowerleg, x_axis)
-		WaitForTurn(rfoot, x_axis)
-		WaitForTurn(lupperleg, x_axis)
-		WaitForTurn(lfoot, x_axis)
-		WaitForTurn(llowerleg, x_axis)
-	end
-end
 
 -- Walk script
-function anim_Walk(signal)
-	SetSignalMask(signal)
+function anim_Walk()
+	Signal(SIG_ANIMATE)
+	SetSignalMask(SIG_ANIMATE)
 	while true do
 		--Spring.Echo("START")
 		Turn(pelvis, z_axis, rad(5), LEG_SPEED / 10)
@@ -388,6 +303,7 @@ function anim_Walk(signal)
 end
 
 function anim_Reset()
+	Signal(SIG_ANIMATE)
 	Turn(pelvis, z_axis, rad(0), LEG_SPEED)
 	Turn(lupperleg, x_axis, rad(0), LEG_SPEED)
 	Turn(llowerleg, x_axis, rad(0), LEG_SPEED)
