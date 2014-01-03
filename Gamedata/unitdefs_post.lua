2313aad35226bfafb919ec2a9c95dd29a9a082cf
flozi <FLOZi@users.noreply.github.com>
2014-01-03 00:57:59 +0000

    * Unbreak stuff

--- Gamedata/unitdefs_post.lua
+++ Gamedata/unitdefs_post.lua
@@ -160,8 +160,10 @@ for name, ud in pairs(UnitDefs) do
 	if name == "cl_dropzone" then CL_DROPZONE_UD = ud end
 	
 	if name == "beacon" or name:find("upgrade") or name:find("dropzone") or name:find("dropship") then 
-		if name == "beacon" or name:find("dropship") or name:find("dropzone") then
+		if name == "beacon" then
 			BEACON_UD = ud 
+			ud.canselfdestruct = false
+		elseif name:find("dropship") or name:find("dropzone") then
 			ud.canselfdestruct = false
 		end
 		ud.canmove = false
