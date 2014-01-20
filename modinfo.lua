c93afc4e0b5ae518e7422f92b647b23814507d32
flozi <FLOZi@users.noreply.github.com>
2014-01-20 19:23:13 +0000

    * Cleanup CurrentIssues.txt * Fix modinfo.lua for rapidness * Some Smawth bitmaps i missed

--- modinfo.lua
+++ modinfo.lua
@@ -7,7 +7,7 @@ local modinfo = {
 		description				= "War is hell.",
 		url						= "http://springrts.com/phpbb/viewforum.php?f=70",
 		modtype					= "1",
-		version					= "0.1.svn", -- change to rev before release
+		version					= "$VERSION",
 }
 
 return modinfo
