0ac0084383e9214e938db9247edb0c524b791040
flozi <flozi@ef3f1c50-5774-440b-a771-62f173def817>
2014-02-15 23:07:14 +0000

    * Make walls less impervious (20% damage -> 80% damage)

--- Gamedata/weapondefs_post.lua
+++ Gamedata/weapondefs_post.lua
@@ -19,7 +19,7 @@ local damageMults = {
 	assault		= 0.6, --40% default
 	vehicle		= 1.25, --150% default
 	vtol		= 1.25, --150% default
-	walls		= 0.2, -- 20$ default
+	walls		= 0.8, -- 80% default
 }
 
 for weapName, wd in pairs(WeaponDefs) do 
