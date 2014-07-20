226471a23fa2dac48c9e4993a09ae651af17b6fc
flozi <FLOZi@users.noreply.github.com>
2014-07-20 10:47:29 +0000

    * SpamBot will now attack 'base' beacons and patrol aircraft

--- LuaRules/Gadgets/ai_spamBot.lua
+++ LuaRules/Gadgets/ai_spamBot.lua
@@ -15,14 +15,15 @@ end
 if gadgetHandler:IsSyncedCode() then
 --	SYNCED
 local AI_TEAMS = {}
-
+local BEACON_ID = UnitDefNames["beacon"].id
+local C3_ID = UnitDefNames["upgrade_c3array"].id
 --local DelayCall = GG.Delay.DelayCall
 local CMD_SEND_ORDER = GG.CustomCommands.GetCmdID("CMD_SEND_ORDER")
 local CMD_C3 = GG.CustomCommands.GetCmdID("CMD_UPGRADE_upgrade_c3array")
 local dropZoneIDs = {}
 local orderSizes = {}
 
-local flagSpots = VFS.Include("maps/flagConfig/" .. Game.mapName .. "_profile.lua")
+local flagSpots = {} --VFS.Include("maps/flagConfig/" .. Game.mapName .. "_profile.lua")
 
 function gadget:GamePreload()
 	-- Initialise unit limit for all AI teams.
@@ -43,7 +44,7 @@ local function Spam(unitID, teamID)
 		if cmdDesc.id < 0 then
 			GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, cmdDesc.id, {}, {}}, 1)
 			orderSizes[teamID] = orderSizes[teamID] + 1
-			--Spring.Echo(orderSizes[teamID], cmdDesc.name)
+			--Spring.Echo(orderSizes[teamID], cmdDesc.name, GG.teamSlotsRemaining[teamID])
 		end
 	end
 	local readyFrame = Spring.GetTeamRulesParam(teamID, "DROPSHIP_COOLDOWN") or 0
@@ -53,6 +54,10 @@ local function Spam(unitID, teamID)
 end
 
 function gadget:UnitCreated(unitID, unitDefID, teamID)
+	if unitDefID == BEACON_ID then
+		local x,_,z = Spring.GetUnitPosition(unitID)
+		table.insert(flagSpots, {x = x, z = z})
+	end
 	if AI_TEAMS[teamID] then
 		local unitDef = UnitDefs[unitDefID]
 		if unitDef.name:find("dropzone") then
@@ -65,26 +70,37 @@ end
 function gadget:UnitDestroyed(unitID, unitDefID, teamID)
 	if AI_TEAMS[teamID] then
 		Spam(dropZoneIDs[teamID], teamID)
-		if unitDefID == UnitDefNames["upgrade_c3array"].id then
+		if unitDefID == C3_ID then
 			GG.Delay.DelayCall(Spring.GiveOrderToUnit, {tonumber(Spring.GetUnitRulesParam(unitID, "beaconID")), CMD_C3, {}, {}}, 1)
 		end
 	end
 end
 
-function gadget:UnitUnloaded(unitID, unitDefID, unitTeam, transportID, transportTeam)
-	if AI_TEAMS[unitTeam] then
-		local spot = flagSpots[math.random(1, #flagSpots)]
-		local offsetX = math.random(50, 150)
-		local offsetZ = math.random(50, 150)
-		offsetX = offsetX * -1 ^ (offsetX % 2)
-		offsetZ = offsetZ * -1 ^ (offsetZ % 2)
-		GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD.FIGHT, {spot.x + offsetX, 0, spot.z + offsetZ}, {}}, 30)
+function gadget:UnitUnloaded(unitID, unitDefID, teamID, transportID, transportTeam)
+	if AI_TEAMS[teamID] then
+		if unitDefID == C3_ID then
+			--Spring.Echo("C3!")
+			Spam(dropZoneIDs[teamID], teamID)
+		elseif UnitDefs[unitDefID].canFly then
+			--Spring.Echo("VTOL!")
+			for _, spot in pairs(flagSpots) do
+				GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD.PATROL, {spot.x, 0, spot.z}, {"shift"}}, 30)
+			end
+		else
+			--Spring.Echo("VEHICLE OR MECH!")
+			local spot = flagSpots[math.random(1, #flagSpots)]
+			local offsetX = math.random(50, 150)
+			local offsetZ = math.random(50, 150)
+			offsetX = offsetX * -1 ^ (offsetX % 2)
+			offsetZ = offsetZ * -1 ^ (offsetZ % 2)
+			GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD.FIGHT, {spot.x + offsetX, 0, spot.z + offsetZ}, {}}, 30)
+		end
 	end
 end
 
 function gadget:UnitGiven(unitID, unitDefID, newTeam, oldTeam)
 	if AI_TEAMS[newTeam] then
-		if unitDefID == UnitDefNames["beacon"].id then
+		if unitDefID == BEACON_ID then
 			GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD_C3, {}, {}}, 1)
 		end
 	end
