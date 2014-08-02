03d39f0c88d857b541f659f7a953b5690d5ed5bd
flozi <FLOZi@users.noreply.github.com>
2014-08-02 12:55:10 +0000

    * Athlete mode for SpamBot

--- LuaRules/Gadgets/ai_spamBot.lua
+++ LuaRules/Gadgets/ai_spamBot.lua
@@ -19,9 +19,10 @@ local modOptions = Spring.GetModOptions()
 local difficulty = tonumber(modOptions and modOptions.ai_difficulty or "1")
 
 local DIREWOLF_ID = UnitDefNames["cl_direwolf_prime"].id
-local DEVASTATOR_ID = UnitDefNames["is_devastator_dvs2"].id
 local ATLAS_ID = UnitDefNames["is_atlas_as7k"].id
 
+local SHADOWCAT_ID = UnitDefNames["cl_shadowcat_prime"].id
+local OSIRIS_ID = UnitDefNames["is_osiris_osr3d"].id
 
 local AI_TEAMS = {}
 local BEACON_ID = UnitDefNames["beacon"].id
@@ -29,6 +30,7 @@ local C3_ID = UnitDefNames["upgrade_c3array"].id
 --local DelayCall = GG.Delay.DelayCall
 local CMD_SEND_ORDER = GG.CustomCommands.GetCmdID("CMD_SEND_ORDER")
 local CMD_JUMP = GG.CustomCommands.GetCmdID("CMD_JUMP")
+local CMD_MASC = GG.CustomCommands.GetCmdID("CMD_MASC")
 
 local PERK_JUMP_RANGE = GG.CustomCommands.GetCmdID("PERK_JUMP_RANGE")
 
@@ -72,7 +74,11 @@ local function Spam(teamID)
 			--Spring.Echo("COMPARING:", orderSizes[teamID], GG.TeamSlotsRemaining(teamID))
 			local buildID
 			if difficulty > 1 then
-				buildID = (sides[teamID] == "clans" and -DIREWOLF_ID) or -ATLAS_ID --DEVASTATOR_ID
+				if difficulty == 3 then
+					buildID = (sides[teamID] == "clans" and -DIREWOLF_ID) or -ATLAS_ID
+				elseif difficulty == 2 then
+					buildID = (sides[teamID] == "clans" and -SHADOWCAT_ID) or -OSIRIS_ID
+				end
 				Spring.AddTeamResource(teamID, "energy", 100)
 			else
 				local cmdDesc = cmdDescs[math.random(1, #cmdDescs)]
@@ -116,6 +122,41 @@ function gadget:UnitDestroyed(unitID, unitDefID, teamID)
 	end
 end
 
+local function RunAndJump(unitID, unitDefID)
+	local jumpLength = 900 -- TODO: unhardcode this
+	local jumpReload = 10
+	local speed = UnitDefs[unitDefID].speed
+	local distCovered = speed * jumpReload
+	-- find target
+	local spot = flagSpots[math.random(1, #flagSpots)]
+	local offsetX = math.random(50, 150)
+	local offsetZ = math.random(50, 150)
+	offsetX = offsetX * -1 ^ (offsetX % 2)
+	offsetZ = offsetZ * -1 ^ (offsetZ % 2)
+	local target = {x = spot.x + offsetX, z = spot.z + offsetZ}
+	local cx, _, cz = Spring.GetUnitPosition(unitID)
+	local targetVector = {x = target.x - cx, z = target.z - cz}
+	local targetVectorLength = math.sqrt(targetVector.x^2 + targetVector.z^2)
+	
+	local distToTarget = GG.GetUnitDistanceToPoint(unitID, target.x, Spring.GetGroundHeight(target.x,target.z), target.z, true)
+	local numSteps = math.floor(distToTarget / (jumpLength + distCovered)) * 2
+	targetVector.x = targetVector.x / targetVectorLength
+	targetVector.z = targetVector.z / targetVectorLength
+	
+	local x = cx
+	local z = cz
+	for i = 1, numSteps, 2 do
+		x = x + targetVector.x*distCovered
+		z = z + targetVector.z*distCovered
+		GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD.MOVE, {x, Spring.GetGroundHeight(x,z), z}, {"shift"}}, 1)
+		x = x + targetVector.x*jumpLength
+		z = z + targetVector.z*jumpLength
+		GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD_JUMP, {x, Spring.GetGroundHeight(x,z), z}, {"shift"}}, 1)
+	end
+	-- make sure last command is a jump onto beacon
+	GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD_JUMP, {target.x, Spring.GetGroundHeight(target.x, target.z), target.z}, {"shift"}}, 1)
+end
+
 local function Wander(unitID, cmd)
 	if Spring.ValidUnitID(unitID) then
 		local spot = flagSpots[math.random(1, #flagSpots)]
@@ -157,8 +198,8 @@ function gadget:UnitUnloaded(unitID, unitDefID, teamID, transportID, transportTe
 			end
 		elseif cp.canjump then
 			--Spring.Echo("JUMP MECH!")
-			Wander(unitID, CMD_JUMP)
 			Perk(unitID, PERK_JUMP_RANGE)
+			RunAndJump(unitID, unitDefID)
 		else
 			--Spring.Echo("VEHICLE OR MECH!")
 			Wander(unitID)
@@ -177,20 +218,34 @@ function gadget:UnitGiven(unitID, unitDefID, newTeam, oldTeam)
 	end
 end
 
+local function UnitIdleCheck(unitID, unitDefID, teamID)
+	local cmdQueueSize = Spring.GetCommandQueue(unitID, -1, false)
+	if cmdQueueSize > 0 then 
+		--Spring.Echo("I'm so not idle!") 
+		return
+	end
+	local ud = UnitDefs[unitDefID]
+	local cp = ud.customParams
+	if cp.unittype then
+		-- random chance a idle unit will wander somewhere else
+		local chance = math.random(1, 100)
+		if chance < 75 then
+			--Spring.Echo(UnitDefs[unitDefID].name .. [[ "Fuck it, I'm off for a wander"]])
+			if cp.canjump then
+				GG.Delay.DelayCall(RunAndJump, {unitID, unitDefID}, 30 * 25)
+			else
+				GG.Delay.DelayCall(Wander, {unitID}, 30 * 25)
+			end
+		else
+			--Spring.Echo(UnitDefs[unitDefID].name .. [[ "I think I'll stay here..."]])
+		end
+	end
+end
+
+	
 function gadget:UnitIdle(unitID, unitDefID, teamID)
 	if AI_TEAMS[teamID] then
-		local ud = UnitDefs[unitDefID]
-		local cp = ud.customParams
-		if cp.unittype then
-			-- random chance a idle unit will wander somewhere else
-			local chance = math.random(1, 100)
-			if chance < 75 then
-				--Spring.Echo(UnitDefs[unitDefID].name .. [[ "Fuck it, I'm off for a wander"]])
-				GG.Delay.DelayCall(Wander, {unitID, cp.canjump and CMD_JUMP}, 30 * 25)
-			else
-				--Spring.Echo(UnitDefs[unitDefID].name .. [[ "I think I'll stay here..."]])
-			end
-		end
+		GG.Delay.DelayCall(UnitIdleCheck, {unitID, unitDefID, teamID}, 10)
 	end
 end
 
