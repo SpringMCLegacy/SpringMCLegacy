fc3cc1ca0d457c897e4512e75ea6d6041d18fe25
flozi <flozi@ef3f1c50-5774-440b-a771-62f173def817>
2014-08-04 01:49:14 +0000

    * First draft of coolant CEG (thanks Forb) * Make cmd_distIcon.lua and mcl_gui_ticketCounter.lua respect user settings * Add blank C3 sounds to suppress warnings

---
+++ effects/Coolant.lua
@@ -0,0 +1,33 @@
+return {
+  ["greengoo"] = {
+    dirtg = {
+      air                = true,
+      class              = [[CSimpleParticleSystem]],
+      count              = 2, -- how much goo?
+      ground             = true,
+	  water				 = true,
+      properties = {
+        airdrag            = 0.7,
+        alwaysvisible      = true,
+        colormap           = [[0 0.5 0 1.0	0 0.25 0 0.5]],
+        directional        = true,
+        emitrot            = 45,
+        emitrotspread      = 32,
+        emitvector         = [[0, -1, 0]],
+        gravity            = [[0, -0.3, 0]], -- Adjust Y value to determine how fast the goo falls
+        numparticles       = 4,
+        particlelife       = 60, -- How long the goo falls
+        particlelifespread = 20,
+        particlesize       = 1,
+        particlesizespread = 2,
+        particlespeed      = 1,
+        particlespeedspread = 4, -- How far out the goo will spread
+        sizegrowth         = 1,
+        sizemod            = 0.9,
+        texture            = [[new_dirta]],
+        useairlos          = false,
+		colorchange			= "stuffs",
+      },
+    },
+  },
+}
