1b6610dde1a794239fd251a5584f6245d66731f9
flozi <FLOZi@users.noreply.github.com>
2014-08-05 11:07:40 +0000

    * Some coolant improvements(?)

--- effects/Coolant.lua
+++ effects/Coolant.lua
@@ -11,20 +11,20 @@ return {
         alwaysvisible      = true,
         colormap           = [[0.32 0.46 0.35 1  0.45 0.60 0.44 0.5]],
         directional        = true,
-        emitrot            = 45,
+        emitrot            = "ir45",
         emitrotspread      = 32,
-        emitvector         = [[0, -1, 0]],
-        gravity            = [[0, -0.3, 0]], -- Adjust Y value to determine how fast the goo falls
+        emitvector         = [[0, 1, 0]],
+        gravity            = [[0, -0.7, 0]], -- Adjust Y value to determine how fast the goo falls 0.3
         numparticles       = 4,
-        particlelife       = 60, -- How long the goo falls
-        particlelifespread = 20,
-        particlesize       = 1,
-        particlesizespread = 2,
-        particlespeed      = 1,
-        particlespeedspread = 4, -- How far out the goo will spread
-        sizegrowth         = 1,
+        particlelife       = "ir60", -- How long the goo falls
+        particlelifespread = "ir20",
+        particlesize       = "ir1",
+        particlesizespread = "ir3",--2,
+        particlespeed      = 5,--1,
+        particlespeedspread = "r6", -- How far out the goo will spread
+        sizegrowth         = 1.1,--1,
         sizemod            = 0.9,
-        texture            = "new_dirta",
+        texture            = "coolant", --"new_dirta",
         useairlos          = false,
       },
     },
