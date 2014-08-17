d72dfb5f07d9c7909adc19b893def9db188e02d6
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2014-08-17 14:50:11 +0000

    Chevalier and Galleon IS vehicles

---
+++ units/is/Vehicles/IS_Galleon.lua
@@ -0,0 +1,41 @@
+local IS_Galleon = LightTank:New{
+	name              	= "Galleon",
+	description         = "Medium Skirmish Tank",
+	objectName        	= "IS_Galleon.s3o",
+	corpse				= "IS_Galleon_X",
+	maxDamage           = 3500,
+	mass                = 4500,
+	trackWidth			= 23,--width to render the decal
+	buildCostEnergy     = 45,
+	buildCostMetal      = 10710,
+	maxVelocity		= 3.2, --96kph/30
+	maxReverseVelocity= 1.6,
+	acceleration    = 0.8,
+	brakeRate       = 0.1,
+	turnRate 		= 450,
+
+	weapons	= {	
+		[1] = {
+			name	= "MBL",
+		},
+		[2] = {
+			name	= "SBL",
+		},
+		[3] = {
+			name	= "SBL",
+		},
+	},
+	
+	customparams = {
+		helptext		= "Armament: 1 x Medium Beam Laser, 2 x Small Beam Laser - Armor: 3.5 tons",
+		heatlimit		= 20,
+		turrettunspeed  = 200,
+		elevationspeed  = 200,
+		wheelspeed      = 200,
+		squadsize 		= 4,
+    },
+}
+
+return lowerkeys({
+	["IS_Galleon"] = IS_Galleon,
+})
