74b7e35e85c23c254711d422ed992a8a0ad2a374
mechwarspring <mechwarspring@ef3f1c50-5774-440b-a771-62f173def817>
2014-08-17 19:22:28 +0000

    Clan Mithras light tank added, also tweaked Donar HP (nerf) and tweaked all vehicle turret health (to 100% that of its hull), may require further nerfing all vehicle health.

---
+++ units/cl/Vehicles/CL_Mithras.lua
@@ -0,0 +1,40 @@
+local CL_Mithras = LightTank:New{
+	name              	= "Enyo",
+	description         = "Light Strike Tank",
+	objectName        	= "CL_Mithras.s3o",
+	corpse				= "CL_Mithras_X",
+	maxDamage           = 3000,
+	mass                = 2500,
+	trackWidth			= 24,--width to render the decal
+	buildCostEnergy     = 55,
+	buildCostMetal      = 15120,
+	maxVelocity		= 3.23, --97kph/30
+	maxReverseVelocity= 1.6,
+	acceleration    = 0.9,
+	brakeRate       = 0.1,
+	turnRate 		= 500,
+	
+	weapons 		= {	
+		[1] = {
+			name	= "UAC2",
+		},
+		[2] = {
+			name	= "CERMBL",
+		},
+		[3] = {
+			name	= "CERMBL",
+		},
+	},
+	customparams = {
+		helptext		= "Armament: 1 x Ultra AC/2, 2 x Clan ER Medium Beam Laser - Armor: 3 tons",
+		heatlimit		= 28,
+		turretturnspeed = 100,
+		elevationspeed  = 150,
+		maxammo = {ac2 = 300},
+		squadsize 		= 4,
+    },
+}
+
+return lowerkeys({
+	["CL_Mithras"] = CL_Mithras,
+})
