193b278169a5caf1794ac542d802d82487ab01c9
zverofaust <misanthrope099@hotmail.com>
2015-05-15 13:34:44 -0400

    Ku medium Clan tank, possibly the ugliest vehicle ever???

---
+++ features/corpses/CL_Ku_X.lua
@@ -0,0 +1,18 @@
+local featureDefs = {
+  CL_Ku_X = {
+    blocking           = false,
+    category           = "corpses",
+    damage             = 900,
+    description        = "Wreckage",
+    featureDead        = "Wreck_X",
+    footprintX         = 3,
+    footprintZ         = 3,
+    height             = "25",
+    hitdensity         = "50",
+    metal              = 2000,
+    object             = "CL_Ku_X.s3o",
+    reclaimable        = true,
+    world              = "All Worlds",
+  },
+}
+return featureDefs
---
+++ units/cl/Vehicles/CL_Ku.lua
@@ -0,0 +1,40 @@
+local CL_Ku = LightTank:New{
+	name              	= "Ku",
+	description         = "Medium Brawler Tank",
+	objectName        	= "CL_Ku.s3o",
+	corpse				= "CL_Ku_X",
+	maxDamage           = 9000,
+	mass                = 5000,
+	trackWidth			= 24,--width to render the decal
+	buildCostEnergy     = 50,
+	buildCostMetal      = 18120,
+	maxVelocity		= 2.2, --97kph/30
+	maxReverseVelocity= 1.2,
+	acceleration    = 0.9,
+	brakeRate       = 0.1,
+	turnRate 		= 500,
+	
+	weapons 		= {	
+		[1] = {
+			name	= "SSRM4",
+		},
+		[2] = {
+			name	= "LBX10",
+		},
+		[3] = {
+			name	= "CERLBL",
+		},
+	},
+	customparams = {
+		helptext		= "Armament: 1 x LBX-10, 1 x ER Large Beam Laser, 1 x SSRM-4 - Armor: 7 tons",
+		heatlimit		= 22,
+		turretturnspeed = 100,
+		elevationspeed  = 150,
+		maxammo = {ac10 = 40, srm = 120},
+		squadsize 		= 3,
+    },
+}
+
+return lowerkeys({
+	["CL_Ku"] = CL_Ku,
+})
