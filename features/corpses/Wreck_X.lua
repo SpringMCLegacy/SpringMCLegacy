b57331b3917fdd0329602dc78f8c632d1e3c8cca
zverofaust <misanthrope099@hotmail.com>
2015-05-15 13:34:23 -0400

    And the rubble featuredef.....

---
+++ features/corpses/Wreck_X.lua
@@ -0,0 +1,17 @@
+local featureDefs = {
+  Wreck_X = {
+    blocking           = false,
+    category           = "corpses",
+    damage             = 10000,
+    description        = "Rubble",
+    footprintX         = 3,
+    footprintZ         = 3,
+    height             = "20",
+    hitdensity         = "50",
+    metal              = 2000,
+    object             = "Wreck_X.s3o",
+	noSelect           = true,
+    world              = "All Worlds",
+  },
+}
+return featureDefs
