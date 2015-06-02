596c121a8dbb0e0bfec45232d43e4cd99aa2fd2e
zverofaust <misanthrope099@hotmail.com>
2015-06-02 12:59:54 -0400

    Draconis Akuma assault Mech (like an offshoot of the Atlas so I used the same model!!!!!! OMG)

---
+++ units/is/Mechs/Assault/Akuma.lua
@@ -0,0 +1,48 @@
+local Akuma = Assault:New{
+	name				= "Akuma",
+	
+	customparams = {
+		tonnage			= 90,
+    },
+}
+
+local AKUX1 = Akuma:New{
+	description         = "Assault Brawler",
+	weapons = {	
+		[1] = {
+			name	= "LBX10",
+		},
+		[2] = {
+			name	= "ERPPC",
+		},
+		[3] = {
+			name	= "MBL",
+		},
+		[4] = {
+			name	= "MPL",
+		},
+		[5] = {
+			name	= "SSRM4",
+		},
+		[6] = {
+			name	= "SSRM6",
+		},
+		[7] = {
+			name	= "MRM30",
+		},
+	},
+		
+	customparams = {
+		variant			= "AKU-X1",
+		speed			= 50,
+		price			= 19590,
+		heatlimit 		= 26,
+		armor			= {type = "standard", tons = 17.5},
+		maxammo 		= {ac10 = 2, srm = 2, mrm = 2},
+		barrelrecoildist = {[1] = 5},
+    },
+}
+
+return lowerkeys({
+	["DC_Akuma_AKUX1"] = AKUX1:New(),
+})
