479231739c20c56fa1f86bb436f9070a01338ccb
zverofaust <misanthrope099@hotmail.com>
2015-06-04 11:58:54 -0400

    Galleon factionalized for testing (commented out at bottom of def)

---
+++ units/is/Vehicles/Light/Galleon.lua
@@ -0,0 +1,37 @@
+local Galleon = LightTank:New{
+	name              	= "Galleon",
+	trackWidth			= 20,
+	
+	customparams = {
+		tonnage			= 30,
+    },
+}
+
+local GAL102 = Galleon:New{
+	description         = "Troop Transport",
+	
+	weapons	= {	
+		[1] = {
+			name	= "MPL",
+		},
+		[2] = {
+			name	= "MBL",
+		},
+		[3] = {
+			name	= "MBL",
+		},
+	},
+	
+customparams = {
+		variant         = "GAL102",
+		speed			= 90,
+		price			= 1620,
+		heatlimit 		= 10,
+		armor			= {type = "standard", tons = 3.5},
+		squadsize 		= 2,
+    },
+}
+
+return lowerkeys({
+	--["FW_Galleon_GAL102"] = GAL102:New(),
+})
