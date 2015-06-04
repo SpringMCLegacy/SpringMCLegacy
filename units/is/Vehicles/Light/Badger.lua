9b6f70731e01071fed727dd4a83b1171a70ac2b0
zverofaust <misanthrope099@hotmail.com>
2015-06-04 12:36:18 -0400

    Badger factionized

---
+++ units/is/Vehicles/Light/Badger.lua
@@ -0,0 +1,45 @@
+local Badger = LightTank:New{
+	name              	= "Badger",
+	trackWidth			= 20,
+	
+	customparams = {
+		tonnage			= 30,
+    },
+}
+
+local Prime = Badger:New{
+	description         = "Troop Transport",
+	
+	transportCapacity		= 5,
+	transportSize = 1,	
+	
+	weapons	= {	
+		[1] = {
+			name	= "MBL",
+		},
+		[2] = {
+			name	= "MBL",
+		},
+		[3] = {
+			name	= "MBL",
+		},
+		[4] = {
+			name	= "SRM2",
+		},
+	},
+	
+customparams = {
+		variant         = "Prime",
+		speed			= 80,
+		price			= 3340,
+		heatlimit 		= 10,
+		armor			= {type = "standard", tons = 5},
+		maxammo 		= {srm = 1},
+		unittype 		= "apc",
+		squadsize 		= 2,
+    },
+}
+
+return lowerkeys({
+	["FS_Badger_Prime"] = Prime:New(),
+})
