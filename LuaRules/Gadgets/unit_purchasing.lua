SpringMCLegacy/SpringMCLegacy

commit aa31be9e076516da6febfdea5d9e0c94bac1d1e8

* Make insurance a modoption (and separate income options out into their own section) * "Make sure nuking isn't giving the nuked mech money for damage applied to the exploding mech via the nuke;" * "Make sure nuker is getting paid for damage caused to enemy mech that goes nuke;" * "On that topic, make sure damage caused to ANYTHING by a nuke doesn't earn money for the owner of the Mech that nuked (I shouldn't get paid for causing a massive ecological disaster)"

LuaRules/Gadgets/unit_purchasing.lua
@@ -50,9 +50,11 @@ local C3_ID = UnitDefNames["upgrade_c3array"].id
 local DROPSHIP_COOLDOWN = 30 * 30 -- 30s, time before the dropship has regained orbit, refuelled etc ready to drop again
 local DROPSHIP_DELAY = 2 * 30 -- 2s, time taken to arrive on the map from SPACE!
 local DAMAGE_REWARD_MULT = (modOptions and tonumber(modOptions.income_damage)) or 1
-local KILL_REWARD_MULT = 0.0
-local INSURANCE_MULT = 0.5
+Spring.SetGameRulesParam("damage_reward_mult", DAMAGE_REWARD_MULT)
+local INSURANCE_MULT = (modOptions and tonumber(modOptions.insurance)) or 0.5
+Spring.SetGameRulesParam("insurance_mult", INSURANCE_MULT)
 
+--local KILL_REWARD_MULT = 0.0
 -- local NUM_ICONS_PER_PAGE = 3 * 8
 	
 local CMD_SEND_ORDER = GG.CustomCommands.GetCmdID("CMD_SEND_ORDER")
@@ -536,10 +538,14 @@ end
 
 local WALL_ID = UnitDefNames["wall"].id
 local GATE_ID = UnitDefNames["wall_gate"].id
+local MELTDOWN = WeaponDefNames["meltdown"].id
 
 function gadget:UnitDamaged(unitID, unitDefID, teamID, damage, paralyzer, weaponID,  projectileID, attackerID, attackerDefID, attackerTeam)
 	if attackerID and attackerTeam and not AreTeamsAllied(teamID, attackerTeam) and unitDefID ~= WALL_ID and unitDefID ~= GATE_ID then
-		AddTeamResource(attackerTeam, "metal", damage * DAMAGE_REWARD_MULT)
+		-- don't allow income from nukes
+		if not (weaponID and weaponID == MELTDOWN) then		
+			AddTeamResource(attackerTeam, "metal", damage * DAMAGE_REWARD_MULT)
+		end
 	end
 end
 

scripts/Mech.lua
@@ -170,7 +170,7 @@ function ChangeHeat(amount)
 	if currHeatLevel > heatLimit then
 		excessHeat = excessHeat + currHeatLevel - heatLimit
 		currHeatLevel = heatLimit
-		if excessHeat >= heatLimit then
+		if excessHeat >= heatLimit * 2 then
 			Spring.DestroyUnit(unitID, true)
 		end
 	elseif currHeatLevel < 0 then
@@ -343,10 +343,10 @@ function hideLimbPieces(limb, hide)
 		RecursiveHide(rootPiece, hide)
 	else -- legs
 		if hide then
-			Spring.Echo("Lost a leg! halving move speed")
+			--Spring.Echo("Lost a leg! halving move speed")
 			speedMod = speedMod / 2
 		else -- leg is restored
-			Spring.Echo("Regained a leg! doubling move speed")
+			--Spring.Echo("Regained a leg! doubling move speed")
 			speedMod = speedMod * 2
 		end
 		GG.SpeedChange(unitID, unitDefID, speedMod)
@@ -358,7 +358,7 @@ function hideLimbPieces(limb, hide)
 		for id, valid in pairs(limbWeapons) do
 			if valid then
 				local weapDef = WeaponDefs[unitDef.weapons[id].weaponDef]
-				Spring.Echo(unitDef.humanName .. ": " .. weapDef.name .. " destroyed!")
+				--Spring.Echo(unitDef.humanName .. ": " .. weapDef.name .. " destroyed!")
 				ToggleWeapon(id, 1)
 			end
 		end
@@ -632,7 +632,7 @@ function script.QueryWeapon(weaponID)
 end
 
 function script.Killed(recentDamage, maxHealth)
-	if excessHeat >= heatLimit then
+	if excessHeat >= heatLimit * 2 then
 		--Spring.Echo("NUUUUUUUUUUUKKKKKE")
 		local x,y,z = Spring.GetUnitPosition(unitID)
 		-- This is a really awful hack , built on top of another hack. 
@@ -641,6 +641,14 @@ function script.Killed(recentDamage, maxHealth)
 		local ownerID = Spring.GetTeamUnitsByDefs(teamID, UnitDefNames["decal_beacon"].id)[1] or unitID
 		local nukeID = Spring.SpawnProjectile(WeaponDefNames["meltdown"].id, {pos = {x,y,z}, owner = ownerID, team = teamID, ttl = 20})
 		Spring.SetProjectileAlwaysVisible(nukeID, true)
+		-- reward the attacker for the HP destroyed
+		local attackerID = Spring.GetUnitLastAttacker(unitID)
+		if attackerID then
+			local attackerTeam = Spring.GetUnitTeam(attackerID)
+			local hp = Spring.GetUnitHealth(unitID)
+			local payout = hp * Spring.GetGameRulesParam("damage_reward_mult")
+			Spring.AddTeamResource(attackerTeam, "metal", payout)
+		end
 	end
 	GG.PlaySoundForTeam(Spring.GetUnitTeam(unitID), "BB_BattleMech_destroyed", 1)
 	--local severity = recentDamage / maxHealth * 100

modoptions.lua
@@ -25,30 +25,8 @@ local options = {
 	{
 		key    = '2gamemode',
 		name   = 'Gameplay Settings',
-		desc   = 'Sets options regarding income and winning conditions',
+		desc   = 'Sets options regarding winning conditions',
 		type   = 'section',
-	},
-	{
-		key		= "income",
-		name		= "Income",
-		desc		= "How many CBills per second?",
-		type		= "number",
-		def		= 10,
-		min		= 1,
-		max		= 1000,
-		step	= 1,
-		section	= '2gamemode',
-	},
-	{
-		key		= "income_damage",
-		name	= "Damage Income (Multiplier)",
-		desc	= "How many CBills per HP of damage?",
-		type	= "number",
-		def		= 1,
-		min		= 0.1,
-		max		= 10.0,
-		step	= 0.1,
-		section	= '2gamemode',
 	},
 	{
 		key 	= "start_tickets",
@@ -81,6 +59,45 @@ local options = {
 		min = 0.1,
 		max = 100,
 		section = "2gamemode",
+	},
+	{
+		key    = '2income',
+		name   = 'Income Settings',
+		desc   = 'Sets options regarding income',
+		type   = 'section',
+	},
+	{
+		key		= "income",
+		name		= "Income",
+		desc		= "How many CBills per second?",
+		type		= "number",
+		def		= 10,
+		min		= 1,
+		max		= 1000,
+		step	= 1,
+		section	= '2gamemode',
+	},
+	{
+		key		= "income_damage",
+		name	= "Damage Income (Multiplier)",
+		desc	= "How many CBills per HP of damage?",
+		type	= "number",
+		def		= 1,
+		min		= 0.1,
+		max		= 10.0,
+		step	= 0.1,
+		section	= '2gamemode',
+	},
+	{
+		key		= "insurance",
+		name	= "Insurance (Multiplier)",
+		desc	= "How many CBills returned to owner when a mech dies",
+		type	= "number",
+		def		= 0.5,
+		min		= 0.1,
+		max		= 1.0,
+		step	= 0.1,
+		section	= '2gamemode',
 	},
 	------------------------------------
 	{

commit d209cf0e1d0f3fc136b04b377baae7464637d227

* " Vehicles shouldn't earn money (turrets too, and possibly dropships);" * Fix a minor oversight re: doubling excessheat limit before nuking (introduced in aa31be9e076516da6febfdea5d9e0c94bac1d1e8)

LuaRules/Gadgets/unit_purchasing.lua
@@ -542,9 +542,12 @@ local MELTDOWN = WeaponDefNames["meltdown"].id
 
 function gadget:UnitDamaged(unitID, unitDefID, teamID, damage, paralyzer, weaponID,  projectileID, attackerID, attackerDefID, attackerTeam)
 	if attackerID and attackerTeam and not AreTeamsAllied(teamID, attackerTeam) and unitDefID ~= WALL_ID and unitDefID ~= GATE_ID then
-		-- don't allow income from nukes
-		if not (weaponID and weaponID == MELTDOWN) then		
-			AddTeamResource(attackerTeam, "metal", damage * DAMAGE_REWARD_MULT)
+		local attackerDefType = attackerDefID and UnitDefs[attackerDefID].customParams.unittype or ""
+		if attackerDefType == "mech" then -- only mechs generate income
+			-- don't allow income from nukes
+			if not (weaponID and weaponID == MELTDOWN) then		
+				AddTeamResource(attackerTeam, "metal", damage * DAMAGE_REWARD_MULT)
+			end
 		end
 	end
 end

scripts/Mech.lua
@@ -177,7 +177,7 @@ function ChangeHeat(amount)
 		currHeatLevel = 0
 	end
 	SetUnitRulesParam(unitID, "heat", math.floor(100 * currHeatLevel / heatLimit))
-	SetUnitRulesParam(unitID, "excess_heat", math.floor(100 * excessHeat / heatLimit))
+	SetUnitRulesParam(unitID, "excess_heat", math.floor(100 * excessHeat / (2 * heatLimit)))
 end
 
 local function CoolOff()
@@ -638,8 +638,8 @@ function script.Killed(recentDamage, maxHealth)
 		-- This is a really awful hack , built on top of another hack. 
 		-- There's some issue with alwaysVisible not working (http://springrts.com/mantis/view.php?id=4483)
 		-- So instead make the owner the decal unit spawned by the teams starting beacon, as it can never die
-		local ownerID = Spring.GetTeamUnitsByDefs(teamID, UnitDefNames["decal_beacon"].id)[1] or unitID
-		local nukeID = Spring.SpawnProjectile(WeaponDefNames["meltdown"].id, {pos = {x,y,z}, owner = ownerID, team = teamID, ttl = 20})
+		--local ownerID = Spring.GetTeamUnitsByDefs(teamID, UnitDefNames["decal_beacon"].id)[1] or unitID
+		local nukeID = Spring.SpawnProjectile(WeaponDefNames["meltdown"].id, {pos = {x,y,z}, owner = unitID, team = teamID, ttl = 20})
 		Spring.SetProjectileAlwaysVisible(nukeID, true)
 		-- reward the attacker for the HP destroyed
 		local attackerID = Spring.GetUnitLastAttacker(unitID)