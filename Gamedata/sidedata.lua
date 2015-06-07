c49034cd800739028aae42873c528c280e86e9c1
Craig Lawrence <flozi@blueyonder.co.uk>
2015-06-07 17:06:01 +0100

    * Remove old unused sides

--- Gamedata/sidedata.lua
+++ Gamedata/sidedata.lua
@@ -1,15 +1,5 @@
 local sidedata = {
 	{
-		name = "Inner Sphere",
-		shortName = "IS",
-		startUnit = "beacon",
-	},
-	{
-		name = "Clans",
-		shortName = "CL",
-		startUnit = "beacon",
-	},
-	{
 		name = "Federated Suns",
 		shortName = "FS",
 		startUnit = "beacon",
