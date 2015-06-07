3816a4831889ae41c37e632a766f7d07bed5b080
Craig Lawrence <flozi@blueyonder.co.uk>
2015-06-07 17:12:00 +0100

    Revert "* Remove old unused sides"
    This reverts commit c49034cd800739028aae42873c528c280e86e9c1.

--- Gamedata/sidedata.lua
+++ Gamedata/sidedata.lua
@@ -1,5 +1,15 @@
 local sidedata = {
 	{
+		name = "Inner Sphere",
+		shortName = "IS",
+		startUnit = "beacon",
+	},
+	{
+		name = "Clans",
+		shortName = "CL",
+		startUnit = "beacon",
+	},
+	{
 		name = "Federated Suns",
 		shortName = "FS",
 		startUnit = "beacon",
