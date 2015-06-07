fd2a70f2c63b8f1542cb86c158b80968287f9d5f
zverofaust <misanthrope099@hotmail.com>
2015-06-07 19:32:59 -0400

    * Switched Firestarter from FedSuns to Lyrans. * Gave FedSuns Fireball to make up for it.

---
+++ units/is/Mechs/Light/Fireball.lua
@@ -0,0 +1,31 @@
+local Fireball = Light:New{
+	name              	= "Fireball",
+	customparams = {
+		tonnage			= 20,
+    },
+}
+
+local ALM9D = Fireball:New{
+	description         = "Light Skirmisher",
+	weapons = {	
+		[1] = {
+			name	= "MBL",
+		},
+		[2] = {
+			name	= "SSRM2",
+		},
+	},
+		
+	customparams = {
+		variant         = "ALM-9D",
+		speed			= 170,
+		price			= 5270,
+		heatlimit 		= 10,
+		armor			= {type = "standard", tons = 4.5},
+		maxammo 		= {srm = 1},
+    },
+}
+
+return lowerkeys({
+	["FS_Fireball_ALM9D"] = ALM9D:New(),
+})
--- units/is/Mechs/Light/Firestarter.lua
+++ units/is/Mechs/Light/Firestarter.lua
@@ -46,5 +46,5 @@ local FS9S = Firestarter:New{
 }
 
 return lowerkeys({
-	["FS_Firestarter_FS9S"] = FS9S:New(),
+	["LA_Firestarter_FS9S"] = FS9S:New(),
 })
