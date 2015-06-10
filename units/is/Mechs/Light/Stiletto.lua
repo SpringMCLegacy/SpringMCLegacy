fbf964a2c9dbe95f31b74502458f7596cc69e103
zverofaust <misanthrope099@hotmail.com>
2015-06-10 17:45:44 -0400

    Someone asked for Lyran ECM light. Tada.

---
+++ units/is/Mechs/Light/Stiletto.lua
@@ -0,0 +1,35 @@
+local Stiletto = Light:New{
+	name				= "Stiletto",
+	customparams = {
+		tonnage			= 35,
+    },
+}
+
+local STO4A = Stiletto:New{
+	description         = "Light EWAR/Skirmisher",
+	weapons	= {	
+		[1] = {
+			name	= "SSRM2",
+		},
+		[2] = {
+			name	= "SSRM2",
+		},
+		[3] = {
+			name	= "LRM5",
+		},
+	},
+		
+	customparams = {
+		variant         = "FLE-17",
+		speed			= 120,
+		price			= 8590,
+		heatlimit 		= 20,
+		armor			= {type = "ferro", tons = 6},
+		ecm 			= true,
+		maxammo 		= {lrm = 1, srm = 1},
+    },
+}
+
+return lowerkeys({
+	["LA_Stiletto_STO4A"] = STO4A:New(),
+})
