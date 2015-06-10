0fa8a5073cc22217c3e9e42b7b6b01a43cf6f36c
zverofaust <misanthrope099@hotmail.com>
2015-06-10 14:47:31 -0400

    Added some map profiles, updated faction table

---
+++ maps/flagConfig/Cattle and Loveplay NO WORMS 1_profile.lua
@@ -0,0 +1,39 @@
+local resources = {
+	{
+		x = 4605,
+		z = 1130,
+		feature = nil
+	},
+	{
+		x = 1360,
+		z = 4005,
+		feature = nil
+	},
+	{
+		x = 7685,
+		z = 3930,
+		feature = nil
+	},
+	{
+		x = 4035,
+		z = 4300,
+		feature = nil
+	},
+	{
+		x = 5240,
+		z = 4290,
+		feature = nil
+	},
+	{
+		x = 2420,
+		z = 5646,
+		feature = nil
+	},
+	{
+		x = 6935,
+		z = 5635,
+		feature = nil
+	},
+}
+
+return resources
---
+++ maps/flagConfig/Whakamutunga_Riri-v05_profile.lua
@@ -0,0 +1,44 @@
+local resources = {
+	{
+		x = 4543,
+		z = 2585,
+		feature = nil
+	},
+	{
+		x = 4375,
+		z = 6060,
+		feature = nil
+	},
+	{
+		x = 2925,
+		z = 4385,
+		feature = nil
+	},
+	{
+		x = 6395,
+		z = 4380,
+		feature = nil
+	},
+	{
+		x = 1990,
+		z = 6180,
+		feature = nil
+	},
+	{
+		x = 1355,
+		z = 2455,
+		feature = nil
+	},
+	{
+		x = 7200,
+		z = 5990,
+		feature = nil
+	},
+	{
+		x = 6525,
+		z = 2150,
+		feature = nil
+	},
+}
+
+return resources
