dc64184b7f3986e435eac24a36354a8f81b0c69d
zverofaust <misanthrope099@hotmail.com>
2015-06-17 12:32:39 -0400

    couple new flag profiles

---
+++ maps/flagConfig/Arena v1_profile.lua
@@ -0,0 +1,24 @@
+local resources = {
+	{
+		x = 5570,
+		z = 5480,
+		feature = nil
+	},
+	{
+		x = 5665,
+		z = 1690,
+		feature = nil
+	},
+	{
+		x = 1510,
+		z = 1805,
+		feature = nil
+	},
+	{
+		x = 9660,
+		z =  2035,
+		feature = nil
+	},
+}
+
+return resources
---
+++ maps/flagConfig/The river Nix 20_profile.lua
@@ -0,0 +1,24 @@
+local resources = {
+	{
+		x = 6020,
+		z = 7425,
+		feature = nil
+	},
+	{
+		x = 6295,
+		z = 11675,
+		feature = nil
+	},
+	{
+		x = 5980,
+		z = 460,
+		feature = nil
+	},
+	{
+		x = 6020,
+		z = 3970,
+		feature = nil
+	},
+}
+
+return resources
