a72088d295431c9022af68b5410cab38c64b2afe
zverofaust <misanthrope099@hotmail.com>
2015-06-21 11:05:15 -0400

    trying out diff Commando variant (slightly better)

--- units/is/Mechs/Light/Commando.lua
+++ units/is/Mechs/Light/Commando.lua
@@ -26,10 +26,37 @@ local COM5S = Commando:New{
 		heatlimit 		= 10,
 		armor			= {type = "ferro", tons = 3},
 		maxammo 		= {srm = 2},
-		artemis			= true,
+    },
+}
+
+local COM7S = Commando:New{
+	description         = "Light Skirmisher",
+	weapons	= {	
+		[1] = {
+			name	= "ASRM4",
+		},
+		[2] = {
+			name	= "SSRM4",
+		},
+		[3] = {
+			name	= "MBL",
+		},
+		[4] = {
+			name	= "MBL",
+		},
+	},
+		
+	customparams = {
+		variant         = "COM-7S",
+		speed			= 90,
+		price			= 6580,
+		heatlimit 		= 20,
+		armor			= {type = "standard", tons = 3.5},
+		maxammo 		= {srm = 2},
     },
 }
 
 return lowerkeys({
 	["LA_Commando_COM5S"] = COM5S:New(),
+	["LA_Commando_COM7S"] = COM7S:New(),
 })
