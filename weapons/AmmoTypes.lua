5360e7f161e0078170f13d166f9b0ffefd4ee1bc
zverofaust <misanthrope099@hotmail.com>
2015-06-22 18:00:22 -0400

    Some tweaks to ammo levels, and tweaks to Arrow missiles.

--- weapons/AmmoTypes.lua
+++ weapons/AmmoTypes.lua
@@ -1,19 +1,19 @@
 return {
-	LRM			= 90,--0.75 x 120
-	SRM			= 180,--2 x 90
+	LRM			= 100,
+	SRM			= 100,
 	ATM			= 60,
-	MRM			= 240,
-	Arrow		= 4,--0.75 x 5
+	MRM			= 200,
+	Arrow		= 8,
 	Narc		= 6,
 
-	AC2			= 225,--5 x 45
-	AC5			= 50,--2.5 x 20
-	AC10 		= 25,--2.5 x 10
-	AC20		= 10,--2 x 5
-	Thumper		= 10,--0.5 x 20
-	Sniper		= 5, --0.5 x 10
+	AC2			= 225,
+	AC5			= 50,
+	AC10 		= 25,
+	AC20		= 10,
+	Thumper		= 10,
+	Sniper		= 5, 
 
-	Gauss		= 13,--1.6 x 8
-	LtGauss		= 40,--2.5 x 16
+	Gauss		= 12,
+	LtGauss		= 40,
 	HvGauss		= 6,
 }
