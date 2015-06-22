d70c36e85d5f95c9fa8e631d960be9f5fa00a308
zverofaust <misanthrope099@hotmail.com>
2015-06-22 09:50:01 -0400

    new Kabuto for DC

---
+++ units/is/Mechs/Light/Kabuto.lua
@@ -0,0 +1,31 @@
+local Kabuto = Light:New{
+	name              	= "Kabuto",
+	customparams = {
+		tonnage 		= 20,
+    },
+}
+
+local KBO7A = Kabuto:New{
+	description         = "Light Skirmisher",
+	weapons	= {	
+		[1] = {
+			name	= "SSRM4",
+		},
+		[2] = {
+			name	= "SSRM4",
+		},
+	},
+		
+	customparams = {
+		variant         = "KBO-7A",
+		speed			= 110,
+		price			= 5240,
+		heatlimit 		= 10,
+		armor			= {type = "standard", tons = 4.5},
+		maxammo 		= {srm = 2},
+    },
+}
+
+return lowerkeys({
+	["DC_Kabuto_KBO7A"] = KBO7A:New(),
+})
