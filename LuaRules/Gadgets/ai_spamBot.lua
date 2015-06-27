22b9b75b44ed7f9e111e22c9e9ebbc6a76958761
Craig Lawrence <flozi@blueyonder.co.uk>
2015-06-28 00:03:52 +0100

    * Make default difficulty on SpamBot non resource cheating

--- LuaRules/Gadgets/ai_spamBot.lua
+++ LuaRules/Gadgets/ai_spamBot.lua
@@ -18,6 +18,7 @@ if gadgetHandler:IsSyncedCode() then
 local modOptions = Spring.GetModOptions()
 local difficulty = tonumber(modOptions and modOptions.ai_difficulty or "1")
 
+local sideMechs = {} -- sideMechs[sideShortName] = {mechDefID1, mechDefID2, ...}
 local sideJumpers = {} -- sideJumpers[sideShortName] = {jumperDefID1, jumperDefID2, ...}
 local sideAssaults = {} -- sideAssaults[sideShortName] = {assaultDefID1, assaultDefID2, ...}
 
@@ -31,7 +32,7 @@ local CMD_SEND_ORDER = GG.CustomCommands.GetCmdID("CMD_SEND_ORDER")
 local CMD_JUMP = GG.CustomCommands.GetCmdID("CMD_JUMP")
 local CMD_MASC = GG.CustomCommands.GetCmdID("CMD_MASC")
 
-local PERK_JUMP_RANGE = GG.CustomCommands.GetCmdID("PERK_JUMP_RANGE")
+local PERK_JUMP_RANGE = GG.CustomCommands.GetCmdID("PERK_JUMP_EFFICIENCY")
 
 local CMD_DROPZONE = GG.CustomCommands.GetCmdID("CMD_DROPZONE")
 local CMD_C3 = GG.CustomCommands.GetCmdID("CMD_UPGRADE_upgrade_c3array")
@@ -43,6 +44,19 @@ local orderSizes = {}
 local flagSpots = {} --VFS.Include("maps/flagConfig/" .. Game.mapName .. "_profile.lua")
 
 local teamUpgradeCounts = {} -- teamUpgradeCounts[teamID] = {c3 = 1, vpad = 1} etc
+
+local function CostSort(unitDefID1, unitDefID2)
+	return UnitDefs[unitDefID1].metalCost < UnitDefs[unitDefID2].metalCost
+end
+
+local function SimpleReverseSearch(sideTable, cost)
+	for i = #sideTable, 1, -1 do
+		local unitDefID = sideTable[i]
+		local foundCost = UnitDefs[unitDefID].metalCost
+		if foundCost < cost then return unitDefID end
+	end
+	return false -- no affordable mech!
+end
 
 function gadget:GamePreload()
 	-- Initialise unit limit for all AI teams.
@@ -64,12 +78,19 @@ function gadget:GamePreload()
 			table.insert(sideJumpers[side], unitDefID)
 			--Spring.Echo("Adding jumper mech", unitDef.name, "to", side)
 		end
-		-- identify all assault mechs by faction
-		if unitDef.customParams.unittype == "mech" and GG.GetWeight(tonumber(unitDef.customParams.tonnage) or 0) == "assault"  then
-			if not sideAssaults[side] then sideAssaults[side] = {} end
-			table.insert(sideAssaults[side], unitDefID)
-			--Spring.Echo("Adding assault mech", unitDef.name, "to", side)
+		if unitDef.customParams.unittype == "mech" then
+			if not sideMechs[side] then sideMechs[side] = {} end
+			table.insert(sideMechs[side], unitDefID)
+			-- identify all assault mechs by faction
+			if GG.GetWeight(tonumber(unitDef.customParams.tonnage) or 0) == "assault"  then
+				if not sideAssaults[side] then sideAssaults[side] = {} end
+				table.insert(sideAssaults[side], unitDefID)
+				--Spring.Echo("Adding assault mech", unitDef.name, "to", side)
+			end
 		end
+	end
+	for side, mechTable in pairs(sideMechs) do
+		table.sort(mechTable, CostSort)
 	end
 end
 
@@ -89,7 +110,6 @@ end
 local function Spam(teamID)
 	local unitID = dropZoneIDs[teamID]
 	if Spring.ValidUnitID(unitID) then
-		Spring.AddTeamResource(teamID, "metal", 10000000)
 		local cmdDescs = Spring.GetUnitCmdDescs(unitID)
 		local side = GG.teamSide[teamID]
 		orderSizes[teamID] = 0
@@ -97,6 +117,7 @@ local function Spam(teamID)
 			--Spring.Echo("COMPARING:", orderSizes[teamID], GG.TeamSlotsRemaining(teamID))
 			local buildID
 			if difficulty > 1 then
+				Spring.AddTeamResource(teamID, "metal", 10000000)
 				if difficulty == 3 then -- Assaults only
 					buildID = -sideAssaults[side][math.random(1, #sideAssaults[side])]
 				elseif difficulty == 2 then -- jumpers only
@@ -104,14 +125,13 @@ local function Spam(teamID)
 				end
 				Spring.AddTeamResource(teamID, "energy", 100)
 			else
-				local cmdDesc = cmdDescs[math.random(1, #cmdDescs)]
-				buildID = cmdDesc.id
+				--local cmdDesc = cmdDescs[math.random(1, #cmdDescs)]
+				buildID = SimpleReverseSearch(sideMechs[side], Spring.GetTeamResources(teamID, "metal") * 0.65)
 			end
-			if buildID < 0 then
-				GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, buildID, {}, {}}, 1)
-				orderSizes[teamID] = orderSizes[teamID] + 1
-				--Spring.Echo("Adding to order;", orderSizes[teamID], UnitDefs[-buildID].name, GG.TeamSlotsRemaining(teamID))
-			end
+			if not buildID then return end -- couldn't find any affordable mechs
+			GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, -buildID, {}, {}}, 1)
+			orderSizes[teamID] = orderSizes[teamID] + 1
+			--Spring.Echo("Adding to order;", orderSizes[teamID], UnitDefs[-buildID].name, GG.TeamSlotsRemaining(teamID))
 		end
 		GG.Delay.DelayCall(SendOrder, {teamID}, 2) -- frame after orders
 	end
@@ -146,7 +166,9 @@ local function Upgrade(unitID, newTeam)
 end
 
 local function RunAndJump(unitID, unitDefID, cmdID, spotNum, replace)
+	--Spring.Echo(UnitDefs[unitDefID].name .. [[ "Run And Jump!"]])
 	if not Spring.ValidUnitID(unitID) or Spring.GetUnitIsDead(unitID) then return end
+	--Spring.Echo(UnitDefs[unitDefID].name .. [[ "Run And Jump is valid and alive!"]])
 	cmdID = cmdID or CMD.MOVE
 	local jumpDef = GG.jumpDefs[unitDefID]
 	local jumpLength = Spring.GetUnitRulesParam(unitID, "jumpRange") or jumpDef.range
@@ -179,10 +201,12 @@ local function RunAndJump(unitID, unitDefID, cmdID, spotNum, replace)
 		z = z + targetVector.z*distCovered
 		y = Spring.GetGroundHeight(x,z)
 		local order = {cmdID, {x, y, z}, {replace and "" or "shift"}}
+		--Spring.Echo("Adding CMD_MOVE")
 		table.insert(orderArray, order)
 		x = x + targetVector.x*jumpLength
 		z = z + targetVector.z*jumpLength
 		y = Spring.GetGroundHeight(x,z)
+		--Spring.Echo("Adding CMD_JUMP")
 		order = {y > 0 and CMD_JUMP or cmdID, {x, y, z}, {"shift"}}
 		table.insert(orderArray, order)
 	end
@@ -201,7 +225,7 @@ local function Wander(unitID, cmd)
 		local offsetZ = math.random(50, 150)
 		offsetX = offsetX * -1 ^ (offsetX % 2)
 		offsetZ = offsetZ * -1 ^ (offsetZ % 2)
-		GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD.MOVE_STATE, {2}, {}}, 1)
+		--GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, CMD.MOVE_STATE, {2}, {}}, 1)
 		if cmd then
 			GG.Delay.DelayCall(Spring.GiveOrderToUnit, {unitID, cmd, {spot.x + offsetX, 0, spot.z + offsetZ}, {}}, 1)
 		end
@@ -239,37 +263,36 @@ local function Perk(unitID, perkID, firstTime)
 	end
 end
 
+local WAIT_TIME = 35 * 30 -- 35s
 local function UnitIdleCheck(unitID, unitDefID, teamID)
 	if Spring.GetUnitIsDead(unitID) then return false end
 	local cmdQueueSize = Spring.GetCommandQueue(unitID, -1, false) or 0
 	if cmdQueueSize > 0 then 
-		--Spring.Echo("I'm so not idle!") 
+		--Spring.Echo(UnitDefs[unitDefID].name .. [[ "I'm so not idle!"]]) 
 		return
 	end
 	local ud = UnitDefs[unitDefID]
 	local cp = ud.customParams
 	if cp.unittype == "mech" then -- vehicles handled by unit_vehiclePad as they are always 'automated'
 		-- random chance a idle unit will wander somewhere else
-		local chance = math.random(1, 100)
-		if chance < 75 then
+		--local chance = math.random(1, 100)
+		--if chance < 75 then
 			--Spring.Echo(UnitDefs[unitDefID].name .. [[ "Fuck it, I'm off for a wander"]])
 			if cp.jumpjets then
-				GG.Delay.DelayCall(RunAndJump, {unitID, unitDefID}, 30 * 25)
+				GG.Delay.DelayCall(RunAndJump, {unitID, unitDefID}, 1)
 			else
-				if not Spring.GetUnitRulesParam(unitID, "dronesout") == 1 then
-					GG.Delay.DelayCall(Wander, {unitID}, 30 * 25)
-				end
+				GG.Delay.DelayCall(Wander, {unitID}, 1)
 			end
-		else
+		--else
 			--Spring.Echo(UnitDefs[unitDefID].name .. [[ "I think I'll stay here..."]])
-		end
+		--end
 	end
 end
 
 	
 function gadget:UnitIdle(unitID, unitDefID, teamID)
 	if AI_TEAMS[teamID] then
-		GG.Delay.DelayCall(UnitIdleCheck, {unitID, unitDefID, teamID}, 10)
+		GG.Delay.DelayCall(UnitIdleCheck, {unitID, unitDefID, teamID}, WAIT_TIME)
 	end
 end
 
@@ -282,9 +305,9 @@ function gadget:UnitUnloaded(unitID, unitDefID, teamID, transportID, transportTe
 			--Spring.Echo("C3!")
 			-- C3's take a little time to deploy and grant the extra tonnage & slots, so delay 10s
 			GG.Delay.DelayCall(Spam, {teamID}, 10 * 30)
-			for k,v in pairs(Spring.GetTeamUnits(teamID)) do
+			--[[for k,v in pairs(Spring.GetTeamUnits(teamID)) do
 				GG.Delay.DelayCall(UnitIdleCheck, {unitID, unitDefID, teamID}, 10 * 30)
-			end
+			end]]
 		elseif unitDefID == VPAD_ID then
 			teamUpgradeCounts[teamID][VPAD_ID] = teamUpgradeCounts[teamID][VPAD_ID] + 1
 		elseif ud.canFly then
@@ -296,12 +319,10 @@ function gadget:UnitUnloaded(unitID, unitDefID, teamID, transportID, transportTe
 			--Spring.Echo("JUMP MECH!")
 			Perk(unitID, PERK_JUMP_RANGE, true)
 			RunAndJump(unitID, unitDefID)
-		else
-			--Spring.Echo("VEHICLE OR MECH!")
+		elseif cp.unittype == "mech" then
+			--Spring.Echo("MECH!")
+			Perk(unitID, nil, true)
 			Wander(unitID)
-			if cp.unittype == "mech" then
-				Perk(unitID, nil, true)
-			end
 		end
 	end
 end
@@ -337,6 +358,20 @@ function gadget:UnitGiven(unitID, unitDefID, newTeam, oldTeam)
 			gadget:UnitUnloaded(unitID, unitDefID, newTeam)
 		end
 	end
+end
+
+function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
+	if AI_TEAMS[teamID] then
+		if UnitDefs[unitDefID].customParams.unittype == "mech" 
+		and not (cmdOptions.shift or cmdOptions.alt)
+		and (Spring.GetCommandQueue(unitID, -1, false) or 0) > 0 
+		and not cmdID == CMD.INSERT
+		then
+			--Spring.Echo(UnitDefs[unitDefID].name, "Tried to replace a queue with ", cmdID, CMD[cmdID])
+			return false -- unit has a queue and this is trying to replace it
+		end
+	end
+	return true
 end
 
 else
