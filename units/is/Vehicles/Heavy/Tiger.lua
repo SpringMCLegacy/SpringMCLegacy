763c526e83df5caaa81adcb130f64c6dcacfa869
zverofaust <misanthrope099@hotmail.com>
2016-01-09 08:09:46 -0500

    new Tiger, broken :(

---
+++ units/is/Vehicles/Heavy/Tiger.lua
@@ -0,0 +1,33 @@
+local Tiger = Tank:New{
+	name              	= "Tiger",
+	description         = "Heavy Strike Tank",
+	trackWidth			= 23,--width to render the decal
+
+	weaTigerns	= {	
+		[1] = {
+			name	= "AC10",
+		},
+		[2] = {
+			name	= "SRM4",
+		},
+		[3] = {
+			name	= "MG",
+		},
+	},
+	
+	customparams = {
+		tonnage			= 55,
+		variant         = "",
+		speed			= 60,
+		price			= 6190,
+		heatlimit 		= 20,
+		armor			= {type = "standard", tons = 7},
+		maxammo 		= {ac10 = 2, srm = 1},
+		barrelrecoildist = {[1] = 3},
+		squadsize 		= 2,
+    },
+}
+
+return lowerkeys({
+	["FS_Tiger"] = Tiger:New(),
+})
