ea2b25761582b8d8e38810c5e2a9386609754418
zverofaust <misanthrope099@hotmail.com>
2016-01-11 00:34:12 -0500

    Added a TMA-0 flag profile

---
+++ maps/flagConfig/TMA-0_profile.lua
@@ -0,0 +1,64 @@
+local resources = {
+	{
+		x = 5853,
+		z = 3687,
+		feature = nil
+	},
+	{
+		x = 8208,
+		z = 5444,
+		feature = nil
+	},
+	{
+		x = 13121,
+		z = 5704,
+		feature = nil
+	},
+	{
+		x = 5592,
+		z = 6612,
+		feature = nil
+	},
+	{
+		x = 10610,
+		z = 6268,
+		feature = nil
+	},
+	{
+		x = 8509,
+		z = 7935,
+		feature = nil
+	},
+	{
+		x = 6894,
+		z = 8069,
+		feature = nil
+	},
+	{
+		x = 4328,
+		z = 8728,
+		feature = nil
+	},
+	{
+		x = 6287,
+		z = 10345,
+		feature = nil
+	},
+	{
+		x = 8593,
+		z = 10455,
+		feature = nil
+	},
+	{
+		x = 11404,
+		z = 9000,
+		feature = nil
+	},
+	{
+		x = 7802,
+		z = 13130,
+		feature = nil
+	},	
+}
+
+return resources
