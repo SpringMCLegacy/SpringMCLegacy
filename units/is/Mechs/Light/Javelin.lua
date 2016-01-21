dd7a33efdebc52f5c5b2a25b0433ceb45d0b8f60
zverofaust <misanthrope099@hotmail.com>
2016-01-21 12:26:03 -0500

    +Javelin textured and tweaked LBX spread from 1200 to 800

--- units/is/Mechs/Light/Javelin.lua
+++ units/is/Mechs/Light/Javelin.lua
@@ -28,6 +28,29 @@ local JVN11B = Javelin:New{
     },
 }
 
+local JVN10P = Javelin:New{
+	description         = "Light Skirmisher",
+	weapons	= {	
+		[1] = {
+			name	= "SRM6",
+		},
+		[2] = {
+			name	= "SSRM4",
+		},
+	},
+		
+	customparams = {
+		variant         = "JVN-10P",
+		speed			= 90,
+		price			= 5900,
+		heatlimit 		= 20,
+		armor			= {type = "ferro", tons = 5},
+		jumpjets		= 6,
+		maxammo 		= {srm = 2},
+    },
+}
+
 return lowerkeys({
 	["FS_Javelin_JVN11B"] = JVN11B:New(),
+	["FS_Javelin_JVN10P"] = JVN10P:New(),
 })
