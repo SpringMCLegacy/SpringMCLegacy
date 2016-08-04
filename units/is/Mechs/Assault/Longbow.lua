02875a304c2f948d590ed9d2cdd1b94527c3c750
zverofaust <misanthrope099@hotmail.com>
2016-08-04 13:15:10 -0400

    Longbow LRM and Arrow.

---
+++ units/is/Mechs/Assault/Longbow.lua
@@ -0,0 +1,77 @@
+local Longbow = Assault:New{
+	name				= "Longbow",
+
+	customparams = {
+		cockpitheight	= 64,
+		tonnage			= "85",
+    },
+}
+	
+local LGB12C = Longbow:New{
+	description         = "Assault LRM Support",
+	weapons	= {	
+		[1] = {
+			name	= "LRM20",
+		},
+		[2] = {
+			name	= "LRM20",
+		},
+		[3] = {
+			name	= "LRM15",
+		},
+		[4] = {
+			name	= "LRM15",
+		},
+		[5] = {
+			name	= "ERSBL",
+		},
+		[6] = {
+			name	= "ERSBL",
+		},
+		[7] = {
+			name	= "ERSBL",
+		},
+	},
+		
+    customparams = {
+		variant			= "LGB-12C",
+		speed			= 50,
+		price			= 16860,
+		heatlimit 		= 24,
+		armor			= {type = "standard", tons = 12.5},
+		maxammo 		= {lrm = 9},
+    },
+}
+
+local LGB8V = Longbow:New{
+	description         = "Assault Artillery Support",
+	weapons	= {	
+		[1] = {
+			name	= "ArrowIV",
+		},
+		[2] = {
+			name	= "ArrowIV",
+		},
+		[3] = {
+			name	= "SPL",
+		},
+	},
+		
+    customparams = {
+		variant			= "LGB-8V",
+		speed			= 50,
+		price			= 18080,
+		heatlimit 		= 32,
+		armor			= {type = "standard", tons = 12.5},
+		maxammo 		= {arrow = 8},
+    },
+}
+	
+return lowerkeys({ 
+	["CC_Longbow_LGB12C"] = LGB12C:New(),
+	["FS_Longbow_LGB12C"] = LGB12C:New(),
+	["FS_Longbow_LGB8V"] = LGB8V:New(),
+	["LA_Longbow_LGB8V"] = LGB8V:New(),
+	["FW_Longbow_LGB12C"] = LGB12C:New(),
+	["FW_Longbow_LGB8V"] = LGB8V:New(),
+})
