50fd9f44764ceb1a4aa23afb20cb641de2ab7d6c
zverofaust <misanthrope099@hotmail.com>
2016-08-07 11:09:38 -0400

    Adding Scarabus so FS and LA have a Mech with TAG.

---
+++ units/is/Mechs/Light/Scarabus.lua
@@ -0,0 +1,43 @@
+local Scarabus = Light:New{
+	name				= "Scarabus",
+	
+	customparams = {
+		cockpitheight	= 36,
+		tonnage			= 30,
+    },
+}
+
+local SCB9A = Scarabus:New{
+	description         = "Light EWAR",
+	weapons	= {	
+		[1] = {
+			name	= "MBL",
+		},
+		[2] = {
+			name	= "MBL",
+		},
+		[3] = {
+			name	= "SBL",
+		},
+		[4] = {
+			name	= "SBL",
+		},
+		[5] = {
+			name	= "TAG",
+		},
+	},
+		
+	customparams = {
+		variant         = "STO-4A",
+		speed			= 150,
+		price			= 8460,
+		heatlimit 		= 10,
+		armor			= {type = "ferro", tons = 5.5},
+		ecm 			= true,
+    },
+}
+
+return lowerkeys({
+	["FS_Scarabus_SCB9A"] = SCB9A:New(),
+	["LA_Scarabus_SCB9A"] = SCB9A:New(),
+})
