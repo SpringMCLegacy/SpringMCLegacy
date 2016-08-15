a46ebda3b73bef53ded8dea867303992965d44c8
Craig Lawrence <flozi@blueyonder.co.uk>
2016-08-15 18:20:01 +0100

    * Contrails for leopard

--- LuaUI/Configs/lupsUnitFXs.lua
+++ LuaUI/Configs/lupsUnitFXs.lua
@@ -22,8 +22,28 @@ effectUnitDefs = {
 		{class='Ribbon',	options={width=6, size=6, piece="root1"}},
 		{class='Ribbon',	options={width=6, size=6, piece="root2"}},
 	},
+
  }
 
+leopard = {
+	{class='Ribbon',	options={width=4, size=12, piece="fin1"}},
+	{class='Ribbon',	options={width=4, size=12, piece="fin2"}},
+	{class='Ribbon',	options={width=2, size=8, piece="fin3"}},
+	{class='Ribbon',	options={width=2, size=8, piece="fin4"}},
+	{class='Ribbon',	options={width=6, size=6, piece="nose1"}},
+	{class='Ribbon',	options={width=6, size=6, piece="nose22"}},
+}
+
+local sideData = VFS.Include("gamedata/sidedata.lua", {})
+local Sides = {}
+for sideNum, data in pairs(sideData) do
+	Sides[sideNum] = data.shortName:lower()
+end
+
+for i, sideName in pairs(Sides) do
+	effectUnitDefs[sideName .. "_dropship_leopard"] = leopard
+end
+	
 effectUnitDefsXmas = {
 	-- armcom = {
 		-- {class='SantaHat',	options={color={0,0.7,0,1.0}, pos={0,4,0.35}, emitVector={0.3,1.0,0.2},	width =  2.7, height=6, ballSize=0.7, piece="head"}},
