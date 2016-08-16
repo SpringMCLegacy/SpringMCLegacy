dce619728731308af4920d39360fef01f7e7f97b
zverofaust <misanthrope099@hotmail.com>
2016-08-16 13:28:57 -0400

    a map config for speed_therockjungle

---
+++ maps/flagConfig/Speed_TheRockJungle_V1.1_profile.lua
@@ -0,0 +1,34 @@
+local resources = {
+	{
+		x = 1054,
+		z = 781,
+		feature = nil
+	},
+	{
+		x = 2416,
+		z = 3297,
+		feature = nil
+	},
+	{
+		x = 4712,
+		z = 1837,
+		feature = nil
+	},
+	{
+		x = 5433,
+		z = 6399,
+		feature = nil
+	},
+	{
+		x = 7666,
+		z = 4784,
+		feature = nil
+	},
+	{
+		x = 9327,
+		z = 7227,
+		feature = nil
+	},
+}
+
+return resources
