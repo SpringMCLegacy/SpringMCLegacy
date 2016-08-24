a2662c77355f303e7d13b52750ea99dca9cc4664
zverofaust <misanthrope099@hotmail.com>
2016-08-24 14:35:48 -0400

    Remodel of Galleon + distribution to all IS factions

--- units/is/Vehicles/Light/Galleon.lua
+++ units/is/Vehicles/Light/Galleon.lua
@@ -1,31 +1,39 @@
 local Galleon = LightTank:New{
 	name              	= "Galleon",
-	trackWidth			= 20,
+	description			= "Light Striker Tank",
+	trackWidth			= 18,
 		
 	weapons	= {	
 		[1] = {
-			name	= "MPL",
+			name	= "MBL",
 		},
 		[2] = {
-			name	= "MBL",
+			name	= "SBL",
+			mainDir = [[1 0 1]],
+			maxAngleDif = 100,
 		},
 		[3] = {
-			name	= "MBL",
+			name	= "SBL",
+			mainDir = [[-1 0 1]],
+			maxAngleDif = 100,
 		},
 	},
 	
 	customparams = {
 		tonnage			= 30,
-		variant         = "GAL-102",
+		variant         = "GAL-100",
 		speed			= 90,
-		price			= 1620,
+		price			= 3090,
 		heatlimit 		= 10,
 		armor			= {type = "standard", tons = 3.5},
-		squadsize 		= 2,
-		replaces		= "fw_scorpion",
+		squadsize 		= 3,
     },
 }
 
 return lowerkeys({
+	["CC_Galleon"] = Galleon:New(),
+	["DC_Galleon"] = Galleon:New(),
+	["FS_Galleon"] = Galleon:New(),
 	["FW_Galleon"] = Galleon:New(),
+	["LA_Galleon"] = Galleon:New(),
 })
