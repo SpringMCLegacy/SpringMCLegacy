c3b621a6ae77c7d8ea09fbf3325866d2c564535f
zverofaust <misanthrope099@hotmail.com>
2016-08-25 08:51:59 -0400

    Goblin remodel and activation (replaces FS APC)

--- units/is/Vehicles/Medium/Goblin.lua
+++ units/is/Vehicles/Medium/Goblin.lua
@@ -1,20 +1,27 @@
 local Goblin = LightTank:New{
 	name              	= "Goblin",
-	description			= "Medium Brawler",
+	description			= "Troop Transport",
 	trackWidth			= 23,--width to render the decal
+	
+	transportCapacity		= 5,
+	transportSize = 1,	
 	
 	weapons	= {	
 		[1] = {
-			name	= "SRM6",
+			name	= "LPL",
 		},
 		[2] = {
-			name	= "LPL",
+			name	= "SRM6",
 		},
 		[3] = {
 			name	= "MG",
+			mainDir = [[1 0 1]],
+			maxAngleDif = 90,
 		},
 		[4] = {
 			name	= "MG",
+			mainDir = [[-1 0 1]],
+			maxAngleDif = 90,
 		},
 		[5] = {
 			name	= "AMS",
@@ -25,15 +32,15 @@ local Goblin = LightTank:New{
 		tonnage			= 45,
 		variant         = "IFV",
 		speed			= 60,
-		price			= 4850,
+		price			= 7900,
 		heatlimit 		= 10,
 		armor			= {type = "standard", tons = 8},
-		maxammo 		= {srm = 1},
+		maxammo 		= {srm = 2},
 		squadsize 		= 1,
+		replaces		= "fs_heavyapc",
     },
 }
 
 return lowerkeys({
-	--["FS_Goblin"] = Goblin:New(),
-	--["FW_Goblin"] = Goblin:New(),
+	["FS_Goblin"] = Goblin:New(),
 })
