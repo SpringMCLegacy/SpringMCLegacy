1c907eabb12fbe3a79e69eb52f4a5a750b278e90
zverofaust <misanthrope099@hotmail.com>
2016-08-25 21:03:48 -0400

    * New map config for "4way noname v02" aka "4-way-beta-map". Start positions suck but it might be interesting. https://springfiles.com/spring/spring-maps/4-way-beta-map

---
+++ maps/flagConfig/4way noname v02_profile.lua
@@ -0,0 +1,34 @@
+local resources = {
+	{
+		x = 6125,
+		z = 8615,
+		feature = nil
+	},
+	{
+		x = 6140,
+		z = 3530,
+		feature = nil
+	},
+	{
+		x = 1945,
+		z = 8695,
+		feature = nil
+	},
+	{
+		x = 10255,
+		z = 8650,
+		feature = nil
+	},
+	{
+		x = 10330,
+		z = 3630,
+		feature = nil
+	},
+	{
+		x = 1875,
+		z = 3635,
+		feature = nil
+	},
+}
+
+return resources
