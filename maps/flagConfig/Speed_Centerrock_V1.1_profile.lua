42cf657346fdbe8eff2cb022f701c1f58b20e386
zverofaust <misanthrope099@hotmail.com>
2016-09-10 09:30:18 -0400

    Vehidles.ods update and added Speed_Centerrock_V1.1 beacon profile because it's a pretty sweet looking map.

---
+++ maps/flagConfig/Speed_Centerrock_V1.1_profile.lua
@@ -0,0 +1,34 @@
+local resources = {
+	{
+		x = 4722,
+		z = 821,
+		feature = nil
+	},
+	{
+		x = 7660,
+		z = 2617,
+		feature = nil
+	},
+	{
+		x = 2478,
+		z = 1867,
+		feature = nil
+	},
+	{
+		x = 6323,
+		z = 5218,
+		feature = nil
+	},
+	{
+		x = 1773,
+		z = 5384,
+		feature = nil
+	},
+	{
+		x = 3226,
+		z = 7015,
+		feature = nil
+	},
+}
+
+return resources
