dc902606c1bc828399ea1b4549ba0cba078f257d
zverofaust <misanthrope099@hotmail.com>
2017-02-15 13:09:42 -0500

    Flag profile for Sandcastles2Big

---
+++ maps/flagConfig/Sandcastles2Big_profile.lua
@@ -0,0 +1,29 @@
+local resources = {
+	{
+		x = 740,
+		z = 6150,
+		feature = nil
+	},
+	{
+		x = 6145,
+		z = 710,
+		feature = nil
+	},
+	{
+		x = 11550,
+		z = 6140,
+		feature = nil
+	},
+	{
+		x = 6159,
+		z = 11550,
+		feature = nil
+	},
+	{
+		x = 6145,
+		z = 6145,
+		feature = nil
+	},
+}
+
+return resources
