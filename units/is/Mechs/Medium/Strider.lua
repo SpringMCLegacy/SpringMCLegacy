a4c3f5e0d7daf8aee2fc5094d38be92d0fbc535f
zverofaust <misanthrope099@hotmail.com>
2017-08-24 10:14:42 -0400

    * They call him Strider.

---
+++ units/is/Mechs/Medium/Strider.lua
@@ -0,0 +1,60 @@
+local Strider = Medium:New{
+	name				= "Strider",
+	
+	customparams = {
+		cockpitheight	= 43,
+		tonnage			= 40,
+    },
+}
+
+local SR1OP = Strider:New{
+	description         = "Medium Skirmisher",
+	weapons	= {	
+		[1] = {
+			name	= "ASRM6",
+		},
+		[2] = {
+			name	= "ASRM6",
+		},
+		[3] = {
+			name	= "SBL",
+		},
+	},
+
+	customparams = {
+		variant			= "SR1-O Prime",
+		speed			= 90,
+		price			= 8670,
+		heatlimit 		= 10,
+		armor			= {type = "standard", tons = 7.5},
+		jumpjets		= 6,
+		maxammo 		= {srm = 2},
+    },
+}
+
+local SR1OB = Strider:New{
+	description         = "Medium LRM Support",
+	weapons	= {	
+		[1] = {
+			name	= "LRM10",
+		},
+		[2] = {
+			name	= "LRM10",
+		},
+	},
+
+	customparams = {
+		variant			= "SR1-O B",
+		speed			= 90,
+		price			= 9100,
+		heatlimit 		= 10,
+		armor			= {type = "standard", tons = 7.5},
+		jumpjets		= 6,
+		maxammo 		= {lrm = 2},
+    },
+}
+
+return lowerkeys({ 
+	["DC_Strider_SR1OP"] = SR1OP:New(),
+	["DC_Strider_SR1OB"] = SR1OB:New(),
+})
