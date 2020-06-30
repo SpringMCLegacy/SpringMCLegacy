99b107cfb99ee8265bba75bfdaef474e61b158de
Zvero <misanthrope099@hotmail.com>
2020-06-30 16:44:47 -0400

    * Increased ammo to 4x vanilla

--- Gamedata/AmmoTypes.lua
+++ Gamedata/AmmoTypes.lua
@@ -1,21 +1,21 @@
 return {
-	LRM			= 48,--24/ton
-	SRM			= 100,--50/ton
+	LRM			= 96,--24/ton
+	SRM			= 200,--50/ton
 	InfSRM		= 2,--1/ton
-	ATM			= 40,--20/ton
-	MRM			= 48,--24/ton
-	Arrow		= 10,--5/ton
-	Narc		= 12,--6/ton
-	MML			= 66,--33-40/ton
+	ATM			= 80,--20/ton
+	MRM			= 96,--24/ton
+	Arrow		= 20,--5/ton
+	Narc		= 24,--6/ton
+	MML			= 132,--33-40/ton
 
-	AC2			= 90,--45/ton
-	AC5			= 40,--20/ton
-	AC10 		= 20,--10/ton
-	AC20		= 10,--5/ton
-	Thumper		= 30,--15/ton ???
-	Sniper		= 20,--10/ton ???
+	AC2			= 180,--45/ton
+	AC5			= 80,--20/ton
+	AC10 		= 40,--10/ton
+	AC20		= 20,--5/ton
+	Thumper		= 20,--15/ton ???
+	Sniper		= 15,--10/ton ???
 
-	Gauss		= 16,--8/ton
-	LtGauss		= 32,--16/ton
-	HvGauss		= 8,--4/ton
+	Gauss		= 32,--8/ton
+	LtGauss		= 64,--16/ton
+	HvGauss		= 16,--4/ton
 }
