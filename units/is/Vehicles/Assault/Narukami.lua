c411f7d61e14eae0db0988153440192dc1a033e6
Zvero <misanthrope099@hotmail.com>
2020-07-06 12:11:54 -0400

    * Re-added faction vehicles, also Narukami (untextured) for DC.

---
+++ units/is/Vehicles/Assault/Narukami.lua
@@ -0,0 +1,40 @@
+local Narukami = Tank:New{
+	name              	= "Narukami",
+	description         = "Superheavy Strike Tank",
+	trackWidth			= 34,--width to render the decal
+	
+	weapons	= {	
+		[1] = {
+			name	= "Gauss",
+		},
+		[2] = {
+			name	= "PPC",
+		},
+		[3] = {
+			name	= "PPC",
+		},
+		[4] = {
+			name	= "AMS",
+		},
+		[5] = {
+			name	= "AMS",
+		},
+	},
+
+	customparams = {
+		tonnage			= 90,
+		variant         = "",
+		speed			= 50,
+		price			= 21250,
+		heatlimit 		= 20,
+		armor			= {type = "standard", tons = 17.5},
+		maxammo 		= {gauss = 2},
+		barrelrecoildist = {[1] = 5},
+		squadsize 		= 1,
+		--replaces		= "dc_behemoth",
+	},
+}
+
+return lowerkeys({
+	["DC_Narukami"] = Narukami:New(),
+})
