0ecc6699b63a26a007a6529443c037408df4a8ce
Zvero <misanthrope099@hotmail.com>
2020-07-09 13:10:46 -0400

    * Tweaks to ammo amounts

--- Gamedata/AmmoTypes.lua
+++ Gamedata/AmmoTypes.lua
@@ -1,21 +1,21 @@
 return {
 	LRM			= 96,--24/ton
-	SRM			= 200,--50/ton
+	SRM			= 100,--50/ton
 	InfSRM		= 2,--1/ton
 	ATM			= 80,--20/ton
 	MRM			= 96,--24/ton
-	Arrow		= 20,--5/ton
-	Narc		= 24,--6/ton
+	Arrow		= 5,--5/ton
+	Narc		= 12,--6/ton
 	MML			= 132,--33-40/ton
 
 	AC2			= 180,--45/ton
 	AC5			= 80,--20/ton
-	AC10 		= 40,--10/ton
-	AC20		= 20,--5/ton
+	AC10 		= 25,--10/ton
+	AC20		= 10,--5/ton
 	Thumper		= 20,--15/ton ???
 	Sniper		= 15,--10/ton ???
 
-	Gauss		= 32,--8/ton
-	LtGauss		= 64,--16/ton
-	HvGauss		= 16,--4/ton
+	Gauss		= 13,--8/ton
+	LtGauss		= 40,--16/ton
+	HvGauss		= 4,--4/ton
 }
