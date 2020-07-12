0b465279996be6523caf3dd9604f7cfa44bdab56
Zvero <misanthrope099@hotmail.com>
2020-07-12 14:34:58 -0400

    * Draconis Sunder assault Omnimech in various flavours

---
+++ units/is/Mechs/Assault/Sunder.lua
@@ -0,0 +1,124 @@
+local Sunder = Assault:New{
+	name				= "Sunder",
+    customparams = {
+		cockpitheight	= 5,
+		tonnage			= 90,
+    },
+}
+
+local SD1OP = Sunder:New{
+	description         = "Assault Brawler",
+	weapons 		= {	
+		[1] = {
+			name	= "AC20",
+		},
+		[2] = {
+			name	= "LBL",
+		},
+		[3] = {
+			name	= "LBL",
+		},
+		[4] = {
+			name	= "MBL",
+			OnlyTargetCategory = "ground",
+		},
+		[5] = {
+			name	= "MBL",
+			OnlyTargetCategory = "ground",
+		},
+		[6] = {
+			name	= "SRM4",
+		},
+		[7] = {
+			name	= "SRM4",
+		},
+		[8] = {
+			name	= "SRM4",
+		},
+	},
+    customparams = {
+		variant			= "SD1-O Prime",
+		speed			= 60,
+		price			= 17470,
+		heatlimit 		= 30,
+		armor			= {type = "standard", tons = 16.5},
+		maxammo 		= {ac20 = 2, srm = 2},
+		barrelrecoildist = {[1] = 3},
+    },
+}
+
+local SD1OA = Sunder:New{
+	description         = "Assault Multirole",
+	weapons 		= {	
+		[1] = {
+			name	= "Gauss",
+		},
+		[2] = {
+			name	= "ERPPC",
+		},
+		[3] = {
+			name	= "MPL",
+			OnlyTargetCategory = "ground",
+		},
+		[4] = {
+			name	= "MPL",
+			OnlyTargetCategory = "ground",
+		},
+		[5] = {
+			name	= "LRM5",
+		},
+		[6] = {
+			name	= "LRM5",
+		},
+		[7] = {
+			name	= "LRM5",
+		},
+	},
+    customparams = {
+		variant			= "SD1-O cfg A",
+		speed			= 50,
+		price			= 20030,
+		heatlimit 		= 30,
+		armor			= {type = "standard", tons = 16.5},
+		maxammo 		= {gauss = 2, lrm = 2},
+		barrelrecoildist = {[1] = 3},
+    },
+}
+
+local SD1OC = Sunder:New{
+	description         = "Assault Brawler",
+	weapons 		= {	
+		[1] = {
+			name	= "MRM40",
+		},
+		[2] = {
+			name	= "MRM30",
+		},
+		[3] = {
+			name	= "MBL",
+			OnlyTargetCategory = "ground",
+		},
+		[4] = {
+			name	= "SSRM4",
+			OnlyTargetCategory = "ground",
+		},
+		[5] = {
+			name	= "SSRM4",
+			OnlyTargetCategory = "ground",
+		},
+	},
+    customparams = {
+		variant			= "SD1-O cfg C",
+		speed			= 60,
+		price			= 17350,
+		heatlimit 		= 30,
+		armor			= {type = "standard", tons = 16.5},
+		maxammo 		= {mrm = 5, srm = 1},
+    },
+}
+
+return lowerkeys({
+	DC_Sunder_SD1OP = SD1OP:New(),
+	DC_Sunder_SD1OA = SD1OA:New(),
+	DC_Sunder_SD1OC = SD1OC:New(),
+})
