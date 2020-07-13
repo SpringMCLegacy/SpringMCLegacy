bd367796d8efd32364e380a21c926dedb4f1ac53
Craig Lawrence <flozi@blueyonder.co.uk>
2020-07-13 15:51:32 +0100

    * Fix mcl_center_camera widget (#136)

--- LuaUI/Widgets/mcl_center_camera.lua
+++ LuaUI/Widgets/mcl_center_camera.lua
@@ -16,7 +16,7 @@ function widget:UnitCreated(unitID, unitDefID)
 	if unitDef.name:find("dropzone") then
 		local x,y,z = Spring.GetUnitPosition(unitID)
 		Spring.SelectUnitArray({unitID})
-		Spring.GiveOrderToUnit(unitID, Spring.GetGameRulesParam("CMD_MENU_LIGHTMECH"), {}, {})
+		Spring.GiveOrderToUnit(unitID, Spring.GetGameRulesParam("CMD_MENU_SCOUTMECH"), {}, {})
 		Spring.SetCameraTarget(x,y,z)
 		widgetHandler:RemoveWidget()
 	end
