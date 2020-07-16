cf8c1421e381ae57ed06bfff0c99369db8152462
Zvero <misanthrope099@hotmail.com>
2020-07-16 17:40:13 -0400

    * Lyran Hollander II medium-class heavy gauss sniper

---
+++ units/is/Mechs/Medium/HollanderII.lua
@@ -0,0 +1,31 @@
+local HollanderII = Medium:New{
+	name				= "Hollander II",
+	customparams = {
+		cockpitheight	= 6,
+		tonnage			= 45,
+    },
+}
+
+local BZKF7 = HollanderII:New{
+	description         = "Medium Ranged",
+	weapons	= {	
+		[1] = {
+			name	= "HeavyGauss",
+			OnlyTargetCategory = "ground",
+		},
+	},
+	
+    customparams = {
+		variant         = "BZK-F7",
+		speed			= 80,
+		price			= 11920,
+		heatlimit 		= 10,
+		armor			= {type = "ferro", tons = 5.5},
+		maxammo 		= {hvgauss = 3},
+		barrelrecoildist = {[1] = 4},
+    },
+}
+
+return lowerkeys({ 
+	["LA_HollanderII_BZKF7"] = BZKF7:New(),
+})
