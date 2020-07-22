dc35c2a02ba3bbba11d1406dc2304868df29b3d6
Craig Lawrence <flozi@blueyonder.co.uk>
2020-07-22 13:25:48 +0100

    * Base DroST on DropShip base class in an attempt to get big engine fx to work (failed)

--- units/dropships/IS_DroST.lua
+++ units/dropships/IS_DroST.lua
@@ -1,35 +1,13 @@
-local IS_DroST = Unit:New{ -- TODO: better base class than Unit
+local IS_DroST = DropShip:New{
 	name              	= "DroST II Landing Craft",
 	description         = "Cargo Landing Craft",
-	objectName        	= "Dropship/DroST.s3o",
+	objectName        	= "Dropship/DroST.s3o", -- TODO: remove once faction textured models are available
 	iconType			= "drost",
-	script				= "Dropship.lua",
 	category 			= "ground notbeacon",
-	activateWhenBuilt   = true,
 	maxDamage           = 13000,
 	mass                = 13000,
-	footprintX			= 20,
-	footprintZ 			= 20,
-	--collisionVolumeType = "ellipsoid",
-	buildCostEnergy     = 0,
-	buildCostMetal      = 0,
-	buildTime           = 0,
-	canMove				= true,
-	maxVelocity			= 0,
-	idleAutoHeal		= 0,
-	maxSlope			= 50,
-	moveState			= 0,
-	levelGround			= false,
-	movementClass		= "LARGEMECH", -- herp
 	usePieceCollisionVolumes = true,
-	power				= 1, -- don't target me!
 	
-	-- Transport tags
-	transportSize		= 8,
-	transportCapacity	= 64, -- 1x transportSize
-	transportMass		= 1000000,
-	holdSteady 			= true,
-
 	--Makes unit use weapon from /weapons folder
 	weapons	= {	
 		[1] = {
@@ -49,17 +27,8 @@ local IS_DroST = Unit:New{ -- TODO: better base class than Unit
 			maxAngleDif = 100,
 		},
 		[5] = {
-			name	= "NBL",
+			name	= "MBL",
 			maxAngleDif = 100,
-		},
-	},
-	--Gets CEG effects from /gamedata/explosions folder
-	sfxtypes = {
-		explosiongenerators = {
-			"custom:heavy_jet_trail_blue",
-			"custom:medium_jet_trail_blue",
-			"custom:dropship_main_engine_stage2",
-			"custom:heavy_jet_trail",
 		},
 	},
 	customparams = {
