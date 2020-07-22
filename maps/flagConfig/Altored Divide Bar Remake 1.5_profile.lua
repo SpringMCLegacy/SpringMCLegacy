283949f0d9b889d09e1c6a26a18b959216ab7b8f
Zvero <misanthrope099@hotmail.com>
2020-07-22 09:13:20 -0400

    * Config profiles for Altored Divide Remake (1v1/2v2) and BlackStar v2 (5-FFA)

---
+++ maps/flagConfig/Altored Divide Bar Remake 1.5_profile.lua
@@ -0,0 +1,39 @@
+local resources = {
+	{
+		x = 1000,
+		z = 3120,
+		feature = nil
+	},
+	{
+		x = 1575,
+		z = 5140,
+		feature = nil
+	},
+	{
+		x = 4200,
+		z = 3160,
+		feature = nil
+	},
+	{
+		x = 4050,
+		z = 5070,
+		feature = nil
+	},
+	{
+		x = 6790,
+		z = 3060,
+		feature = nil
+	},
+	{
+		x = 7050,
+		z = 5115,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 15,
+	water = 8,
+}
+
+return resources, temps
