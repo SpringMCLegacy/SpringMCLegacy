80138b6c06f033e85fd4d748cf270f7ad1b3c9b7
Zvero <misanthrope099@hotmail.com>
2020-07-26 14:51:20 -0400

    * Beacon profile for Talus_v2, my new favorite map: https://springfiles.com/spring/spring-maps/talusv2

---
+++ maps/flagConfig/Talus_v2_profile.lua
@@ -0,0 +1,58 @@
+local resources = {
+	{
+		x = 1247,
+		z = 3015,
+		feature = nil
+	},
+	{
+		x = 6961,
+		z = 5160,
+		feature = nil
+	},
+	{
+		x = 2699,
+		z = 4954,
+		feature = nil
+	},
+	{
+		x = 5821,
+		z = 3227,
+		feature = nil
+	},
+	{
+		x = 4055,
+		z = 7012,
+		feature = nil
+	},
+	{
+		x = 4219,
+		z = 1295,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 30,
+	water = 25,
+}
+
+local starts = {
+	[0] = { -- teamID 1
+		x = 7149,
+		z = 749,
+	},
+	[1] = { -- teamID 2
+		x = 1050,
+		z = 7646,
+	},
+	[2] = { -- teamID 3
+		x = 7792,
+		z = 1416,
+	},
+	[3] = { -- teamID 4
+		x = 333,
+		z = 6923,
+	},
+}
+
+return resources, temps, starts
