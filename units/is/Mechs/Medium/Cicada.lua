f29ba16923f6febe3686f42320728bee608d1865
Zvero <misanthrope099@hotmail.com>
2020-08-05 13:39:50 -0400

    * Cicada medium scout for FWL

---
+++ units/is/Mechs/Medium/Cicada.lua
@@ -0,0 +1,40 @@
+local Cicada = Medium:New{
+	name				= "Cicada",
+	
+	customparams = {
+		cockpitheight	= 3,
+		tonnage			= 40,
+    },
+}
+
+local CDA3M = Cicada:New{
+	description         = "Medium Scout",
+	weapons	= {	
+		[1] = {
+			name	= "UAC5",
+		},
+		[2] = {
+			name	= "MBL",
+		},
+		[3] = {
+			name	= "MBL",
+		},
+		[4] = {
+			name	= "SPL",
+		},
+	},
+
+	customparams = {
+		variant			= "CMA-1S",
+		speed			= 120,
+		price			= 8120,
+		heatlimit 		= 20,
+		armor			= {type = "standard", tons = 4},
+		maxammo 		= {ac5 = 1},
+		barrelrecoildist = {[1] = 3},
+    },
+}
+
+return lowerkeys({ 
+	["FW_Cicada_CDA3M"] = CDA3M:New(),
+})
