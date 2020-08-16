718aa4d6475f24654edb29efb19645440d0c70b7
Zvero <misanthrope099@hotmail.com>
2020-08-15 22:28:26 -0400

    * Anubis

---
+++ units/is/Mechs/Light/Anubis.lua
@@ -0,0 +1,49 @@
+local Anubis = Light:New{
+	name				= "Anubis",
+	customparams = {
+		cockpitheight	= 4,
+		tonnage			= 30,
+    },
+}
+
+local ABS3L = Anubis:New{
+	description         = "Light Sniper",
+	weapons	= {	
+		[1] = {
+			name	= "LRM5",
+		},
+		[2] = {
+			name	= "LRM5",
+			SlaveTo	= 1,
+		},
+		[3] = {
+			name	= "LRM5",
+			SlaveTo	= 1,
+		},
+		[4] = {
+			name	= "LRM5",
+			SlaveTo	= 1,
+		},
+		[5] = {
+			name	= "ERSBL",
+		},
+		[6] = {
+			name	= "ERSBL",
+			SlaveTo	= 5,
+		},
+	},
+	
+    customparams = {
+		variant         = "ABS-3L",
+		speed			= 120,
+		price			= 9510,
+		heatlimit 		= 20,
+		armor			= {type = "stealth", tons = 5.5},
+		maxammo 		= {lrm = 1},
+		ecm 			= true,
+    },
+}
+
+return lowerkeys({ 
+	["CC_Anubis_ABS3L"] = ABS3L:New(),
+})
