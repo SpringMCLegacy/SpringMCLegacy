4d2af95714379c65aed49c9c17c4f2b69d19458e
zverofaust <aestewart84@gmail.com>
2025-05-12 21:10:18 -0400

    Create Jade Empress 1.41_profile.lua

---
+++ maps/flagConfig/Jade Empress 1.41_profile.lua
@@ -0,0 +1,70 @@
+local resources = {
+	{
+		x = 7007,
+		z = 3294,
+		feature = nil
+	},
+	{
+		x = 12848,
+		z = 8889,
+		feature = nil
+	},
+	{
+		x = 3929,
+		z = 5139,
+		feature = nil
+	},
+	{
+		x = 11388,
+		z = 12782,
+		feature = nil
+	},
+	{
+		x = 5994,
+		z = 7373,
+		feature = nil
+	},
+	{
+		x = 9016,
+		z = 11665,
+		feature = nil
+	},
+	{
+		x = 3482,
+		z = 8627,
+		feature = nil
+	},
+	{
+		x = 8251,
+		z = 12944,
+		feature = nil
+	},
+	{
+		x = 4268,
+		z = 10350,
+		feature = nil
+	},
+	{
+		x = 6307,
+		z = 12483,
+		feature = nil
+	},
+	{
+		x = 4555,
+		z = 7545,
+		feature = nil
+	},
+	{
+		x = 9843,
+		z = 10332,
+		feature = nil
+	},
+	
+}
+
+local temps = {
+	ambient = 40,
+	water = 30,
+}
+
+return resources, temps
