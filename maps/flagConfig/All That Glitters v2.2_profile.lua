c7337dc64bb4cbadda16000757873a82aa7d15c6
zverofaust <aestewart84@gmail.com>
2025-05-17 19:02:48 -0400

    Create All That Glitters v2.2_profile.lua

---
+++ maps/flagConfig/All That Glitters v2.2_profile.lua
@@ -0,0 +1,70 @@
+local resources = {
+	{
+		x = 3244,
+		z = 940,
+		feature = nil
+	},
+	{
+		x = 5283,
+		z = 656,
+		feature = nil
+	},
+	{
+		x = 5098,
+		z = 2299,
+		feature = nil
+	},
+	{
+		x = 2927,
+		z = 3377,
+		feature = nil
+	},
+	{
+		x = 5217,
+		z = 4280,
+		feature = nil
+	},
+	{
+		x = 1516,
+		z = 5771,
+		feature = nil
+	},
+	{
+		x = 3110,
+		z = 6934,
+		feature = nil
+	},
+	{
+		x = 1072,
+		z = 7303,
+		feature = nil
+	},
+	{
+		x = 1183,
+		z = 9702,
+		feature = nil
+	},
+	{
+		x = 2767,
+		z = 9507,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 10,
+	water = 5,
+}
+
+local starts = {
+	[0] = { -- teamID 1
+		x = 1073,
+		z = 937,
+	},
+	[1] = { -- teamID 2
+		x = 5171,
+		z = 9164,
+	},
+}
+
+return resources, temps, starts
