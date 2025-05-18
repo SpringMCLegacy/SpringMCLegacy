c8872e11b6d7da0035ff4c87c8d20ae7b05cd3bb
zverofaust <aestewart84@gmail.com>
2025-05-18 14:21:57 -0400

    Create Starwatcher 1.0_profile.lua
    https://springfiles.springrts.com/?type=2&filter=517902c239611ce59262e50ec4e9f7ee

---
+++ maps/flagConfig/Starwatcher 1.0_profile.lua
@@ -0,0 +1,78 @@
+local resources = {
+	{
+		x = 4291,
+		z = 1015,
+		feature = nil
+	},
+	{
+		x = 7414,
+		z = 1097,
+		feature = nil
+	},
+	{
+		x = 1137,
+		z = 3311,
+		feature = nil
+	},
+	{
+		x = 2927,
+		z = 3864,
+		feature = nil
+	},
+	{
+		x = 5080,
+		z = 2936,
+		feature = nil
+	},
+	{
+		x = 9319,
+		z = 4246,
+		feature = nil
+	},
+	{
+		x = 11280,
+		z = 3932,
+		feature = nil
+	},
+	{
+		x = 7195,
+		z = 5101,
+		feature = nil
+	},
+	{
+		x = 4877,
+		z = 7141,
+		feature = nil
+	},
+	{
+		x = 8056,
+		z = 7152,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 20,
+	water = 10,
+}
+
+local starts = {
+	[0] = { -- teamID 1
+		x = 782,
+		z = 644,
+	},
+	[1] = { -- teamID 2
+		x = 11475,
+		z = 7503,
+	},
+	[2] = { -- teamID 3
+		x = 11563,
+		z = 608,
+	},
+	[3] = { -- teamID 4
+		x = 778,
+		z = 7514,
+	},
+}
+
+return resources, temps, starts
