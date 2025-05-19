4aa2ae698adc535ed7b3722be266d38e715083f6
zverofaust <aestewart84@gmail.com>
2025-05-19 12:11:15 -0400

    Create Carrot Mountains v2.0_profile.lua

---
+++ maps/flagConfig/Carrot Mountains v2.0_profile.lua
@@ -0,0 +1,118 @@
+local resources = {
+	{
+		x = 6240, 
+		z = 6023,
+		feature = nil
+	},
+	{
+		x = 6278, 
+		z = 644,
+		feature = nil
+	},
+	{
+		x = 3543, 
+		z = 1995,
+		feature = nil
+	},
+	{
+		x = 8006, 
+		z = 1549,
+		feature = nil
+	},
+	{
+		x = 7359, 
+		z = 3470,
+		feature = nil
+	},
+	{
+		x = 4754, 
+		z = 4271,
+		feature = nil
+	},
+	{
+		x = 1316, 
+		z = 3691,
+		feature = nil
+	},
+	{
+		x = 9633, 
+		z = 4259,
+		feature = nil
+	},
+	{
+		x = 11162, 
+		z = 3466,
+		feature = nil
+	},
+	{
+		x = 2417, 
+		z = 7261,
+		feature = nil
+	},
+	{
+		x = 4807, 
+		z = 8061,
+		feature = nil
+	},
+	{
+		x = 8375, 
+		z = 6672,
+		feature = nil
+	},
+	{
+		x = 10709, 
+		z = 7310,
+		feature = nil
+	},
+	{
+		x = 9844, 
+		z = 10044,
+		feature = nil
+	},
+	{
+		x = 6231, 
+		z = 9420,
+		feature = nil
+	},
+	{
+		x = 1056, 
+		z = 8820,
+		feature = nil
+	},
+	{
+		x = 3425, 
+		z = 10717,
+		feature = nil
+	},
+	{
+		x = 7787, 
+		z = 11976,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 35,
+	water = 20,
+}
+
+local starts = {
+	[0] = { -- teamID 1
+		x = 841,
+		z = 1520,
+	},
+	[1] = { -- teamID 2
+		x = 11943,
+		z = 11794,
+	},
+	[2] = { -- teamID 3
+		x = 11026,
+		z = 1370,
+	},
+	[3] = { -- teamID 4
+		x = 498,
+		z = 10740,
+	},
+}
+
+return resources, temps, starts
