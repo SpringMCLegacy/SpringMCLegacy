2bb6a65fd0dfc5c4b7f8780abca1013d00b3a8dc
zverofaust <aestewart84@gmail.com>
2025-05-24 12:30:58 -0400

    Create Seven Rivers v1.21_profile.lua

---
+++ maps/flagConfig/Seven Rivers v1.21_profile.lua
@@ -0,0 +1,58 @@
+local resources = {
+	{
+		x = 5777,
+		z = 924,
+		feature = nil
+	},
+	{
+		x = 5547,--
+		z = 3076,
+		feature = nil
+	},
+	{
+		x = 10043,--
+		z = 3640,
+		feature = nil
+	},
+	{
+		x = 2294,--
+		z = 4717,
+		feature = nil
+	},
+	{
+		x = 6637,--
+		z = 5021,
+		feature = nil
+	},
+	{
+		x = 6577,
+		z = 7251,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 35,
+	water = 25,
+}
+
+local starts = {
+	[0] = { -- teamID
+		x = 2242,
+		z = 963,
+	},
+	[1] = { -- teamID
+		x = 10080,
+		z = 7237,
+	},
+	[2] = { -- teamID
+		x = 11069,
+		z = 447,
+	},
+	[3] = { -- teamID
+		x = 449,
+		z = 7528,
+	},
+}
+
+return resources, temps, starts
