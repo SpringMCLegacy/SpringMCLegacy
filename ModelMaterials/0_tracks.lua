7966cd536833a82569166a5fce90b1c83b93a319
FLOZi <craiglawrence86@gmail.com>
2025-05-24 17:22:10 +0100

    * Blind commit to probably fix vehicle tracks

--- ModelMaterials/0_tracks.lua
+++ ModelMaterials/0_tracks.lua
@@ -90,7 +90,7 @@ local unitMaterials = {}
 for i=1,#UnitDefs do
   local udef = UnitDefs[i]
 
-  if udef.leaveTracks and udef.customParams.baseclass == "Vehicle" and not udef.customParams.wheels and udef.customParams.normaltex then
+  if udef.leaveTracks and udef.customParams.baseclass == "vehicle" and not udef.customParams.wheels and udef.customParams.normaltex then
     unitMaterials[udef.name] = {
 	  "trackShader", 
 	  NORMALTEX = udef.customParams.normaltex,
