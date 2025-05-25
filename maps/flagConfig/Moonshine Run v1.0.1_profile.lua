fd9aba572cbf1d6a412a947f3952ea3402750835
zverofaust <aestewart84@gmail.com>
2025-05-25 01:54:55 -0400

    Create Moonshine Run v1.0.1_profile.lua

---
+++ maps/flagConfig/Moonshine Run v1.0.1_profile.lua
@@ -0,0 +1,79 @@
+local resources = {
+	{
+		x = 4264,
+		z = 6711,
+		feature = nil
+	},
+	{
+		x = 8025,
+		z = 1419,
+		feature = nil
+	},
+	{
+		x = 4137,
+		z = 1859,
+		feature = nil
+	},
+	{
+		x = 8078,
+		z = 6394,
+		feature = nil
+	},
+	{
+		x = 3684,
+		z = 4619,
+		feature = nil
+	},
+	{
+		x = 9066,
+		z = 3788,
+		feature = nil
+	},
+	{
+		x = 5900,
+		z = 3175,
+		feature = nil
+	},
+	{
+		x = 6360,
+		z = 4969,
+		feature = nil
+	},
+	{
+		x = 11364,
+		z = 4739,
+		feature = nil
+	},
+	{
+		x = 1031,
+		z = 3440,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 0,
+	water = -20,
+	hovers = true,
+}
+
+local starts = {
+	[0] = { -- teamID
+		x = 1897,
+		z = 6601,
+	},
+	[1] = { -- teamID
+		x = 10343,
+		z = 1472,
+	},
+	[2] = { -- teamID
+		x = 1482,
+		z = 1377,
+	},
+	[3] = { -- teamID
+		x = 10677,
+		z = 6695,
+	},
+}
+
+return resources, temps, starts
