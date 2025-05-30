57fa4e63c393a5c1ef6afe9e4ab591d07808793d
FLOZi <craiglawrence86@gmail.com>
2025-05-30 20:03:26 +0100

    * Return vpad to a sane 5500 maxdamage -- Do we set all outposts other than garrison to 5000? What explains the current variance?

--- units/outposts/Outpost_VehiclePad.lua
+++ units/outposts/Outpost_VehiclePad.lua
@@ -1,7 +1,7 @@
 local Outpost_VehiclePad = Outpost:New{
 	name              	= "Vehicle Pad (Support)",
 	description         = "Allows delivery of offensive vehicle support units",
-	maxDamage           = 72000,
+	maxDamage           = 5500,
 	mass                = 5000,
 	buildCostMetal      = 10000,
 
