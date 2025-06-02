e2ee8838d695971078b1512eb40299fb09d211ce
zverofaust <aestewart84@gmail.com>
2025-06-02 00:45:17 -0400

    Replace Ravaged_v2 with Ravaged Remake v1.2, visual improvements
    https://discord.com/channels/1364417352619266192/1364417443841183854/1378957086494560388

---
+++ maps/flagConfig/Ravaged Remake v1.2_profile.lua
@@ -0,0 +1,19 @@
+local resources = {
+	{
+		x = 1780,
+		z = 1130,
+		feature = nil
+	},
+	{
+		x = 3350,
+		z = 4060,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 8,
+	water = 2,
+}
+
+return resources, temps
