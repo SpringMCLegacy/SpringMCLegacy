fbc5cce43c0ea16d7e865c23f79f8ce0c22fea70
zverofaust <aestewart84@gmail.com>
2025-06-06 14:16:41 -0400

    Create Ascendancy v2.2_profile.lua

---
+++ maps/flagConfig/Ascendancy v2.2_profile.lua
@@ -0,0 +1,84 @@
+local resources = {
+	{
+		x = 4469,
+		z = 9113,
+		feature = nil
+	},
+	{
+		x = 8067,
+		z = 8955,
+		feature = nil
+	},
+	{
+		x = 6151,
+		z = 8066,
+		feature = nil
+	},
+	{
+		x = 6130,
+		z = 6375,
+		feature = nil
+	},
+	{
+		x = 4707,
+		z = 5104,
+		feature = nil
+	},
+	{
+		x = 7554,
+		z = 4797,
+		feature = nil
+	},
+	{
+		x = 904,
+		z = 1456,
+		feature = nil
+	},
+	{
+		x = 11233,
+		z = 1423,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = -10,
+	water = -30,
+}
+
+local starts = {
+	[0] = { -- teamID 1
+		x = 2276,
+		z = 7899,
+	},
+	[1] = { -- teamID 2
+		x = 10075,
+		z = 8161,
+	},
+	[2] = { -- teamID 3
+		x = 569,
+		z = 4691,
+	},
+	[3] = { -- teamID 4
+		x = 11790,
+		z = 4804,
+	},
+	[4] = { -- teamID 1
+		x = 2956,
+		z = 2865,
+	},
+	[5] = { -- teamID 2
+		x = 8977,
+		z = 3074,
+	},
+	[6] = { -- teamID 3
+		x = 2202,
+		z = 11248,
+	},
+	[7] = { -- teamID 4
+		x = 10243,
+		z = 11697,
+	},
+}
+
+return resources, temps, starts
