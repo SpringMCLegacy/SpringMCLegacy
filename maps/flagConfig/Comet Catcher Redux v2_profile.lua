fb918915fc9322faebe5a7b5b68c596063f1e6a3
FLOZi <craiglawrence86@gmail.com>
2025-06-06 22:45:35 +0100

    * Its not TA without Comet Catcher

---
+++ maps/flagConfig/Comet Catcher Redux v2_profile.lua
@@ -0,0 +1,35 @@
+local resources = {
+	{
+		x = 4671,
+		z = 6810,
+		feature = nil,
+	},
+	{
+		x = 1002,
+		z = 5244,
+		feature = nil,
+	},
+	{
+		x = 5121,
+		z = 2910,
+		feature = nil,
+	},
+	{
+		x = 1458,
+		z = 1313,
+		feature = nil,
+	},
+	{
+		x = 3031,
+		z = 3767,
+		feature = nil,
+	},
+}
+
+local temps = {
+	ambient = 0,
+	water = 0,
+	hovers = false,
+}
+
+return resources, temps
