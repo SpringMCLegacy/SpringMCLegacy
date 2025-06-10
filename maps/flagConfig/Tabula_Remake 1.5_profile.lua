f378b524283df8343b59e6d8ebd2d805136d83df
zverofaust <aestewart84@gmail.com>
2025-06-10 11:05:39 -0400

    Create Tabula_Remake 1.5_profile.lua

---
+++ maps/flagConfig/Tabula_Remake 1.5_profile.lua
@@ -0,0 +1,74 @@
+local resources = {
+	{
+		x = 1505,
+		z = 5575,
+		feature = nil
+	},
+	{
+		x = 4133,
+		z = 5922,
+		feature = nil
+	},
+	{
+		x = 6172,
+		z = 5000,
+		feature = nil
+	},
+	{
+		x = 2035,
+		z = 2221,
+		feature = nil
+	},
+	{
+		x = 4050,
+		z = 1258,
+		feature = nil
+	},
+	{
+		x = 6686,
+		z = 1600,
+		feature = nil
+	},
+	{
+		x = 2411,
+		z = 4107,
+		feature = nil
+	},
+	{
+		x = 5815,
+		z = 3136,
+		feature = nil
+	},
+	{
+		x = 4061,
+		z = 3597,
+		feature = nil
+	},
+}
+
+local temps = {
+	ambient = 20,
+	water = 15,
+	hovers = false,
+}
+
+local starts = {
+	[0] = { -- teamID 1
+		x = 699,
+		z = 561,
+	},
+	[1] = { -- teamID 2
+		x = 7493,
+		z = 6613,
+	},
+	[2] = { -- teamID 1
+		x = 458,
+		z = 6400,
+	},
+	[3] = { -- teamID 2
+		x = 7741,
+		z = 772,
+	},
+}
+
+return resources, temps, starts
