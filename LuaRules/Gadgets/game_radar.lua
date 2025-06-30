6abd136ce9c6c9c6061157a62167a4b0ebef3eac
FLOZi <craiglawrence86@gmail.com>
2025-06-30 20:37:27 +0100

    * Fix a lua error relating to bola pods trying to change speed of static units i.e. outposts -- should probably disallow targeting with NARC if it is Bola'd, but hey, pintle nerf

--- LuaRules/Gadgets/game_radar.lua
+++ LuaRules/Gadgets/game_radar.lua
@@ -350,7 +350,7 @@ function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, w
 	if weaponID == NARC_ID then
 		-- Don't allow dropships to be NARCed
 		if UnitDefs[unitDefID].customParams.dropship then return 0 end
-		if speedChange then -- Bola Pod
+		if speedChange and GG.mechCache[unitDefID] then -- Bola Pod
 			--Spring.Echo("speed change now", Spring.GetGameFrame())
 			GG.SpeedChange(unitID, unitDefID, 0.1)
 			DelayCall(GG.SpeedChange, {unitID, unitDefID, 1}, 5*30)
