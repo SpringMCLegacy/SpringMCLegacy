42efc8392ca8e2381d68170ae6b492e86d5eb5f1
zverofaust <aestewart84@gmail.com>
2025-07-12 17:49:22 -0400

    Update Centerrock Remake 1.2_profile.lua

--- maps/flagConfig/Centerrock Remake 1.2_profile.lua
+++ maps/flagConfig/Centerrock Remake 1.2_profile.lua
@@ -1,28 +1,48 @@
 local resources = {
-	{
+	{ --center
 		x = 4230,
 		z = 3750,
-		feature = nil
+		feature = nil,
+		radiusmult = 2.5,
+		points = 0,	
 	},
-	{
+	{ --west plateau
 		x = 1305,
 		z = 3890,
-		feature = nil
+		feature = nil,
+		radius = 300,
+		points = 6,	
 	},
-	{
+	{ --north plateau
 		x = 4640,
 		z = 1000,
-		feature = nil
+		feature = nil,
+		points = 0,
 	},
-	{
+	{ --south plateau
 		x = 4375,
 		z = 6600,
-		feature = nil
+		feature = nil,
+		points = 0,
 	},
-	{
+	{ --east plateau
 		x = 7580,
 		z = 2960,
-		feature = nil
+		feature = nil,	
+		radius = 300,
+		points = 6,	
+	},
+	{ --northeast corner
+		x = 7687,
+		z = 299,
+		feature = nil,	
+		radius = 300,
+	},
+	{ --southwest corner
+		x = 567,
+		z = 6278,
+		feature = nil,	
+		radius = 300,
 	},
 }
 
@@ -31,4 +51,17 @@ local temps = {
 	water = 14,
 }
 
-return resources, temps
+local starts = {
+	[0] = { -- teamID 1
+		x = 2253,
+		z = 433,
+		alwaysbeacon = 1,
+	},
+	[1] = { -- teamID 2
+		x = 6636,
+		z = 7700,
+		alwaysbeacon = 1,
+	},
+}
+
+return resources, temps, starts
