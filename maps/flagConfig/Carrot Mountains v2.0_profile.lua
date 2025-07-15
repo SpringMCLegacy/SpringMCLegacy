e07c8011365721399d3a2f67650d25d1680c0e50
zverofaust <aestewart84@gmail.com>
2025-07-15 09:03:26 -0400

    Fix Carrot mountain being broken since ???

--- maps/flagConfig/Carrot Mountains v2.0_profile.lua
+++ maps/flagConfig/Carrot Mountains v2.0_profile.lua
@@ -1,93 +1,93 @@
-llocal resources = {
+local resources = {
 	{
 		x = 6240, 
 		z = 6023,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 6278, 
 		z = 644,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 3543, 
 		z = 1995,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 8006, 
 		z = 1549,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 7359, 
 		z = 3470,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 4754, 
 		z = 4271,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 1316, 
 		z = 3691,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 9633, 
 		z = 4259,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 11162, 
 		z = 3466,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 2417, 
 		z = 7261,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 4807, 
 		z = 8061,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 8375, 
 		z = 6672,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 10709, 
 		z = 7310,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 9844, 
 		z = 10044,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 6231, 
 		z = 9420,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 1056, 
 		z = 8820,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 3425, 
 		z = 10717,
-		feature = nil
+		feature = nil,
 	},
 	{
 		x = 7787, 
 		z = 11976,
-		feature = nil
+		feature = nil,
 	},
 }
 
