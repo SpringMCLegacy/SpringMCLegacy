5709071f8b94948c20bd7e3a1ea57469801d1912
FLOZi <craiglawrence86@gmail.com>
2025-07-25 22:36:08 +0100

    * Move cruise missile icon code to outpost_launcher, creates a new instance of ProjectileCreated callin, will see if that causes any appreciable slowness

--- LuaRules/Gadgets/outpost_launcher.lua
+++ LuaRules/Gadgets/outpost_launcher.lua
@@ -26,11 +26,12 @@ local UseTeamResource 		= Spring.UseTeamResource
 -- Constants
 local COLOURS = GG.GameConstants.colours
 local CRUISE_MISSILE_COST = 10000
+local CRUISE_MISSILE_ID = WeaponDefNames["cruisemissile"].id
 local LAUNCHER_ID = UnitDefNames["outpost_launcher"].id
 local CMD_STOCKPILE = CMD.STOCKPILE
 
 -- Variables
-
+local CMs = {}
 
 function gadget:StockpileChanged(unitID, unitDefID, unitTeam, weaponNum, oldCount, newCount)
 	--Spring.Echo("StockpileChanged", unitID, unitDefID, unitTeam, weaponNum, oldCount, newCount)
@@ -70,7 +71,31 @@ function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOpt
 	return true
 end
 
+
+function NukeIcon(proID, teamID, iconUnit)
+	if not iconUnit then
+		CMs[proID] = true
+		local x,y,z = Spring.GetProjectilePosition(proID)
+		iconUnit = Spring.CreateUnit("nuke_icon", x, y, z, 0, teamID)
+		Spring.MoveCtrl.Enable(iconUnit)
+	end
+	if CMs[proID] then
+		local x,y,z = Spring.GetProjectilePosition(proID)
+		local vx,vy,vz = Spring.GetProjectileVelocity(proID)
+		Spring.MoveCtrl.SetPosition(iconUnit, x,y,z)
+		Spring.MoveCtrl.SetVelocity(iconUnit, vx, vy, vz)
+		GG.Delay.DelayCall(NukeIcon, {proID, teamID, iconUnit}, 5)
+	end
+end
+
+function gadget:ProjectileCreated(proID, proOwnerID, weaponID)
+	if weaponID == CRUISE_MISSILE_ID then
+		NukeIcon(proID, Spring.GetUnitTeam(proOwnerID))
+	end
+end
+
 function gadget:Initialize()
+	Script.SetWatchProjectile(CRUISE_MISSILE_ID, true)
 	for _,unitID in ipairs(Spring.GetAllUnits()) do
 		local teamID = Spring.GetUnitTeam(unitID)
 		local unitDefID = Spring.GetUnitDefID(unitID)
