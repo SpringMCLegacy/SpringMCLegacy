c27799d942a8bf00a851abbd43111aebc8c314e6
zverofaust <aestewart84@gmail.com>
2025-08-04 08:08:50 -0400

    Wolfhound retexture + new variant

--- units/is/Mechs/Light/Wolfhound.lua
+++ units/is/Mechs/Light/Wolfhound.lua
@@ -1,6 +1,12 @@
 local Wolfhound = Light:New{
 	name              	= "Wolfhound",
 	
+	leaveTracks			= true,	
+	trackType			= "Wolfhound",
+	trackOffset			= 6,
+	trackWidth			= 20,
+	trackStretch 		= 2,
+	
 	customparams = {
 		cockpitheight	= 8.5,
 		tonnage 		= 35,
@@ -18,12 +24,15 @@ local WLF2 = Wolfhound:New{
 		},
 		[3] = {
 			name	= "MBL",
+			SlaveTo = 2;
 		},
 		[4] = {
 			name	= "MBL",
+			SlaveTo = 2;
 		},
 		[5] = {
 			name	= "MBL",
+			SlaveTo = 2;
 		},
 	},
 		
@@ -37,6 +46,42 @@ local WLF2 = Wolfhound:New{
     },
 }
 
+local WLF3S = Wolfhound:New{
+	description         = "Light Vanguard",
+	weapons	= {	
+		[1] = {
+			name	= "ERPPC",
+		},
+		[2] = {
+			name	= "ERMBL",
+		},
+		[3] = {
+			name	= "ERMBL",
+			SlaveTo = 2;
+		},
+		[4] = {
+			name	= "ERMBL",
+			SlaveTo = 2;
+		},
+		[5] = {
+			name	= "ERSBL",
+			SlaveTo = 2;
+		},
+	},
+		
+	customparams = {
+		variant         = "WLF-3S",
+		speed			= 90,
+		price			= 11790,
+		heatlimit 		= 12,--10 Double
+		armor			= 7.5,
+		mods			= {"doubleheatsinks", "endosteel", "lightengine"},
+    },
+}
+
 return lowerkeys({
 	["LA_Wolfhound_WLF2"] = WLF2:New(),
+	["FS_Wolfhound_WLF2"] = WLF2:New(),
+	
+	["LA_Wolfhound_WLF3S"] = WLF3S:New(),
 })
