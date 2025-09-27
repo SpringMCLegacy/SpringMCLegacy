3c2d2bfe80cde67ea89e8ccd2c2672c2a28cb788
FLOZi <craiglawrence86@gmail.com>
2025-09-27 18:57:16 +0100

    * Goldilocks and the aero speed multiplier

--- units/dropships/Avenger.lua
+++ units/dropships/Avenger.lua
@@ -193,7 +193,7 @@ maxRudder          = 0.0015,
 		entryDelay 		= 45,
 		prepDelay 		= 60,
 		spawnAtTarget	= true,
-		speed			= 252 * 1.5, -- 2520
+		speed			= 252 * 1.75, -- 2520
 		price      		= 36000,
 		maxfuel = 30,
     },
