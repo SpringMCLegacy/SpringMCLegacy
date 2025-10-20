b49692f76a09b2ac9c55d8b74df265e7acb439c9
zverofaust <aestewart84@gmail.com>
2025-10-20 11:20:52 -0400

    Update Krakatoa_V2.0_profile.lua
    Make more interesting/better

--- maps/flagConfig/Krakatoa_V2.0_profile.lua
+++ maps/flagConfig/Krakatoa_V2.0_profile.lua
@@ -3,47 +3,56 @@ local resources = {
 		x = 7190,
 		z = 7032,
 		feature = nil,
-		points = 6,
-		radius = 400,
+		points = 0,
+		radius = 800,
 	},
-	{ -- N
+	--[[{ -- N
 		x = 7181,
 		z = 3998,
+		points = 0,
 		feature = nil,
-	},
+		radius = 800,
+	},]]
 	{ -- NE
 		x = 9255,
 		z = 5003,
+		points = 3,
 		feature = nil,
 	},
-	{ -- E
+	--[[{ -- E
 		x = 10315,
 		z = 7218,
+		points = 0,
 		feature = nil,
-	},
+	},]]
 	{ -- SE
 		x = 9508,
 		z = 9325,
+		points = 3,
 		feature = nil,
 	},
-	{ -- S
+	--[[{ -- S
 		x = 7403,
 		z = 10276,
+		points = 0,
 		feature = nil,
-	},
+	},]]
 	{ -- SW
 		x = 5348,
 		z = 9603,
+		points = 3,
 		feature = nil,
 	},
-	{ -- W
+	--[[{ -- W
 		x = 4323,
 		z = 7089,
+		points = 0,
 		feature = nil,
-	},
+	},]]
 	{ -- NW
 		x = 4923,
 		z = 5092,
+		points = 3,
 		feature = nil,
 	},
 }
@@ -57,42 +66,42 @@ local starts = {
 	[0] = { -- teamID
 		x = 2181,
 		z = 5282,
-		alwaysbeacon = true,
+		--alwaysbeacon = true,
 	},
 	[1] = { -- teamID
 		x = 12341,
 		z = 9037,
-		alwaysbeacon = true,
+		--alwaysbeacon = true,
 	},
 	[2] = { -- teamID
-		x = 4896,
-		z = 2369,
-		alwaysbeacon = true,
-	},
-	[3] = { -- teamID
-		x = 9524,
-		z = 12084,
-		alwaysbeacon = true,
-	},
-	[4] = { -- teamID
 		x = 9143,
 		z = 2214,
-		alwaysbeacon = true,
+		--alwaysbeacon = true,
 	},
-	[5] = { -- teamID
+	[3] = { -- teamID
 		x = 5311,
 		z = 12077,
-		alwaysbeacon = true,
+		--alwaysbeacon = true,
+	},
+	[4] = { -- teamID
+		x = 4896,
+		z = 2369,
+		--alwaysbeacon = true,
+	},
+	[5] = { -- teamID
+		x = 9524,
+		z = 12084,
+		--alwaysbeacon = true,
 	},
 	[6] = { -- teamID
 		x = 12098,
 		z = 5285,
-		alwaysbeacon = true,
+		--alwaysbeacon = true,
 	},
 	[7] = { -- teamID
 		x = 2339,
 		z = 9333,
-		alwaysbeacon = true,
+		--alwaysbeacon = true,
 	},
 	
 	
