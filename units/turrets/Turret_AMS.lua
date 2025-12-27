efcc2ca6730847ae31c0afb782397b39b5c2cfdb
FLOZi <craiglawrence86@gmail.com>
2025-12-27 22:39:04 +0000

    * Transition light base LAMS to become AMS -- beware, AMS is ridiculously too good right now due to my lua burnblow implementation

---
+++ units/turrets/Turret_AMS.lua
@@ -0,0 +1,27 @@
+local Turret_AMS = Tower:New{
+	description         = "Anti-Missile System",
+	buildCostMetal      = 3300,
+	airSightDistance 		= 1500,
+	maxDamage           = 1000,
+
+	weapons = {	
+		[1] = {
+			name	= "AMS",
+		},
+		--[2] = {
+		--	name	= "AMS_Shield",
+		--},
+	},
+	customparams = {
+		turretturnspeed = 9000,
+		elevationspeed  = 9000,
+		turrettype = "turret",
+    },
+	sounds = {
+		select = "Turret",
+	}
+}
+
+return lowerkeys({
+	["Turret_AMS"] = Turret_AMS,
+})
