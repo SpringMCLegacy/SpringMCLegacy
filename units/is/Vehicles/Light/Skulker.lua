94cb58ad2881a144b3e80c9a5932aa7e87d111e2
zverofaust <aestewart84@gmail.com>
2025-12-26 19:09:36 -0500

    Skulker light scout car

---
+++ units/is/Vehicles/Light/Skulker.lua
@@ -0,0 +1,32 @@
+local Skulker = LightTank:New{
+	name              	= "Skulker",
+	description			= "Light Scout",
+	
+	trackWidth			= 12,--width to render the decal
+	
+	weapons 		= {	
+		[1] = {
+			name	= "MBL",
+		},
+	},
+	
+	customparams = {
+		tonnage			= 20,
+		variant         = "",
+		speed			= 110,
+		price			= 1150,
+		heatlimit 		= 10,
+		armor			= 4.5,
+		maxammo 		= {srm = 1},
+		squadsize 		= 3,
+		wheels			= true,
+    },
+}
+
+return lowerkeys({
+	["CC_Skulker"] = Skulker:New(),
+	["DC_Skulker"] = Skulker:New(),
+	["FS_Skulker"] = Skulker:New(),
+	["FW_Skulker"] = Skulker:New(),
+	["LA_Skulker"] = Skulker:New(),
+})
