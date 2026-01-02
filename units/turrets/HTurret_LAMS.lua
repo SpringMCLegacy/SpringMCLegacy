b87e3cfc141efb65e2365771521240c435bb159b
zverofaust <aestewart84@gmail.com>
2026-01-02 04:58:38 -0500

    Rest of Heavy Turrets

---
+++ units/turrets/HTurret_LAMS.lua
@@ -0,0 +1,21 @@
+local HTurret_LAMS = HeavyTurret:New{
+	description         = "Laser Anti-Missile System",
+	buildCostMetal      = 8500,
+	airSightDistance 		= 1750,
+	maxDamage           = 2000,
+
+	weapons = {	
+		[1] = {
+			name	= "LAMS",
+		},
+	},
+	customparams = {
+		turretturnspeed = 9000,
+		elevationspeed  = 9000,
+		turrettype = "turret",
+    },
+}
+
+return lowerkeys({
+	["HTurret_LAMS"] = HTurret_LAMS,
+})
