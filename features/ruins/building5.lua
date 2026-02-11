cc42e9c19142d325268ad72ced19e14de6cb1c55
zverofaust <aestewart84@gmail.com>
2026-02-11 07:05:48 -0500

    Building 5

---
+++ features/ruins/building5.lua
@@ -0,0 +1,13 @@
+local featureDefs = {
+  building5 = {
+    blocking           = true,
+    damage             = 5000,
+    description        = "Derelict Structure",
+    footprintX         = 5,
+    footprintZ         = 5,
+    metal              = 2000,
+    object             = "ruin/building5.s3o",
+    reclaimable        = true,
+  },
+}
+return featureDefs
