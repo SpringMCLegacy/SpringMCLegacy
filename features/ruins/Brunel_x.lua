dc18df4daa732e1185fe4a043b957d7b7d7c67aa
zverofaust <aestewart84@gmail.com>
2026-08-28 12:52:15 -0400

    Couple of map prop structures + Brunel_x.lua file for placement with (unreleased) metal spot prop spawner.

---
+++ features/ruins/Brunel_x.lua
@@ -0,0 +1,13 @@
+local featureDefs = {
+  brunel_x = {
+    blocking           = true,
+    damage             = 1000,
+    description        = "Wrecked Dump Truck",
+    footprintX         = 2,
+    footprintZ         = 2,
+    metal              = 220,
+    object             = "vehicle/corpse/Brunel_x.s3o",
+    reclaimable        = true,
+  },
+}
+return featureDefs
---
+++ features/ruins/fuel_tank_cluster.lua
@@ -0,0 +1,13 @@
+local featureDefs = {
+  fuel_tank_cluster = {
+    blocking           = true,
+    damage             = 1000,
+    description        = "Derelict Structure",
+    footprintX         = 2,
+    footprintZ         = 2,
+    metal              = 220,
+    object             = "ruin/fuel_tank_cluster.s3o",
+    reclaimable        = true,
+  },
+}
+return featureDefs
