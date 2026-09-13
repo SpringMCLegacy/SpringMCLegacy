a88ba7aece2d3296d7f8aa0f7a85bc2448ea1cc4
zverofaust <aestewart84@gmail.com>
2026-09-13 08:00:30 -0400

    A power turbine

---
+++ features/ruins/powerturbine.lua
@@ -0,0 +1,13 @@
+local featureDefs = {
+  powerturbine = {
+    blocking           = true,
+    damage             = 1000,
+    description        = "Derelict Structure",
+    footprintX         = 2,
+    footprintZ         = 2,
+    metal              = 400,
+    object             = "ruin/powerturbine.s3o",
+    reclaimable        = true,
+  },
+}
+return featureDefs
